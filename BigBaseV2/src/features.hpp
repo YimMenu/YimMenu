a548fe4f3204d44aa58fc633423917a5c236de5c
Yimura <andreas.maerten@scarlet.be>
2020-12-26 16:48:39 +0100

    fix(Globals): Moved globals from features to big namespace

--- BigBaseV2/src/features.hpp
+++ BigBaseV2/src/features.hpp
@@ -3,18 +3,21 @@
 #include "structs/player.hpp"
 #include "structs/temp.hpp"
 
-namespace big::features
+namespace big
 {
 	inline Player g_playerId;
 	inline Player g_selectedPlayer = -1;
 	inline player g_players[32];
 
-	// Screen Width & Height
-	inline int x, y;
-
 	// Temporary Variable struct
 	inline temp g_temp = temp{};
 
-	void run_tick();
-	void script_func();
+	// Screen Width & Height
+	inline int x, y;
+
+	namespace features
+	{
+		void run_tick();
+		void script_func();
+	}
 }
