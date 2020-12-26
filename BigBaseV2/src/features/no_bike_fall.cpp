dfa5f36a62f6331e9ec2e40b7bea32c9b5af8f05
Yimura <andreas.maerten@scarlet.be>
2020-12-26 22:07:07 +0100

    fix(NoBikeFall): Execute native in script thread to prevent crashes

--- BigBaseV2/src/features/no_bike_fall.cpp
+++ BigBaseV2/src/features/no_bike_fall.cpp
@@ -6,6 +6,9 @@ namespace big
 	{
 		bool bNoBikeFall = g_settings.options["no_bike_fall"].get<bool>();
 
-		PED::SET_PED_CAN_BE_KNOCKED_OFF_VEHICLE(PLAYER::PLAYER_PED_ID(), bNoBikeFall);
+		QUEUE_JOB_BEGIN_CLAUSE(=)
+		{
+			PED::SET_PED_CAN_BE_KNOCKED_OFF_VEHICLE(PLAYER::PLAYER_PED_ID(), bNoBikeFall);
+		}QUEUE_JOB_END_CLAUSE
 	}
 }
