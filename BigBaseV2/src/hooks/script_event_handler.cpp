939da8769db118d26c45b053f8353ec763c855db
Yimura <andreas.maerten@scarlet.be>
2020-12-28 01:32:22 +0100

    feat(Hook): Added logging to script event handler

--- BigBaseV2/src/hooks/script_event_handler.cpp
+++ BigBaseV2/src/hooks/script_event_handler.cpp
@@ -1,9 +1,17 @@
 #include "hooking.hpp"
+#include "natives.hpp"
 
 namespace big
 {
 	bool hooks::script_event_handler(std::int64_t NetEventStruct, std::int64_t CNetGamePlayer)
 	{
+		auto args = reinterpret_cast<std::int64_t*>(NetEventStruct + 0x70);
+		Player SenderID = *reinterpret_cast<std::int8_t*>(CNetGamePlayer + 0x2D);
+
+		const auto ScriptEventHash = args[0];
+
+		LOG(INFO) << "Received Script Event " << ScriptEventHash << " from Player " << PLAYER::GET_PLAYER_NAME(SenderID);
+
 		return g_hooking->m_script_event_hook.get_original<decltype(&script_event_handler)>()(NetEventStruct, CNetGamePlayer);
 	}
 }
