9ba248517e66f52158c7ef2ca285581fd1bf6201
Yimura <andreas.maerten@scarlet.be>
2020-12-29 00:21:15 +0100

    feat(TabTunables): Added option te set car sell value at INT_MAX

--- BigBaseV2/src/gui/tab_bar/tab_tunables.cpp
+++ BigBaseV2/src/gui/tab_bar/tab_tunables.cpp
@@ -11,9 +11,14 @@ namespace big
 			if (ImGui::Checkbox("No Idle Kick", g_settings.options["no_idle_kick"].get<bool*>()))
 				g_settings.save();
 
-			if (ImGui::Button("Set Car Sell Value at $25.000.000"))
+			if (ImGui::Button("Set Car Sell Value at 25 million"))
 			{
-				*script_global(99007).at(970).as<int*>() = (int)25e6;
+				*script_global(99007).at(970).as<int*>() = 25000000;
+			}
+
+			if (ImGui::Button("Set Car Sell Value at INT_MAX (2.1 billion)"))
+			{
+				*script_global(99007).at(970).as<int*>() = INT_MAX;
 			}
 
 			ImGui::EndTabItem();
