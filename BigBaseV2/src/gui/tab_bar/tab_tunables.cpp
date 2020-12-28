c48647c64e01ea1239129e088d2ee8dfe3082ffd
Yimura <andreas.maerten@scarlet.be>
2020-12-28 01:24:03 +0100

    feat(TabTuneables): Added sell car for 25mill

--- BigBaseV2/src/gui/tab_bar/tab_tunables.cpp
+++ BigBaseV2/src/gui/tab_bar/tab_tunables.cpp
@@ -11,6 +11,11 @@ namespace big
 			if (ImGui::Checkbox("No Idle Kick", g_settings.options["no_idle_kick"].get<bool*>()))
 				g_settings.save();
 
+			if (ImGui::Button("Set Car Sell Value at $25.000.000"))
+			{
+				*script_global(99007).at(970).as<int*>() = (int)25e6;
+			}
+
 			ImGui::EndTabItem();
 		}
 	}
