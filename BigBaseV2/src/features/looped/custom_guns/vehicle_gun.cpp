1667b46ba9fe556bb1b2499a7cd311c9ad473ed3
Yimura <andreas.maerten@scarlet.be>
2021-01-22 02:08:30 +0100

    fix(VehicleGun): Removed height offset

--- BigBaseV2/src/features/looped/custom_guns/vehicle_gun.cpp
+++ BigBaseV2/src/features/looped/custom_guns/vehicle_gun.cpp
@@ -25,7 +25,7 @@ namespace big
 
 				if (PAD::IS_DISABLED_CONTROL_JUST_RELEASED(0, 24))
 				{
-					Vector3 location = ENTITY::GET_OFFSET_FROM_ENTITY_IN_WORLD_COORDS(player, 0.f, 10.f, 1.5f);
+					Vector3 location = ENTITY::GET_OFFSET_FROM_ENTITY_IN_WORLD_COORDS(player, 0.f, 10.f, 0.f);
 					Vehicle veh = functions::spawn_vehicle(
 						"bus",
 						location,
