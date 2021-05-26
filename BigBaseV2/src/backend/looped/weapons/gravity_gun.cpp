3941178ace03ef57ebfc198a90e8802720306f21
Yimura <andreas.maerten@scarlet.be>
2021-05-26 14:24:49 +0200

    refactor(GravityGun): Fixed cleanup of selected entity

--- BigBaseV2/src/backend/looped/weapons/gravity_gun.cpp
+++ BigBaseV2/src/backend/looped/weapons/gravity_gun.cpp
@@ -19,96 +19,91 @@ namespace big
 		bool bGravityGun = g.weapons.custom_weapon == CustomWeapon::GRAVITY_GUN;
 		double multiplier = 3.0;
 
-		if (bGravityGun)
+		// ZOOMED IN
+		if (bGravityGun && PAD::IS_DISABLED_CONTROL_PRESSED(0, 25))
 		{
-			// ZOOMED IN
-			if (PAD::IS_DISABLED_CONTROL_PRESSED(0, 25))
+			PLAYER::DISABLE_PLAYER_FIRING(PLAYER::PLAYER_ID(), true);
+			for (int control : controls)
+				PAD::DISABLE_CONTROL_ACTION(0, control, true);
+
+			location = ENTITY::GET_ENTITY_COORDS(PLAYER::PLAYER_PED_ID(), true);
+
+			// Attack RELEASED
+			if (PAD::IS_DISABLED_CONTROL_PRESSED(0, 24) && ent == 0)
 			{
-				PLAYER::DISABLE_PLAYER_FIRING(PLAYER::PLAYER_ID(), true);
-				for (int control : controls)
-					PAD::DISABLE_CONTROL_ACTION(0, control, true);
-
-				location = ENTITY::GET_ENTITY_COORDS(PLAYER::PLAYER_PED_ID(), true);
-
-				// Attack RELEASED
-				if (PAD::IS_DISABLED_CONTROL_PRESSED(0, 24) && ent == 0)
+				if (entity::raycast(&ent))
 				{
-					if (entity::raycast(&ent))
-					{
-						if (ENTITY::IS_ENTITY_A_PED(ent) && PED::IS_PED_A_PLAYER(ent))
-						{
-							ent = 0;
-
-							notify::above_map("You can't move player entities!");
-						}
-						else
-						{
-							other = ENTITY::GET_ENTITY_COORDS(ent, true);
-							dist = (float)math::distance_between_vectors(location, other);
-
-							if (dist > 500)
-							{
-								ent = 0;
-
-								notify::above_map("Entity is too far.");
-							}
-							else
-							{
-								entity::take_control_of(ent);
-
-								if (ENTITY::IS_ENTITY_A_PED(ent) && !PED::IS_PED_RAGDOLL(ent)) TASK::SET_HIGH_FALL_TASK(ent, 0, 0, 0);
-
-								notify::above_map("Selected entity at crosshair.");
-							}
-						}
-					}
-					else
+					if (ENTITY::IS_ENTITY_A_PED(ent) && PED::IS_PED_A_PLAYER(ent))
 					{
 						ent = 0;
 
-						notify::above_map("No entity found.");
+						notify::above_map("You can't move player entities!");
+					}
+					else
+					{
+						other = ENTITY::GET_ENTITY_COORDS(ent, true);
+						dist = (float)math::distance_between_vectors(location, other);
+
+						if (dist > 500)
+						{
+							ent = 0;
+
+							notify::above_map("Entity is too far.");
+						}
+						else
+						{
+							entity::take_control_of(ent);
+
+							if (ENTITY::IS_ENTITY_A_PED(ent) && !PED::IS_PED_RAGDOLL(ent)) TASK::SET_HIGH_FALL_TASK(ent, 0, 0, 0);
+
+							notify::above_map("Selected entity at crosshair.");
+						}
 					}
 				}
-
-				if (ENTITY::DOES_ENTITY_EXIST(ent))
+				else
 				{
-					if (PAD::IS_DISABLED_CONTROL_PRESSED(0, 14))
-						dist -= 5;
-					if (PAD::IS_DISABLED_CONTROL_PRESSED(0, 15))
-						dist += 5;
+					ent = 0;
 
-					entity::take_control_of(ent);
-
-					ENTITY::SET_ENTITY_COLLISION(ent, false, false);
-
-					other = ENTITY::GET_ENTITY_COORDS(ent, true);
-
-					Vector3 rot = CAM::GET_GAMEPLAY_CAM_ROT(2);
-					float pitch = math::deg_to_rad(rot.x); // vertical
-					// float roll = rot.y;
-					float yaw = math::deg_to_rad(rot.z + 90); // horizontal
-
-					Vector3 velocity;
-
-					velocity.x = location.x + (dist * cos(pitch) * cos(yaw)) - other.x;
-					velocity.y = location.y + (dist * sin(yaw) * cos(pitch)) - other.y;
-					velocity.z = location.z + (dist * sin(pitch)) - other.z;
-
-					ENTITY::SET_ENTITY_VELOCITY(ent, velocity.x * (float)multiplier, velocity.y * (float)multiplier, velocity.z * (float)multiplier);
-					ENTITY::SET_ENTITY_ALPHA(ent, 105, 0);
+					notify::above_map("No entity found.");
 				}
 			}
-			else if (ent != 0)
+
+			if (ENTITY::DOES_ENTITY_EXIST(ent))
 			{
+				if (PAD::IS_DISABLED_CONTROL_PRESSED(0, 14))
+					dist -= 5;
+				if (PAD::IS_DISABLED_CONTROL_PRESSED(0, 15))
+					dist += 5;
+
 				entity::take_control_of(ent);
 
-				ENTITY::SET_ENTITY_COLLISION(ent, true, true);
-				ENTITY::SET_ENTITY_ALPHA(ent, 255, 0);
+				ENTITY::SET_ENTITY_COLLISION(ent, false, false);
 
-				ent = 0;
+				other = ENTITY::GET_ENTITY_COORDS(ent, true);
 
-				notify::above_map("Released entity.");
+				Vector3 rot = CAM::GET_GAMEPLAY_CAM_ROT(2);
+				float pitch = math::deg_to_rad(rot.x); // vertical
+				// float roll = rot.y;
+				float yaw = math::deg_to_rad(rot.z + 90); // horizontal
+
+				Vector3 velocity;
+
+				velocity.x = location.x + (dist * cos(pitch) * cos(yaw)) - other.x;
+				velocity.y = location.y + (dist * sin(yaw) * cos(pitch)) - other.y;
+				velocity.z = location.z + (dist * sin(pitch)) - other.z;
+
+				ENTITY::SET_ENTITY_VELOCITY(ent, velocity.x * (float)multiplier, velocity.y * (float)multiplier, velocity.z * (float)multiplier);
+				ENTITY::SET_ENTITY_ALPHA(ent, 105, 0);
 			}
+		}
+		else if (ent != 0 && entity::take_control_of(ent))
+		{
+			ENTITY::SET_ENTITY_COLLISION(ent, true, true);
+			ENTITY::RESET_ENTITY_ALPHA(ent);
+
+			ent = 0;
+
+			notify::above_map("Released entity.");
 		}
 	}
 }
