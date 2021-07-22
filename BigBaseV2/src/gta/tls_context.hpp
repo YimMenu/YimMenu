ad99272383a3952c8711d1920609958d78fd71d3
Yimura <andreas.maerten@scarlet.be>
2021-07-23 00:46:33 +0200

    fix(Invoker): TLS Context size changed

--- BigBaseV2/src/gta/tls_context.hpp
+++ BigBaseV2/src/gta/tls_context.hpp
@@ -7,17 +7,17 @@ namespace rage
 	class tlsContext
 	{
 	public:
-		char m_padding1[0xC8];          // 0x00
-		sysMemAllocator *m_allocator;   // 0xC8
-		char m_padding2[0x760];         // 0xD0
-		scrThread *m_script_thread;     // 0x830
-		bool m_is_script_thread_active; // 0x838
+		char m_padding1[0xC8]; // 0x00
+		sysMemAllocator* m_allocator; // 0xC8
+		char m_padding2[0x758]; // 0xD0
+		scrThread* m_script_thread; // 0x828
+		bool m_is_script_thread_active; // 0x830
 
-		static tlsContext *get()
+		static tlsContext* get()
 		{
 			return *reinterpret_cast<tlsContext**>(__readgsqword(0x58));
 		}
 	};
 
-	static_assert(sizeof(tlsContext) == 0x840);
+	static_assert(sizeof(tlsContext) == 0x838);
 }
