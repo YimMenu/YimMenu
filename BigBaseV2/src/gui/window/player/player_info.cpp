80eda87590d76b5557676e4769c40338b1ad07fd
Yimura <andreas.maerten@scarlet.be>
2021-07-28 00:31:59 +0200

    feat(PlayerInfo): Added R* ID and IP

--- BigBaseV2/src/gui/window/player/player_info.cpp
+++ BigBaseV2/src/gui/window/player/player_info.cpp
@@ -13,6 +13,11 @@ namespace big
 			ImGui::Text("Player ID: %d", g.selected_player.id);
 			ImGui::Text("Session Host: %s", g.selected_player.net_player->is_host() ? "Yes" : "No");
 
+			rage::netPlayerData* net_data = g.selected_player.net_player->get_net_data();
+
+			ImGui::Text("Rockstar ID: %d", net_data->m_rockstar_id);
+			ImGui::Text("IP Address: %d.%d.%d.%d:%d", net_data->m_online_ip.m_field1, net_data->m_online_ip.m_field2, net_data->m_online_ip.m_field3, net_data->m_online_ip.m_field4, net_data->m_online_port);
+
 			ImGui::EndTabItem();
 		}
 	}
