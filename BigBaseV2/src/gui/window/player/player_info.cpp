996bf1a67814ed60f45b4e6871cbdb952ba632aa
Yimura <andreas.maerten@scarlet.be>
2021-08-05 01:26:03 +0200

    feat(PlayerInfo): Check if net_player is safe to use

--- BigBaseV2/src/gui/window/player/player_info.cpp
+++ BigBaseV2/src/gui/window/player/player_info.cpp
@@ -11,12 +11,16 @@ namespace big
 			ImGui::Separator();
 
 			ImGui::Text("Player ID: %d", g.selected_player.id);
-			ImGui::Text("Session Host: %s", g.selected_player.net_player->is_host() ? "Yes" : "No");
 
-			rage::netPlayerData* net_data = g.selected_player.net_player->get_net_data();
+			if (g.selected_player.net_player)
+			{
+				ImGui::Text("Session Host: %s", g.selected_player.net_player->is_host() ? "Yes" : "No");
 
-			ImGui::Text("Rockstar ID: %d", net_data->m_rockstar_id);
-			ImGui::Text("IP Address: %d.%d.%d.%d:%d", net_data->m_online_ip.m_field1, net_data->m_online_ip.m_field2, net_data->m_online_ip.m_field3, net_data->m_online_ip.m_field4, net_data->m_online_port);
+				rage::netPlayerData* net_data = g.selected_player.net_player->get_net_data();
+
+				ImGui::Text("Rockstar ID: %d", net_data->m_rockstar_id);
+				ImGui::Text("IP Address: %d.%d.%d.%d:%d", net_data->m_online_ip.m_field1, net_data->m_online_ip.m_field2, net_data->m_online_ip.m_field3, net_data->m_online_ip.m_field4, net_data->m_online_port);
+			}
 
 			ImGui::EndTabItem();
 		}
