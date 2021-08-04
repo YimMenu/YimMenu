ca389bb4cd5ab31a620c865b6ba1dabdee41ab2e
Yimura <andreas.maerten@scarlet.be>
2021-08-04 22:16:58 +0200

    feat(TopBar): Added Player logged in name & removed unused code

--- BigBaseV2/src/gui/window/window_top_bar.cpp
+++ BigBaseV2/src/gui/window/window_top_bar.cpp
@@ -13,26 +13,7 @@ namespace big
 			if (ImGui::BeginMenu("Info"))
 			{
 				ImGui::MenuItem("Logged in as:", NULL, false, false);
-				ImGui::MenuItem("unknown", NULL, false, false);
-
-				if (ImGui::MenuItem("Am I lobby host?"))
-				{
-					QUEUE_JOB_BEGIN_CLAUSE()
-					{
-						notify::above_map(NETWORK::NETWORK_IS_HOST() ? "~g~Yes you are the host." : "You aren't the host.");
-					}
-					QUEUE_JOB_END_CLAUSE
-
-				}
-
-				if (ImGui::MenuItem("Flagged Account?"))
-				{
-					QUEUE_JOB_BEGIN_CLAUSE()
-					{
-						notify::above_map(NETWORK::NETWORK_PLAYER_IS_BADSPORT() ? "You have been ~r~reported multiple times!" : "Your account is in good standing.");
-					}
-					QUEUE_JOB_END_CLAUSE
-				}
+				ImGui::MenuItem(g_local_player == nullptr || g_local_player->m_player_info == nullptr ? "unknown" : g_local_player->m_player_info->m_name, NULL, false, false);
 
 				ImGui::EndMenu();
 			}
@@ -62,7 +43,7 @@ namespace big
 
 			if (ImGui::BeginMenu("Quit"))
 			{
-				if (ImGui::MenuItem("Unload Menu (may crash)"))
+				if (ImGui::MenuItem("Unload Menu"))
 				{
 					g_running = false;
 				}
