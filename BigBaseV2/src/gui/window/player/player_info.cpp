5467abfa911c92859fd1109f32aed5a2f5a8d060
Yimura <andreas.maerten@scarlet.be>
2021-09-21 18:10:58 +0200

    fix(PlayerInfo): IP address offsets

--- BigBaseV2/src/gui/window/player/player_info.cpp
+++ BigBaseV2/src/gui/window/player/player_info.cpp
@@ -12,14 +12,13 @@ namespace big
 
 			ImGui::Text("Player ID: %d", g.selected_player.id);
 
-			if (g.selected_player.net_player)
+			if (g.selected_player.net_player != nullptr)
 			{
 				ImGui::Text("Session Host: %s", g.selected_player.net_player->is_host() ? "Yes" : "No");
+				CPlayerInfo* player_info = g.selected_player.net_player->player_info;
 
-				rage::netPlayerData* net_data = g.selected_player.net_player->get_net_data();
-
-				ImGui::Text("Rockstar ID: %d", net_data->m_rockstar_id);
-				ImGui::Text("IP Address: %d.%d.%d.%d:%d", net_data->m_online_ip.m_field1, net_data->m_online_ip.m_field2, net_data->m_online_ip.m_field3, net_data->m_online_ip.m_field4, net_data->m_online_port);
+				ImGui::Text("Rockstar ID: %d", player_info->m_rockstar_id);
+				ImGui::Text("IP Address: %d.%d.%d.%d:%d", player_info->m_external_ip.m_field1, player_info->m_external_ip.m_field2, player_info->m_external_ip.m_field3, player_info->m_external_ip.m_field4, player_info->m_external_port);
 			}
 
 			ImGui::EndTabItem();
