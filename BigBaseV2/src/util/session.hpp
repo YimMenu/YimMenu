1d210e80200a42c56272fe0c33c356e00c5ee0fe
iRevolutionDev <tavinho.daniel@gmail.com>
2021-12-15 16:25:10 -0300

    Update 1.58
    feat(Globals): Updated phone calls
    feat(Globals): Updated session

--- BigBaseV2/src/util/session.hpp
+++ BigBaseV2/src/util/session.hpp
@@ -8,12 +8,12 @@ namespace big::session
 	void join_type(SessionType session)
 	{
 		if (session.id == eSessionType::LEAVE_ONLINE)
-			*script_global(1574587).at(2).as<int*>() = -1;
+			*script_global(1312443).at(2).as<int*>() = -1;
 		else
-			*script_global(1575004).as<int*>() = (int)session.id;
+			*script_global(1312860).as<int*>() = (int)session.id;
 
-		*script_global(1574587).as<int*>() = 1;
+		*script_global(1312443).as<int*>() = 1;
 		script::get_current()->yield(200ms);
-		*script_global(1574587).as<int*>() = 0;
+		*script_global(1312443).as<int*>() = 0;
 	}
 }
