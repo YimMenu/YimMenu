5a68bcb0e3608a4a881f272ffe2017d41536c8a5
karifeld <96363819+karifeld@users.noreply.github.com>
2021-12-19 14:02:38 +0800

    feat(Self): Better no ragdoll

--- BigBaseV2/src/backend/looped/self/no_ragdoll.cpp
+++ BigBaseV2/src/backend/looped/self/no_ragdoll.cpp
@@ -3,19 +3,10 @@
 
 namespace big
 {
-	static bool bLastNoRagdoll = false;
-
 	void looped::self_no_ragdoll()
 	{
-		bool bNoRagdoll = g.self.no_ragdoll;
 		Ped player = PLAYER::PLAYER_PED_ID();
 
-		if ((bNoRagdoll && PED::IS_PED_RUNNING_RAGDOLL_TASK(player)) || (!bNoRagdoll && bNoRagdoll != bLastNoRagdoll))
-		{
-			if (ENTITY::GET_ENTITY_HEIGHT_ABOVE_GROUND(player) < 1.0)
-				TASK::CLEAR_PED_TASKS_IMMEDIATELY(player);
-
-			bLastNoRagdoll = bNoRagdoll;
-		}
+		PED::SET_PED_CAN_RAGDOLL(player, !g.self.no_ragdoll);
 	}
 }
