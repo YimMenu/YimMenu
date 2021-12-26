31907153b2e912b0a9a3a421afc2f260a6a8b9eb
Gibson Smith <imnotgibson@gmail.com>
2021-12-26 09:27:25 -0500

    added tp to waypoint hotkey, fixed unlimited ammo

--- BigBaseV2/src/features.cpp
+++ BigBaseV2/src/features.cpp
@@ -12,6 +12,9 @@ namespace big
 {
 	void features::run_tick()
 	{
+		if (GetAsyncKeyState(VK_F12)) {
+			teleport::to_waypoint();
+		}
 		backend::loop();
 	}
 	float features::max_vehicle_torque = 5.f;
@@ -22,14 +25,24 @@ namespace big
 
 	void features::unlimited_ammo(Ped ped)
 	{
-
 		if (features::unlimited_ammo_bool)
 		{
-			WEAPON::SET_PED_INFINITE_AMMO_CLIP(ped, true);
-		}
-		else
-		{
-			WEAPON::SET_PED_INFINITE_AMMO_CLIP(ped, false);
+			Hash cur;
+			if (WEAPON::GET_CURRENT_PED_WEAPON(PLAYER::PLAYER_PED_ID(), &cur, 1))
+			{
+				if (WEAPON::IS_WEAPON_VALID(cur))
+				{
+					int maxAmmo;
+					if (WEAPON::GET_MAX_AMMO(PLAYER::PLAYER_PED_ID(), cur, &maxAmmo))
+					{
+						WEAPON::SET_PED_AMMO(PLAYER::PLAYER_PED_ID(), cur, maxAmmo, true);
+
+						maxAmmo = WEAPON::GET_MAX_AMMO_IN_CLIP(PLAYER::PLAYER_PED_ID(), cur, 1);
+						if (maxAmmo > 0)
+							WEAPON::SET_AMMO_IN_CLIP(PLAYER::PLAYER_PED_ID(), cur, maxAmmo);
+					}
+				}
+			}
 		}
 
 	}
