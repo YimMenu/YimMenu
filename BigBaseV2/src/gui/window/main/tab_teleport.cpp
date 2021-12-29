15baa946cf8980ab66dec3538362e2bf5bad52c2
Kaki <volker.schlegel@tutanota.com>
2021-12-29 11:48:53 +0100

    feat(Teleport): Teleport to objective

--- BigBaseV2/src/gui/window/main/tab_teleport.cpp
+++ BigBaseV2/src/gui/window/main/tab_teleport.cpp
@@ -16,6 +16,26 @@ namespace big
 				}QUEUE_JOB_END_CLAUSE
 			}
 
+			if (ImGui::Button("Waypoint"))
+			{
+				QUEUE_JOB_BEGIN_CLAUSE()
+				{
+					teleport::to_blip(1, 5);
+					teleport::to_blip(57, 5);
+					teleport::to_blip(128, 5);
+					teleport::to_blip(129, 5);
+					teleport::to_blip(130, 5);
+					teleport::to_blip(143, 5);
+					teleport::to_blip(144, 5);
+					teleport::to_blip(145, 5);
+					teleport::to_blip(146, 5);
+					teleport::to_blip(271, 5);
+					teleport::to_blip(286, 5);
+					teleport::to_blip(287, 5);
+					teleport::to_blip(288, 5);
+				}QUEUE_JOB_END_CLAUSE
+			}
+
 			ImGui::EndTabItem();
 		}
 	}
