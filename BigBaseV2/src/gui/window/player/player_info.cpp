c82d25b24a3c1e8d36018796eddf8ebc0e0e732c
The-Denno <97184968+The-Denno@users.noreply.github.com>
2022-01-05 18:31:09 +0100

    Update player_info.cpp
    feat(GUI): added player ped in godmode vehicle detection in player_info.cpp

--- BigBaseV2/src/gui/window/player/player_info.cpp
+++ BigBaseV2/src/gui/window/player/player_info.cpp
@@ -20,6 +20,16 @@ namespace big
 					netPlayerData& netData = player_info->m_net_player_data;
 
 					ImGui::Text("Session Host: %s", net_player->is_host() ? "Yes" : "No");
+					
+					//Check if the player ped is in a vehicle
+					if (player_info->m_ped->m_vehicle != nullptr) 
+					{
+						ImGui::Text("Car GodMode: %s", player_info->m_ped->m_vehicle->m_godmode ? "Yes" : "No");
+					}
+					else 
+					{
+					    ImGui::Text("Car GodMode: %s", "No Car Detected");
+					}
 
 					ImGui::Text("Rockstar ID: %d", netData.m_rockstar_id);
 					ImGui::Text("IP Address: %d.%d.%d.%d:%d", netData.m_external_ip.m_field1, netData.m_external_ip.m_field2, netData.m_external_ip.m_field3, netData.m_external_ip.m_field4, netData.m_external_port);
