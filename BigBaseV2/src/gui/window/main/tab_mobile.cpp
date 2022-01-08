0167556de5545dc34c22172fabe5464ce832111c
Yimura <andreas.maerten@scarlet.be>
2022-01-08 17:48:46 +0100

    feat(PersonalVehicles): Alphabetic list

--- BigBaseV2/src/gui/window/main/tab_mobile.cpp
+++ BigBaseV2/src/gui/window/main/tab_mobile.cpp
@@ -53,16 +53,17 @@ namespace big
 				{
 					for (auto& it : g_mobile_service->m_personal_vehicles)
 					{
+						std::string label = it.first;
 						auto& personal_veh = it.second;
 
-						std::string lower = personal_veh.get_display_name().c_str();
+						std::string lower = label.c_str();
 						std::transform(lower.begin(), lower.end(), lower.begin(), tolower);
 
 						if (lower.find(lower_search) != std::string::npos)
 						{
 							if (ImGui::Selectable(
-								personal_veh.get_display_name().c_str(),
-								personal_veh.get_id() == mobile::util::get_current_personal_vehicle()
+								label.c_str(),
+								personal_veh->get_id() == mobile::util::get_current_personal_vehicle()
 							))
 							{
 								strcpy(search, "");
@@ -70,7 +71,7 @@ namespace big
 
 								QUEUE_JOB_BEGIN_CLAUSE(&personal_veh)
 								{
-									personal_veh.summon();
+									personal_veh->summon();
 								}QUEUE_JOB_END_CLAUSE
 							}
 						}
