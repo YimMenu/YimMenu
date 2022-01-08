cc39b5060f1deb28471c6a467ccd37b7c823abe8
Yimura <andreas.maerten@scarlet.be>
2022-01-08 20:30:13 +0100

    fix(Globals): Globals created at runtime need their cache built

--- BigBaseV2/src/gui/window/dbg/debug_globals.cpp
+++ BigBaseV2/src/gui/window/dbg/debug_globals.cpp
@@ -30,28 +30,28 @@ namespace big
 				ImGui::Separator();
 
 				char label[64];
-				sprintf(label, "Freeze###freeze_%d", global.m_base_address);
+				sprintf(label, "Freeze###freeze_%d", global.get_id());
 				ImGui::Checkbox(label, &global.m_freeze);
 				ImGui::SameLine();
 
 				ImGui::Text(global.m_name.c_str());
 				ImGui::SameLine();
 
-				sprintf(label, "###input_%d", global.m_base_address);
+				sprintf(label, "###input_%d", global.get_id());
 				ImGui::PushItemWidth(200.f);
 				ImGui::InputInt(label, global.get());
 				ImGui::PopItemWidth();
 
-				sprintf(label, "Write###btn_%d", global.m_base_address);
+				sprintf(label, "Write###btn_%d", global.get_id());
 				if (ImGui::Button(label))
 					global.write();
 
 				ImGui::SameLine();
-				sprintf(label, "Delete##delet_%d", global.m_base_address);
+				sprintf(label, "Delete##delete_%d", global.get_id());
 				if (ImGui::Button(label))
 				{
 					for (int i = 0; i < g_globals_service->m_globals.size(); i++)
-						if (const auto& it = g_globals_service->m_globals.at(i); it.m_base_address == global.m_base_address && it.m_name == global.m_name)
+						if (auto& it = g_globals_service->m_globals.at(i); it.get_id() == global.get_id())
 							g_globals_service->m_globals.erase(g_globals_service->m_globals.begin() + i);
 
 					break;
