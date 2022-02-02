d51391172bd5698775e8d3258418fe12a3c0361f
Yimura <andreas.maerten@scarlet.be>
2022-02-02 13:32:53 +0100

    fix(PlayerWindow): Incorrect method for ID setting

--- BigBaseV2/src/gui/window/window_player.cpp
+++ BigBaseV2/src/gui/window/window_player.cpp
@@ -6,7 +6,7 @@ namespace big
 {
 	void window::w_player()
 	{
-		std::string title = fmt::format("Player Options: {}##player_options", g_player_service->get_selected()->get_name());
+		std::string title = fmt::format("Player Options: {}###player_options", g_player_service->get_selected()->get_name());
 
 		ImGui::SetNextWindowSize({ 350.f, 300.f }, ImGuiCond_FirstUseEver);
 		if (g.window.player && g_player_service->get_selected()->is_valid() && ImGui::Begin(title.c_str(), &g.window.player))
