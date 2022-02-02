aa71daec24549d896f9b8801f7c0e0a25ca63581
Yimura <andreas.maerten@scarlet.be>
2022-02-02 13:05:04 +0100

    feat(Hooks): Don't log player join/leave

--- BigBaseV2/src/hooks/player_join.cpp
+++ BigBaseV2/src/hooks/player_join.cpp
@@ -5,8 +5,6 @@ namespace big
 {
 	void hooks::player_join(CNetworkObjectMgr* _this, CNetGamePlayer* net_player)
 	{
-		LOG(INFO) << "Player '" << net_player->get_name() << "' joined taking slot #" << (int)net_player->m_player_id;
-
 		g_player_service->player_join(net_player);
 
 		return g_hooking->m_player_has_joined_hook.get_original<decltype(&hooks::player_join)>()(_this, net_player);
--- BigBaseV2/src/hooks/player_left.cpp
+++ BigBaseV2/src/hooks/player_left.cpp
@@ -5,8 +5,6 @@ namespace big
 {
 	void hooks::player_leave(CNetworkObjectMgr* _this, CNetGamePlayer* net_player)
 	{
-		LOG(INFO) << "Player '" << net_player->get_name() << "' left freeing slot #" << (int)net_player->m_player_id;
-
 		g_player_service->player_leave(net_player);
 
 		return g_hooking->m_player_has_left_hook.get_original<decltype(&hooks::player_leave)>()(_this, net_player);
