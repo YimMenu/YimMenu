d82c50f07b8753c1c618a50c339eec9317d65ddb
LiamD-Flop <contact@liamd.me>
2022-02-28 22:50:58 +0100

    rework(GUI): Optimized animation

--- BigBaseV2/src/views/view_active_view.cpp
+++ BigBaseV2/src/views/view_active_view.cpp
@@ -5,26 +5,23 @@ namespace big
 	void view::active_view() {
 		static float tabs_open_animation = -(float)g->window.x * 0.15f;
 		static float alpha = 0.f;
+
+		const float max_position = (float)g->window.x * 0.15f;
+
 		if (g->window.switched_view) {
 			g->window.switched_view = false;
 			window_animator.reset();
 		}
-		window_animator.animate(600, [](const float& progress)
+		window_animator.animate(600, [max_position](const float& progress)
 		{
 			alpha = progress;
 			switch (current_tab->tab)
 			{
 			case tabs::NONE:
-				if (tabs_open_animation >= (float)g->window.x * 0.15f)
-					tabs_open_animation = (float)g->window.x * 0.15f;
-				else
-					tabs_open_animation = (((float)g->window.x * 0.30f) * progress - (float)g->window.x * 0.15f);
+				tabs_open_animation = tabs_open_animation <= -max_position ? -max_position : max_position - (((float)g->window.x * 0.30f) * progress);
 				break;
 			default:
-				if (tabs_open_animation <= -(float)g->window.x * 0.15f)
-					tabs_open_animation = -(float)g->window.x * 0.15f;
-				else
-					tabs_open_animation = (float)g->window.x * 0.15f - (((float)g->window.x * 0.30f) * progress);
+				tabs_open_animation = tabs_open_animation >= max_position ? max_position : (((float)g->window.x * 0.30f) * progress) - max_position;
 				break;
 			}
 		});
