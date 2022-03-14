7c4e85f5cb76af5eedc49410f795648a61589adb
LiamD-Flop <contact@liamd.me>
2022-03-14 20:51:12 +0100

    feat(CTX MENU): Little cleaning up

--- BigBaseV2/src/services/context_menu_service.hpp
+++ BigBaseV2/src/services/context_menu_service.hpp
@@ -4,11 +4,27 @@
 
 namespace big
 {
+	enum class ContextEntityType
+	{
+		PED,
+		PLAYER,
+		VEHICLE,
+		OBJECT,
+		SHARED
+	};
 
 	struct context_option
 	{
 		std::string name;
 		std::function<void()> command;
+	};
+
+	struct s_context_menu
+	{
+		ContextEntityType type;
+		int current_option = 0;
+		ImVec2 menu_size = {};
+		std::vector<context_option> options;
 	};
 
 	class context_menu_service final
@@ -20,28 +36,59 @@ namespace big
 
 		bool enabled = false;
 		void get_entity_closest_to_screen_center();
-
-		int current_option = 0;
+		void load_shared();
+		s_context_menu* get_context_menu();
 
 		Entity handle;
 		rage::fwEntity* pointer;
 
-		std::vector<context_option> options = {
-			{"EXPLODE", [this] {
-				rage::fvector3 pos = this->pointer->m_navigation->m_position;
-				FIRE::ADD_EXPLOSION(pos.x, pos.y, pos.z, 1, 1000, 1, 0, 1, 0);
-				}},
-			{"DELETE", [this] {
-				entity::delete_entity(this->handle);
-				}}
-		};
-	};
+		s_context_menu vehicle_menu{
+			ContextEntityType::VEHICLE,
+			0,{}, {
+			{"KILL ENGINE", [this]
+			{
+				VEHICLE::SET_VEHICLE_ENGINE_HEALTH(this->handle, 0.f);
+				VEHICLE::SET_VEHICLE_ENGINE_ON(this->handle, false, true, false);
+			}}
+		} };
 
-	enum class ContextEntityType
-	{
-		PED,
-		PLAYER,
-		VEHICLE
+		s_context_menu ped_menu{
+			ContextEntityType::PED,
+			0,{}, {}};
+
+		s_context_menu shared_menu{
+			ContextEntityType::SHARED,
+			0,
+			{}, {
+				{"EXPLODE", [this] {
+					rage::fvector3 pos = this->pointer->m_navigation->m_position;
+					FIRE::ADD_EXPLOSION(pos.x, pos.y, pos.z, 1, 1000, 1, 0, 1, 0);
+					}},
+				{"DELETE", [this] {
+					entity::delete_entity(this->handle);
+					}},
+				{"DELETE", [this] {
+					entity::delete_entity(this->handle);
+					}},
+				{"DELETE", [this] {
+					entity::delete_entity(this->handle);
+					}},
+				{"DELETE", [this] {
+					entity::delete_entity(this->handle);
+					}},
+				{"DELETE", [this] {
+					entity::delete_entity(this->handle);
+					}},
+			}
+		};
+
+		std::unordered_map<ContextEntityType, s_context_menu> options = {
+			{ContextEntityType::VEHICLE, vehicle_menu},
+			{ContextEntityType::PED, ped_menu},
+			{ContextEntityType::SHARED, shared_menu},
+		};
+
+		
 	};
 
 	inline context_menu_service* g_context_menu_service{};
--- BigBaseV2/src/views/esp/view_esp.cpp
+++ BigBaseV2/src/views/esp/view_esp.cpp
@@ -32,7 +32,9 @@ namespace big
 		{
 			ImDrawList* draw_list = ImGui::GetWindowDrawList();
 
-			if (g_context_menu_service->enabled)
+			if (g_context_menu_service->enabled &&
+				g_context_menu_service->pointer &&
+				g_context_menu_service->pointer->m_navigation)
 			{
 				float context_screen_x;
 				float context_screen_y;
@@ -43,12 +45,23 @@ namespace big
 				const float context_target_multplr = context_target_distance > g->esp.global_render_distance[1] ? -1.f : 6.17757f / context_target_distance;
 
 				if (g_pointers->m_get_screen_coords_for_world_coords(context_target_pos.data, &context_screen_x, &context_screen_y))
-
-				for (int i = 0; i < g_context_menu_service->options.size(); i++)
 				{
-					context_option co = g_context_menu_service->options.at(i);
-					draw_list->AddText({ (float)g->window.x * context_screen_x + (67.5f * context_target_multplr), (float)g->window.y * context_screen_y - (175.f * context_target_multplr) + (20.f * i) + 20.f }, g_context_menu_service->current_option == i ? g->esp.friend_color : g->esp.color, co.name.c_str());
+					s_context_menu* cm = g_context_menu_service->get_context_menu();
+
+					float cm_start_x = (float)g->window.x * context_screen_x + (67.5f * context_target_multplr);
+					float cm_start_y = (float)g->window.y * context_screen_y - (175.f * context_target_multplr);
+
+					ImU32 cm_col = ImGui::ColorConvertFloat4ToU32({ 0.549f, 0.639f, 0.710f, 0.3f });
+
+					draw_list->AddRectFilled({ cm_start_x - 2.f , cm_start_y }, { cm_start_x + 2.f + cm->menu_size.x, cm_start_y + cm->menu_size.y }, cm_col, 5.f);
+
+					for (int i = 0; i < cm->options.size(); i++)
+					{
+						context_option co = cm->options.at(i);
+						draw_list->AddText({ cm_start_x + 7.f, cm_start_y + (20.f * (float)i) + 5.f }, cm->current_option == i ? ImGui::ColorConvertFloat4ToU32({ 0.f, 1.f, 0.f, 1.f }) : ImGui::ColorConvertFloat4ToU32({ 1.f, 1.f, 1.f, 1.f }), co.name.c_str());
+					}
 				}
+				
 			}
 
 			for (auto& item : g_player_service->m_players)
