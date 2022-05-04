0d2cfe10d45b7b9c8a0da7c32d079d15961c6f70
Mystro <83551872+Mystro69@users.noreply.github.com>
2022-05-04 15:55:19 +0300

    (Add) Flying vehicle

--- BigBaseV2/src/util/toxic.hpp
+++ BigBaseV2/src/util/toxic.hpp
@@ -65,6 +65,24 @@ namespace big::toxic
 
 		TASK::CLEAR_PED_TASKS_IMMEDIATELY(target);
 	}
+	
+	inline void flying_vehicle(const Player player)
+	{
+		Entity ent = PLAYER::GET_PLAYER_PED_SCRIPT_INDEX(player);
+
+		if (!PED::IS_PED_IN_ANY_VEHICLE(ent, true))
+			g_notification_service->push_warning("Toxic", "Target player is not in a vehicle.");
+		else {
+			ent = PED::GET_VEHICLE_PED_IS_IN(ent, false);
+
+			Vector3 location = ENTITY::GET_ENTITY_COORDS(PLAYER::PLAYER_PED_ID(), true);
+
+			if (NETWORK::NETWORK_REQUEST_CONTROL_OF_ENTITY(ent))
+				ENTITY::APPLY_FORCE_TO_ENTITY(ent, 1, 0.f, 0.f, 50000.f, 0.f, 0.f, 0.f, 0, 0, 1, 1, 0, 1);
+			else
+				g_notification_service->push_warning("Toxic", "Failed to take control of player vehicle.");
+		}
+	}
 
 	inline void clear_wanted_player(Player target)
 	{
