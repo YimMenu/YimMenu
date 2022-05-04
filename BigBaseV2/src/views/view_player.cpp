dceb6a4a0fd853889dc07b5ee07fe4600ebba519
Mystro <83551872+Mystro69@users.noreply.github.com>
2022-05-04 10:37:17 +0300

    (Add) Kick From Vehicle

--- BigBaseV2/src/views/view_player.cpp
+++ BigBaseV2/src/views/view_player.cpp
@@ -121,6 +121,10 @@ namespace big
 				components::button("Taze", [] {
 					toxic::taze_player(g_player_service->get_selected()->id());
 				});
+				
+				components::button("Kick From Vehicle", [] {
+					toxic::kick_from_vehicle(g_player_service->get_selected()->id());
+				});
 			}
 		}
 	}
