f0330f2277d3fca4542eedc0a629904055ac12b5
Yimura <andreas.maerten@scarlet.be>
2022-05-13 17:19:43 +0200

    refactor: Remove Detected Code (#203)
    * feat(premake): Cleaned up premake file, disabled ImGui asserts and other unused files
    
    * feat(renderer): Don't build default font
    
    * feat(vendor): replace ImGui with GTA V safe ImGui
    
    * revert: feat(System): Added looped feature to disable sigscanner

--- BigBaseV2/src/backend/backend.cpp
+++ BigBaseV2/src/backend/backend.cpp
@@ -10,7 +10,6 @@ namespace big
 	void backend::loop()
 	{
 		g->attempt_save();
-		looped::system_anticheat_bypass();
 		looped::system_self_globals();
 		looped::system_update_pointers();
 
--- premake5.lua
+++ premake5.lua
@@ -1,237 +1,227 @@
 workspace "BigBaseV2"
-  architecture "x64"
-  startproject "BigBaseV2"
+	architecture "x64"
+	startproject "BigBaseV2"
 
-  configurations
-  {
-    "Debug",
-    "Release",
-    "Dist"
-  }
-
-  outputdir = "%{cfg.buildcfg}"
-
-  IncludeDir = {}
-  IncludeDir["fmtlib"] = "vendor/fmtlib/include"
-  IncludeDir["json"] = "vendor/json/single_include"
-  IncludeDir["MinHook"] = "vendor/MinHook/include"
-  IncludeDir["ImGui"] = "vendor/ImGui"
-  IncludeDir["ImGuiImpl"] = "vendor/ImGui/examples"
-  IncludeDir["g3log"] = "vendor/g3log/src"
-  IncludeDir["GTAV_Classes"] = "vendor/GTAV-Classes"
-  
-  CppVersion = "C++17"
-  MsvcToolset = "v143"
-  WindowsSdkVersion = "10.0"
-  
-  function DeclareMSVCOptions()
-    filter "system:windows"
-    staticruntime "Off"
-	floatingpoint "Fast"
-	vectorextensions "AVX2"
-    systemversion (WindowsSdkVersion)
-    toolset (MsvcToolset)
-    cppdialect (CppVersion)
-
-    defines
-    {
-      "_CRT_SECURE_NO_WARNINGS",
-      "NOMINMAX",
-      "WIN32_LEAN_AND_MEAN",
-      "_WIN32_WINNT=0x601" -- Support Windows 7
-    }
-    
-    disablewarnings
-    {
-      "4100", -- C4100: unreferenced formal parameter
-      "4201", -- C4201: nameless struct/union
-      "4307",  -- C4307: integral constant overflow
-      "4996"  -- C4996: deprecated in C++17
-    }
-  end
-  
-  function file_exists(name)
-   local f=io.open(name,"r")
-   if f~=nil then io.close(f) return true else return false end
-  end
-   
-  function DeclareDebugOptions()
-    filter "configurations:Debug"
-      defines { "_DEBUG" }
-      symbols "On"
-    filter "not configurations:Debug"
-      defines { "NDEBUG" }
-  end
-   
-  project "ImGui"
-    location "vendor/%{prj.name}"
-    kind "StaticLib"
-    language "C++"
-
-    targetdir ("bin/lib/" .. outputdir)
-    objdir ("bin/lib/int/" .. outputdir .. "/%{prj.name}")
-    
-    files
-    {
-		"vendor/%{prj.name}/imgui.cpp",
-		"vendor/%{prj.name}/imgui_demo.cpp",
-		"vendor/%{prj.name}/imgui_draw.cpp",
-		"vendor/%{prj.name}/imgui_tables.cpp",
-		"vendor/%{prj.name}/imgui_widgets.cpp",
-		"vendor/%{prj.name}/backends/imgui_impl_dx11.cpp",
-		"vendor/%{prj.name}/backends/imgui_impl_win32.cpp"
-    }
-
-    includedirs
-    {
-		"vendor/%{prj.name}"
-    }
-
-    DeclareMSVCOptions()
-    DeclareDebugOptions()
-
-  project "fmtlib"
-    location "vendor/%{prj.name}"
-    kind "StaticLib"
-    language "C++"
-
-    targetdir ("bin/lib/" .. outputdir)
-    objdir ("bin/lib/int/" .. outputdir .. "/%{prj.name}")
-
-    files
-    {
-      "vendor/%{prj.name}/include/**.h",
-      "vendor/%{prj.name}/src/**.cc"
-    }
-
-    includedirs
-    {
-      "vendor/%{prj.name}/include"
-    }
-
-    DeclareMSVCOptions()
-    DeclareDebugOptions()
-
-  project "MinHook"
-    location "vendor/%{prj.name}"
-    kind "StaticLib"
-    language "C"
-
-    targetdir ("bin/lib/" .. outputdir)
-    objdir ("bin/lib/int/" .. outputdir .. "/%{prj.name}")
-
-    files
-    {
-      "vendor/%{prj.name}/include/**.h",
-      "vendor/%{prj.name}/src/**.h",
-      "vendor/%{prj.name}/src/**.c"
-    }
-
-    DeclareMSVCOptions()
-    DeclareDebugOptions()
-	
-  project "g3log"
-    location "vendor/%{prj.name}"
-    kind "StaticLib"
-    language "C++"
-
-    targetdir ("bin/lib/" .. outputdir)
-    objdir ("bin/lib/int/" .. outputdir .. "/%{prj.name}")
-	
-	includedirs
-    {
-      "vendor/%{prj.name}/src"
-    }
-
-	if(file_exists("vendor\\g3log\\src\\g3log\\generated_definitions.hpp") == false) then
-		file = io.open("vendor\\g3log\\src\\g3log\\generated_definitions.hpp", "w")
-		if(file == nil) then
-			premake.error("Failed to locate vendor directories. Try doing git pull --recurse-submodules.")
-		end
-		file:write("// AUTO GENERATED MACRO DEFINITIONS FOR G3LOG\n\n\n/* ==========================================================================\n*2015 by KjellKod.cc. This is PUBLIC DOMAIN to use at your own risk and comes\n* with no warranties. This code is yours to share, use and modify with no\n\n*strings attached and no restrictions or obligations.\n* \n* For more information see g3log/LICENSE or refer refer to http://unlicense.org\n\n*============================================================================*/\n#pragma once\n\n\n// CMake induced definitions below. See g3log/Options.cmake for details.");
-	end
-	
-    files
-    {
-      "vendor/%{prj.name}/src/**.hpp",
-      "vendor/%{prj.name}/src/**.cpp"
-    }
-	
-	removefiles
+	configurations
 	{
-	  "vendor/%{prj.name}/src/crashhandler_unix.cpp"
+		"Debug",
+		"Release",
+		"Dist"
 	}
 
-    DeclareMSVCOptions()
-    DeclareDebugOptions()
+	outputdir = "%{cfg.buildcfg}"
 
-  project "BigBaseV2"
-    location "BigBaseV2"
-    kind "SharedLib"
-    language "C++"
+	CppVersion = "C++17"
+	MsvcToolset = "v143"
+	WindowsSdkVersion = "10.0"
+  
+	function DeclareMSVCOptions()
+		filter "system:windows"
+		staticruntime "Off"
+		floatingpoint "Fast"
+		systemversion (WindowsSdkVersion)
+		toolset (MsvcToolset)
+		cppdialect (CppVersion)
 
-    targetdir ("bin/" .. outputdir)
-    objdir ("bin/int/" .. outputdir .. "/%{prj.name}")
+		defines
+		{
+			"_CRT_SECURE_NO_WARNINGS",
+			"NOMINMAX",
+			"WIN32_LEAN_AND_MEAN",
+			"_WIN32_WINNT=0x601" -- Support Windows 7
+		}
 
-    PrecompiledHeaderInclude = "common.hpp"
-    PrecompiledHeaderSource = "%{prj.name}/src/common.cpp"
- 
-    files
-    {
-      "%{prj.name}/src/**.hpp",
-      "%{prj.name}/src/**.h",
-      "%{prj.name}/src/**.cpp",
-      "%{prj.name}/src/**.asm"
-    }
+		disablewarnings
+		{
+			"4100", -- C4100: unreferenced formal parameter
+			"4201", -- C4201: nameless struct/union
+			"4307",  -- C4307: integral constant overflow
+			"4996"  -- C4996: deprecated in C++17
+		}
+	end
+  
+	function file_exists(name)
+		local f=io.open(name,"r")
+		if f~=nil then io.close(f) return true else return false end
+	end
+   
+	function DeclareDebugOptions()
+		filter "configurations:Debug"
+		    defines { "_DEBUG" }
+		    symbols "On"
+		filter "not configurations:Debug"
+		    defines { "NDEBUG" }
+	end
 
-    includedirs
-    {
-      "%{IncludeDir.fmtlib}",
-      "%{IncludeDir.json}",
-      "%{IncludeDir.MinHook}",
-      "%{IncludeDir.ImGui}",
-      "%{IncludeDir.ImGuiImpl}",
-      "%{IncludeDir.g3log}",
-      "%{IncludeDir.GTAV_Classes}",
-      "%{prj.name}/src"
-    }
+	project "fmtlib"
+		location "vendor/%{prj.name}"
+		kind "StaticLib"
+		language "C++"
 
-    libdirs
-    {
-      "bin/lib"
-    }
+		targetdir ("bin/lib/" .. outputdir)
+		objdir ("bin/lib/int/" .. outputdir .. "/%{prj.name}")
 
-    links
-    {
-      "fmtlib",
-      "MinHook",
-      "ImGui",
-      "g3log"
-    }
+		files
+		{
+			"vendor/%{prj.name}/include/**.h",
+			"vendor/%{prj.name}/src/**.cc"
+		}
 
-    pchheader "%{PrecompiledHeaderInclude}"
-    pchsource "%{PrecompiledHeaderSource}"
+		includedirs
+		{
+			"vendor/%{prj.name}/include"
+		}
 
-    forceincludes
-    {
-      "%{PrecompiledHeaderInclude}"
-    }
+		DeclareMSVCOptions()
+		DeclareDebugOptions()
+	
+	project "g3log"
+		location "vendor/%{prj.name}"
+		kind "StaticLib"
+		language "C++"
 
-    DeclareMSVCOptions()
-    DeclareDebugOptions()
+		targetdir ("bin/lib/" .. outputdir)
+		objdir ("bin/lib/int/" .. outputdir .. "/%{prj.name}")
+		
+		includedirs
+		{
+		    "vendor/%{prj.name}/src"
+		}
 
-    flags { "NoImportLib", "Maps" }
+		if(file_exists("vendor\\g3log\\src\\g3log\\generated_definitions.hpp") == false) then
+			file = io.open("vendor\\g3log\\src\\g3log\\generated_definitions.hpp", "w")
+			if(file == nil) then
+				premake.error("Failed to locate vendor directories. Try doing git pull --recurse-submodules.")
+			end
+			file:write("#pragma once");
+		end
+		
+		files
+		{
+		    "vendor/%{prj.name}/src/**.hpp",
+		    "vendor/%{prj.name}/src/**.cpp"
+		}
+		
+		removefiles
+		{
+		    "vendor/%{prj.name}/src/crashhandler_unix.cpp"
+		}
 
-    filter "configurations:Debug"
-	  flags { "LinkTimeOptimization", "MultiProcessorCompile" }
-	  editandcontinue "Off"
-      defines { "BIGBASEV2_DEBUG" }
+		DeclareMSVCOptions()
+		DeclareDebugOptions()
+   
+	project "ImGui"
+		location "vendor/%{prj.name}"
+		kind "StaticLib"
+		language "C++"
 
-    filter "configurations:Release"
-	  flags { "LinkTimeOptimization", "NoManifest", "MultiProcessorCompile" }
-      defines { "BIGBASEV2_RELEASE" }
-      optimize "speed"
-    filter "configurations:Dist"
-      flags { "LinkTimeOptimization", "FatalWarnings", "NoManifest", "MultiProcessorCompile" }
-      defines { "BIGBASEV2_DIST" }
-      optimize "speed"
+		targetdir ("bin/lib/" .. outputdir)
+		objdir ("bin/lib/int/" .. outputdir .. "/%{prj.name}")
+		
+		files
+		{
+			"vendor/%{prj.name}/imgui.cpp",
+			"vendor/%{prj.name}/imgui_draw.cpp",
+			"vendor/%{prj.name}/imgui_tables.cpp",
+			"vendor/%{prj.name}/imgui_widgets.cpp",
+			"vendor/%{prj.name}/backends/imgui_impl_dx11.cpp",
+			"vendor/%{prj.name}/backends/imgui_impl_win32.cpp"
+		}
+
+		includedirs
+		{
+			"vendor/%{prj.name}"
+		}
+		
+		defines {
+			"IMGUI_DISABLE_DEMO_WINDOWS",
+			"IMGUI_DISABLE_METRICS_WINDOW",
+			"IM_ASSERT(_EXPR) ((void)(_EXPR))"
+		}
+
+		DeclareMSVCOptions()
+		DeclareDebugOptions()
+
+	project "MinHook"
+		location "vendor/%{prj.name}"
+		kind "StaticLib"
+		language "C"
+
+		targetdir ("bin/lib/" .. outputdir)
+		objdir ("bin/lib/int/" .. outputdir .. "/%{prj.name}")
+
+		files
+		{
+		    "vendor/%{prj.name}/include/**.h",
+		    "vendor/%{prj.name}/src/**.h",
+		    "vendor/%{prj.name}/src/**.c"
+		}
+
+		DeclareMSVCOptions()
+		DeclareDebugOptions()
+
+	project "BigBaseV2"
+		location "BigBaseV2"
+		kind "SharedLib"
+		language "C++"
+
+		targetdir ("bin/" .. outputdir)
+		objdir ("bin/int/" .. outputdir .. "/%{prj.name}")
+	 
+		files
+		{
+		    "%{prj.name}/src/**.hpp",
+		    "%{prj.name}/src/**.h",
+		    "%{prj.name}/src/**.cpp",
+		    "%{prj.name}/src/**.asm"
+		}
+
+		includedirs
+		{
+			"%{prj.name}/src/",
+		    "vendor/fmtlib/include",
+		    "vendor/g3log/src",
+		    "vendor/GTAV-Classes",
+		    "vendor/ImGui",
+		    "vendor/json/single_include",
+		    "vendor/MinHook/include"
+		}
+
+		libdirs
+		{
+		    "bin/lib"
+		}
+
+		links
+		{
+		    "fmtlib",
+		    "g3log",
+		    "ImGui",
+		    "MinHook"
+		}
+
+		pchheader "common.hpp"
+		pchsource "%{prj.name}/src/common.cpp"
+
+		forceincludes { -- microsoft bad
+		    "common.hpp"
+		}
+
+		DeclareMSVCOptions()
+		DeclareDebugOptions()
+
+		flags { "NoImportLib", "Maps", "LinkTimeOptimization", "MultiProcessorCompile" }
+
+		filter "configurations:Debug"
+		    flags {  }
+		    editandcontinue "Off"
+		    defines { "DEBUG" }
+
+		filter "configurations:Release"
+		    flags { "NoManifest" }
+		    defines { "RELEASE" }
+		    optimize "speed"
+		filter "configurations:Dist"
+		    flags { "FatalWarnings", "NoManifest" }
+		    defines { "DIST" }
+		    optimize "speed"
