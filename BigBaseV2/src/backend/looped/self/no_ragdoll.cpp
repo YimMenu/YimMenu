736f7d03794eeef04f38e34fc488c032f6c2c59f
karifeld <rbpdomain@protonmail.com>
2022-05-22 17:42:44 +0800

    refactor(No Ragdoll, Infinite Clip): Only run on boolean change.

--- BigBaseV2/src/backend/looped/self/no_ragdoll.cpp
+++ BigBaseV2/src/backend/looped/self/no_ragdoll.cpp
@@ -3,8 +3,17 @@
 
 namespace big
 {
+	static bool bLastNoRagdoll = false;
+
 	void looped::self_no_ragdoll()
 	{
-		PED::SET_PED_CAN_RAGDOLL(self::ped, !g->self.no_ragdoll);
+		bool bNoRagdoll = g->self.no_ragdoll;
+
+		if (bNoRagdoll || (!bNoRagdoll && bNoRagdoll != bLastNoRagdoll))
+		{
+			PED::SET_PED_CAN_RAGDOLL(self::ped, !g->self.no_ragdoll);
+
+			bLastNoRagdoll = g->self.no_ragdoll;
+		}
 	}
 }
