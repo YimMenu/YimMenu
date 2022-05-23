3248ce08d2ee535847f782efa432b3132514deae
DayibBaba <79384354+DayibBaba@users.noreply.github.com>
2022-05-23 18:51:04 +0200

    Wrapped the scripts ui in popup

--- BigBaseV2/src/views/settings/view_settings.cpp
+++ BigBaseV2/src/views/settings/view_settings.cpp
@@ -5,6 +5,25 @@
 
 namespace big
 {
+
+	void scripts_popupmodal() {
+
+		ImGui::BeginGroup();
+		components::small_text("Scripts");
+		ImGui::SameLine(ImGui::GetWindowWidth() - 100);
+		if (ImGui::Button("Close")) ImGui::CloseCurrentPopup();
+		ImGui::Spacing();
+		components::small_text("These scripts are responsible for all looped features,\nOnly disable if you know what you are doing.");
+
+		for (auto& s : g_script_mgr.get_scripts_data()) {
+			if (ImGui::Checkbox(s->m_name, s->get_enabled_pointer())) g_notification_service->push(std::string(s->m_name).append(" script"), *s->get_enabled_pointer() ? "Resumed" : "Halted");
+		}
+
+		ImGui::EndGroup();
+
+
+	}
+
 	void view::settings() {
 		components::small_text("Hotkeys");
 
@@ -22,15 +41,15 @@ namespace big
 		ImGui::Separator();
 
 
-		ImGui::BeginGroup();
-		components::small_text("Scripts");
-		ImGui::Spacing();
-		components::small_text("These scripts are responsible for all looped features,\nOnly disable if you know what you are doing.");
+		if (ImGui::Button("Manage scripts")) ImGui::OpenPopup("Scripts");
 
-		for (auto& s : g_script_mgr.get_scripts_data()) {
-			if (ImGui::Checkbox(s->m_name, s->get_enabled_pointer())) g_notification_service->push(std::string(s->m_name).append(" script"), *s->get_enabled_pointer() ? "Resumed" : "Halted");
+		ImGui::SetNextWindowSize({ 388,576 }, ImGuiCond_FirstUseEver); ImGui::SetNextWindowPos({ 780,228 }, ImGuiCond_FirstUseEver);
+		if (ImGui::BeginPopupModal("Scripts")) {
+
+			scripts_popupmodal();
+
+			ImGui::EndPopup();
 		}
 		
-		ImGui::EndGroup();
 	}
 }
