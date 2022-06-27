7bfc04bea0d2c65a755eb11184ee8423f0c58842
DayibBaba <79384354+DayibBaba@users.noreply.github.com>
2022-06-27 20:12:45 +0200

    Name and status variables for the Script system. (#244)
    Co-authored-by: Yimura <andreas.maerten@scarlet.be>

--- BigBaseV2/src/main.cpp
+++ BigBaseV2/src/main.cpp
@@ -1,4 +1,4 @@
-﻿#include "common.hpp"
+#include "common.hpp"
 #include "core/globals.hpp"
 #include "features.hpp"
 #include "fiber_pool.hpp"
@@ -31,145 +31,145 @@ BOOL APIENTRY DllMain(HMODULE hmod, DWORD reason, PVOID)
 
 		g_hmodule = hmod;
 		g_main_thread = CreateThread(nullptr, 0, [](PVOID) -> DWORD
-		{
-			while (!FindWindow(L"grcWindow", L"Grand Theft Auto V"))
-				std::this_thread::sleep_for(1s);
-
-			std::filesystem::path base_dir = std::getenv("appdata");
-			base_dir /= "BigBaseV2";
-			auto file_manager_instance = std::make_unique<file_manager>(base_dir);
-
-			auto globals_instance = std::make_unique<menu_settings>(
-				file_manager_instance->get_project_file("./settings.json")
-			);
-
-			auto logger_instance = std::make_unique<logger>(
-				"YimMenu",
-				file_manager_instance->get_project_file("./cout.log")
-			);
-
-			EnableMenuItem(GetSystemMenu(FindWindowA(NULL, "YimMenu"), 0), SC_CLOSE, MF_BYCOMMAND | MF_DISABLED | MF_GRAYED);
-
-			try
 			{
-				LOG(INFO) << "Yim's Menu Initializing";
-				auto pointers_instance = std::make_unique<pointers>();
-				LOG(INFO) << "Pointers initialized.";
+				while (!FindWindow(L"grcWindow", L"Grand Theft Auto V"))
+					std::this_thread::sleep_for(1s);
 
-				auto renderer_instance = std::make_unique<renderer>();
-				LOG(INFO) << "Renderer initialized.";
+				std::filesystem::path base_dir = std::getenv("appdata");
+				base_dir /= "BigBaseV2";
+				auto file_manager_instance = std::make_unique<file_manager>(base_dir);
 
-				auto fiber_pool_instance = std::make_unique<fiber_pool>(11);
-				LOG(INFO) << "Fiber pool initialized.";
+				auto globals_instance = std::make_unique<menu_settings>(
+					file_manager_instance->get_project_file("./settings.json")
+					);
 
-				auto hooking_instance = std::make_unique<hooking>();
-				LOG(INFO) << "Hooking initialized.";
+				auto logger_instance = std::make_unique<logger>(
+					"YimMenu",
+					file_manager_instance->get_project_file("./cout.log")
+					);
 
-				g->load();
-				LOG(INFO) << "Settings Loaded.";
+				EnableMenuItem(GetSystemMenu(FindWindowA(NULL, "YimMenu"), 0), SC_CLOSE, MF_BYCOMMAND | MF_DISABLED | MF_GRAYED);
 
-				auto thread_pool_instance = std::make_unique<thread_pool>();
-				LOG(INFO) << "Thread pool initialized.";
+				try
+				{
+					LOG(INFO) << "Yim's Menu Initializing";
+					auto pointers_instance = std::make_unique<pointers>();
+					LOG(INFO) << "Pointers initialized.";
 
-				auto context_menu_service_instance = std::make_unique<context_menu_service>();
-				auto globals_service_instace = std::make_unique<globals_service>();
-				auto mobile_service_instance = std::make_unique<mobile_service>();
-				auto notification_service_instance = std::make_unique<notification_service>();
-				auto player_service_instance = std::make_unique<player_service>();
-				auto vehicle_preview_service_instance = std::make_unique<vehicle_preview_service>();
-				auto vehicle_service_instance = std::make_unique<vehicle_service>();
-				auto gui_service_instance = std::make_unique<gui_service>();
-				LOG(INFO) << "Registered service instances...";
+					auto renderer_instance = std::make_unique<renderer>();
+					LOG(INFO) << "Renderer initialized.";
 
-				g_script_mgr.add_script(std::make_unique<script>(&features::script_func));
-				g_script_mgr.add_script(std::make_unique<script>(&gui::script_func));
+					auto fiber_pool_instance = std::make_unique<fiber_pool>(11);
+					LOG(INFO) << "Fiber pool initialized.";
 
-				g_script_mgr.add_script(std::make_unique<script>(&backend::self_loop));
-				g_script_mgr.add_script(std::make_unique<script>(&backend::weapons_loop));
-				g_script_mgr.add_script(std::make_unique<script>(&backend::vehicles_loop));
-				g_script_mgr.add_script(std::make_unique<script>(&backend::misc_loop));
-				g_script_mgr.add_script(std::make_unique<script>(&backend::remote_loop));
-				g_script_mgr.add_script(std::make_unique<script>(&backend::noclip_loop));
-				g_script_mgr.add_script(std::make_unique<script>(&backend::lscustoms_loop));
-				g_script_mgr.add_script(std::make_unique<script>(&backend::vehiclefly_loop));
-				g_script_mgr.add_script(std::make_unique<script>(&backend::rgbrandomizer_loop));
-				g_script_mgr.add_script(std::make_unique<script>(&backend::turnsignal_loop));
-				g_script_mgr.add_script(std::make_unique<script>(&backend::disable_control_action_loop));
-				g_script_mgr.add_script(std::make_unique<script>(&context_menu_service::context_menu));
-				LOG(INFO) << "Scripts registered.";
+					auto hooking_instance = std::make_unique<hooking>();
+					LOG(INFO) << "Hooking initialized.";
 
-				auto native_hooks_instance = std::make_unique<native_hooks>();
-				LOG(INFO) << "Dynamic native hooker initialized.";
+					g->load();
+					LOG(INFO) << "Settings Loaded.";
 
-				g_hooking->enable();
-				LOG(INFO) << "Hooking enabled.";
+					auto thread_pool_instance = std::make_unique<thread_pool>();
+					LOG(INFO) << "Thread pool initialized.";
 
-				g_running = true;
+					auto context_menu_service_instance = std::make_unique<context_menu_service>();
+					auto globals_service_instace = std::make_unique<globals_service>();
+					auto mobile_service_instance = std::make_unique<mobile_service>();
+					auto notification_service_instance = std::make_unique<notification_service>();
+					auto player_service_instance = std::make_unique<player_service>();
+					auto vehicle_preview_service_instance = std::make_unique<vehicle_preview_service>();
+					auto vehicle_service_instance = std::make_unique<vehicle_service>();
+					auto gui_service_instance = std::make_unique<gui_service>();
+					LOG(INFO) << "Registered service instances...";
 
-				while (g_running)
-					std::this_thread::sleep_for(500ms);
+					g_script_mgr.add_script(std::make_unique<script>(&features::script_func, "Backend Main", false));
+					g_script_mgr.add_script(std::make_unique<script>(&gui::script_func, "GUI", false));
 
-				g_hooking->disable();
-				LOG(INFO) << "Hooking disabled.";
+					g_script_mgr.add_script(std::make_unique<script>(&backend::self_loop, "Self"));
+					g_script_mgr.add_script(std::make_unique<script>(&backend::weapons_loop, "Weapon"));
+					g_script_mgr.add_script(std::make_unique<script>(&backend::vehicles_loop, "Vehicle"));
+					g_script_mgr.add_script(std::make_unique<script>(&backend::misc_loop, "Miscellaneous"));
+					g_script_mgr.add_script(std::make_unique<script>(&backend::remote_loop, "Remote"));
+					g_script_mgr.add_script(std::make_unique<script>(&backend::noclip_loop, "No Clip"));
+					g_script_mgr.add_script(std::make_unique<script>(&backend::lscustoms_loop, "LS Customs"));
+					g_script_mgr.add_script(std::make_unique<script>(&backend::vehiclefly_loop, "Vehicle Fly"));
+					g_script_mgr.add_script(std::make_unique<script>(&backend::rgbrandomizer_loop, "RGB Randomizer"));
+					g_script_mgr.add_script(std::make_unique<script>(&backend::turnsignal_loop, "Turn Signals"));
+					g_script_mgr.add_script(std::make_unique<script>(&backend::disable_control_action_loop, "Disable Controls"));
+					g_script_mgr.add_script(std::make_unique<script>(&context_menu_service::context_menu, "Context Menu"));
+					LOG(INFO) << "Scripts registered.";
 
-				native_hooks_instance.reset();
-				LOG(INFO) << "Dynamic native hooker uninitialized.";
+					auto native_hooks_instance = std::make_unique<native_hooks>();
+					LOG(INFO) << "Dynamic native hooker initialized.";
 
-				g_script_mgr.remove_all_scripts();
-				LOG(INFO) << "Scripts unregistered.";
+					g_hooking->enable();
+					LOG(INFO) << "Hooking enabled.";
 
-				gui_service_instance.reset();
-				LOG(INFO) << "Gui Service reset.";
-				vehicle_service_instance.reset();
-				LOG(INFO) << "Vehicle Service reset.";
-				vehicle_preview_service_instance.reset();
-				LOG(INFO) << "Vehicle Preview Service reset.";
-				mobile_service_instance.reset();
-				LOG(INFO) << "Mobile Service reset.";
-				player_service_instance.reset();
-				LOG(INFO) << "Player Service reset.";
-				globals_service_instace.reset();
-				LOG(INFO) << "Globals Service reset.";
-				context_menu_service_instance.reset();
-				LOG(INFO) << "Context Service reset.";
-				LOG(INFO) << "Services uninitialized.";
+					g_running = true;
 
-				// Make sure that all threads created don't have any blocking loops
-				// otherwise make sure that they have stopped executing
-				thread_pool_instance->destroy();
-				LOG(INFO) << "Destroyed thread pool.";
+					while (g_running)
+						std::this_thread::sleep_for(500ms);
 
-				thread_pool_instance.reset();
-				LOG(INFO) << "Thread pool uninitialized.";
+					g_hooking->disable();
+					LOG(INFO) << "Hooking disabled.";
 
-				hooking_instance.reset();
-				LOG(INFO) << "Hooking uninitialized.";
+					native_hooks_instance.reset();
+					LOG(INFO) << "Dynamic native hooker uninitialized.";
 
-				fiber_pool_instance.reset();
-				LOG(INFO) << "Fiber pool uninitialized.";
+					g_script_mgr.remove_all_scripts();
+					LOG(INFO) << "Scripts unregistered.";
 
-				renderer_instance.reset();
-				LOG(INFO) << "Renderer uninitialized.";
+					gui_service_instance.reset();
+					LOG(INFO) << "Gui Service reset.";
+					vehicle_service_instance.reset();
+					LOG(INFO) << "Vehicle Service reset.";
+					vehicle_preview_service_instance.reset();
+					LOG(INFO) << "Vehicle Preview Service reset.";
+					mobile_service_instance.reset();
+					LOG(INFO) << "Mobile Service reset.";
+					player_service_instance.reset();
+					LOG(INFO) << "Player Service reset.";
+					globals_service_instace.reset();
+					LOG(INFO) << "Globals Service reset.";
+					context_menu_service_instance.reset();
+					LOG(INFO) << "Context Service reset.";
+					LOG(INFO) << "Services uninitialized.";
 
-				pointers_instance.reset();
-				LOG(INFO) << "Pointers uninitialized.";
-			}
-			catch (std::exception const &ex)
-			{
-				LOG(WARNING) << ex.what();
-			}
+					// Make sure that all threads created don't have any blocking loops
+					// otherwise make sure that they have stopped executing
+					thread_pool_instance->destroy();
+					LOG(INFO) << "Destroyed thread pool.";
 
-			LOG(INFO) << "Farewell!";
-			logger_instance->destroy();
-			logger_instance.reset();
+					thread_pool_instance.reset();
+					LOG(INFO) << "Thread pool uninitialized.";
 
-			globals_instance.reset();
+					hooking_instance.reset();
+					LOG(INFO) << "Hooking uninitialized.";
 
-			file_manager_instance.reset();
+					fiber_pool_instance.reset();
+					LOG(INFO) << "Fiber pool uninitialized.";
 
-			CloseHandle(g_main_thread);
-			FreeLibraryAndExitThread(g_hmodule, 0);
-		}, nullptr, 0, &g_main_thread_id);
+					renderer_instance.reset();
+					LOG(INFO) << "Renderer uninitialized.";
+
+					pointers_instance.reset();
+					LOG(INFO) << "Pointers uninitialized.";
+				}
+				catch (std::exception const& ex)
+				{
+					LOG(WARNING) << ex.what();
+				}
+
+				LOG(INFO) << "Farewell!";
+				logger_instance->destroy();
+				logger_instance.reset();
+
+				globals_instance.reset();
+
+				file_manager_instance.reset();
+
+				CloseHandle(g_main_thread);
+				FreeLibraryAndExitThread(g_hmodule, 0);
+			}, nullptr, 0, &g_main_thread_id);
 	}
 
 	return true;
