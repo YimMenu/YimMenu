034affd0a325be01f7bd0dffe06626a2bd0be810
Yimura <andreas.maerten@scarlet.be>
2022-07-03 01:05:33 +0200

    refactor: hooking restructure (#311)

---
+++ BigBaseV2/src/hooks/gui/swap_chain_present.cpp
@@ -0,0 +1,21 @@
+#include "hooking.hpp"
+#include "renderer.hpp"
+#include "script.hpp"
+
+namespace big
+{
+	HRESULT hooks::swapchain_present(IDXGISwapChain* this_, UINT sync_interval, UINT flags)
+	{
+		TRY_CLAUSE
+		{
+			if (g_running)
+			{
+				g_renderer->on_present();
+			}
+
+			return g_hooking->m_swapchain_hook.get_original<decltype(&swapchain_present)>(swapchain_present_index)(this_, sync_interval, flags);
+		} EXCEPT_CLAUSE
+
+		return NULL;
+	}
+}
---
+++ BigBaseV2/src/hooks/gui/wndproc.cpp
@@ -0,0 +1,21 @@
+#include "hooking.hpp"
+#include "renderer.hpp"
+#include "script.hpp"
+
+namespace big
+{
+	LRESULT hooks::wndproc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
+	{
+		TRY_CLAUSE
+		{
+			if (g_running)
+			{
+				g_renderer->wndproc(hwnd, msg, wparam, lparam);
+			}
+
+			return CallWindowProcW(g_hooking->m_og_wndproc, hwnd, msg, wparam, lparam);
+		} EXCEPT_CLAUSE
+
+		return NULL;
+	}
+}
