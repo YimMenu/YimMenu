5fa1032b4d96053b2609358b114138d911073073
Tom Chan <aa15032261@toms.directory>
2022-07-02 23:08:01 +0800

    Fixed logic in godmode loop.

--- BigBaseV2/src/backend/looped/self/godmode.cpp
+++ BigBaseV2/src/backend/looped/self/godmode.cpp
@@ -3,16 +3,19 @@
 
 namespace big
 {
-	const uint32_t PROOF_MASK = (uint32_t)eEntityProofs::BULLET | 
-		(uint32_t)eEntityProofs::FIRE       | (uint32_t)eEntityProofs::COLLISION  | 
-		(uint32_t)eEntityProofs::MELEE      | (uint32_t)eEntityProofs::GOD        | 
-		(uint32_t)eEntityProofs::EXPLOSION  | (uint32_t)eEntityProofs::STEAM      | 
-		(uint32_t)eEntityProofs::DROWN      | (uint32_t)eEntityProofs::WATER;
-
+	static uint32_t lastProofBits = 0;
 
 	void looped::self_godmode() {
 		if (g_local_player != nullptr) {
-			g_local_player->m_damage_bits = (g_local_player->m_damage_bits & ~PROOF_MASK) | g->self.proof_mask;
+			uint32_t proofBits = g->self.proof_mask;
+			uint32_t changedProofBits = proofBits ^ lastProofBits;
+			uint32_t changedOrEnabledProofBits = proofBits | changedProofBits;
+
+			if (changedOrEnabledProofBits) {
+				uint32_t unchangedBits = g_local_player->m_damage_bits & ~changedOrEnabledProofBits;
+				g_local_player->m_damage_bits = unchangedBits | proofBits;
+				lastProofBits = proofBits;
+			}
 		}
 	}
 }
