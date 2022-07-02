6e621b597300edf3bfbeed0697d7b593eda9c5bf
howdoiusekeyboard <90883505+howdoiusekeyboard@users.noreply.github.com>
2022-07-03 01:34:03 +0530

    Conflicts; Fingers crossed  (#17)

--- BigBaseV2/src/views/self/view_self.cpp
+++ BigBaseV2/src/views/self/view_self.cpp
@@ -1,5 +1,6 @@
 #include "fiber_pool.hpp"
 #include "util/entity.hpp"
+#include "util/player.hpp"
 #include "views/view.hpp"
 
 namespace big
@@ -7,7 +8,7 @@ namespace big
 	void view::self() {
 		components::button("Suicide", [] {
 			ENTITY::SET_ENTITY_HEALTH(self::ped, 0, 0);
-			});
+		});
 
 		ImGui::SameLine();
 
@@ -15,6 +16,20 @@ namespace big
 			ENTITY::SET_ENTITY_HEALTH(self::ped, PED::GET_PED_MAX_HEALTH(self::ped), 0);
 			PED::SET_PED_ARMOUR(self::ped, PLAYER::GET_PLAYER_MAX_ARMOUR(self::id));
 			});
+
+		ImGui::SameLine();
+
+		components::button("Fill Inventory", [] {
+			std::string mpPrefix = player::get_mp_prefix();
+			STATS::STAT_SET_INT(rage::joaat(mpPrefix + "NO_BOUGHT_YUM_SNACKS"), 30, true);
+			STATS::STAT_SET_INT(rage::joaat(mpPrefix + "NO_BOUGHT_HEALTH_SNACKS"), 15, true);
+			STATS::STAT_SET_INT(rage::joaat(mpPrefix + "NO_BOUGHT_EPIC_SNACKS"), 5, true);
+			STATS::STAT_SET_INT(rage::joaat(mpPrefix + "MP_CHAR_ARMOUR_1_COUNT"), 10, true);
+			STATS::STAT_SET_INT(rage::joaat(mpPrefix + "MP_CHAR_ARMOUR_2_COUNT"), 10, true);
+			STATS::STAT_SET_INT(rage::joaat(mpPrefix + "MP_CHAR_ARMOUR_3_COUNT"), 10, true);
+			STATS::STAT_SET_INT(rage::joaat(mpPrefix + "MP_CHAR_ARMOUR_4_COUNT"), 10, true);
+			STATS::STAT_SET_INT(rage::joaat(mpPrefix + "MP_CHAR_ARMOUR_5_COUNT"), 10, true);
+		});
 
 		ImGui::SameLine();
 
