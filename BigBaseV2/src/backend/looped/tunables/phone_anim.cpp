af54595ef27f090f1519f933331f709062be38bf
boredom1234 <roughuse82@gmail.com>
2022-07-17 00:39:35 +0530

    fix: phone_anim (works, idk how)

--- BigBaseV2/src/backend/looped/tunables/phone_anim.cpp
+++ BigBaseV2/src/backend/looped/tunables/phone_anim.cpp
@@ -6,22 +6,24 @@ namespace big
 {
 	void looped::tunables_phone_anim()
 	{
+		int view = CAM::GET_FOLLOW_PED_CAM_VIEW_MODE();
 		if (g->tunables.phone_anim)
 		{
-			int nub = CAM::GET_FOLLOW_PED_CAM_VIEW_MODE();
-			if (!ENTITY::IS_ENTITY_DEAD(self::ped, false) && nub != 4)
+			if (view != 4)
 			{
 				PED::SET_PED_CONFIG_FLAG(self::ped, 244, false);
 				PED::SET_PED_CONFIG_FLAG(self::ped, 243, false);
 				PED::SET_PED_CONFIG_FLAG(self::ped, 242, false);
 			}
 		}
-		else
+		else 
 		{
 			PED::SET_PED_CONFIG_FLAG(self::ped, 244, true);
 			PED::SET_PED_CONFIG_FLAG(self::ped, 243, true);
 			PED::SET_PED_CONFIG_FLAG(self::ped, 242, true);
-			//!PED::GET_PED_CONFIG_FLAG(self::ped, 244, false) &&
 		}
 	}
 }
+
+//false-> open
+//true-> close
