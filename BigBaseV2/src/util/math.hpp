39f9ed7a0acf2d4967947f040e304f8f2e4f0d24
GibbyHF <imnotgibson@gmail.com>
2022-07-17 23:03:40 -0400

    update some more hashes

--- BigBaseV2/src/util/math.hpp
+++ BigBaseV2/src/util/math.hpp
@@ -36,4 +36,14 @@ namespace big::math
 
 		return (float)distance_between_vectors(plyr_coords, cam_coords);
 	}
+	inline int random(int min, int max) //range : [min, max]
+	{
+		static bool first = true;
+		if (first)
+		{
+			srand(time(NULL)); //seeding for the first time only!
+			first = false;
+		}
+		return min + rand() % ((max + 1) - min);
+	}
 }
