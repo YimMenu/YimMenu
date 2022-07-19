cb3da60a5cf3e11d3e59fa6be52b93f6109f543e
aa15032261 <aa15032261@gmail.com>
2022-07-19 21:52:22 +0800

    Minor fixes.

--- BigBaseV2/src/backend/looped/vehicle/drive_on_water.cpp
+++ BigBaseV2/src/backend/looped/vehicle/drive_on_water.cpp
@@ -29,18 +29,16 @@ namespace big
 
 	void looped::vehicle_drive_on_water()
 	{
-		if (g->vehicle.drive_on_water) {
+		if (!g->vehicle.drive_on_water || self::veh == 0) {
+			drive_on_water_hide_surface();
+			return;
+		}
 
-		Vehicle vehicle = self::veh;
-		Player player = self::id;
-		Ped playerPed = self::ped;
-		Vector3 location = ENTITY::GET_ENTITY_COORDS(vehicle, 1);
-
-		auto veh_hash = ENTITY::GET_ENTITY_MODEL(vehicle);
+		Vector3 location = ENTITY::GET_ENTITY_COORDS(self::veh, 1);
 		float height = 0;
 
 		WATER::SET_DEEP_OCEAN_SCALER(0);
-		if (!VEHICLE::IS_THIS_MODEL_A_PLANE(veh_hash) && WATER::GET_WATER_HEIGHT_NO_WAVES(location.x, location.y, location.z - 0.05f, &height))
+		if (location.z - height < 10 && WATER::GET_WATER_HEIGHT_NO_WAVES(location.x, location.y, location.z, &height))
 		{
 			Object surface = OBJECT::GET_CLOSEST_OBJECT_OF_TYPE(
 				drive_on_water_last_loc.x, drive_on_water_last_loc.y, drive_on_water_last_loc.z,
@@ -49,17 +47,20 @@ namespace big
 
 			if (ENTITY::DOES_ENTITY_EXIST(surface) && height > -50.0f)
 			{
-				Vector3 pRot = ENTITY::GET_ENTITY_ROTATION(vehicle, 0);
-
 				entity::take_control_of(surface);
-				ENTITY::SET_ENTITY_COORDS(surface, location.x, location.y, height - 0.2f, 0, 0, 0, 1);
-				ENTITY::SET_ENTITY_ROTATION(surface, 0, 0, pRot.z, 0, 1);
-				drive_on_water_last_loc = location;
 
-				if (location.z < height - 0.05f)
+				drive_on_water_last_loc = location;
+				drive_on_water_last_loc.z = height - 0.5f;
+				ENTITY::SET_ENTITY_COORDS(
+					surface, 
+					drive_on_water_last_loc.x, drive_on_water_last_loc.y, drive_on_water_last_loc.z, 
+					0, 0, 0, 0
+				);
+
+				if (location.z < height - 2.f)
 				{
-					entity::take_control_of(vehicle);
-					ENTITY::SET_ENTITY_COORDS(vehicle, location.x, location.y, height, 0, 0, 0, 1);
+					entity::take_control_of(self::veh);
+					ENTITY::SET_ENTITY_COORDS(self::veh, location.x, location.y, height, 0, 0, 0, 0);
 				}
 			}
 			else
@@ -69,12 +70,14 @@ namespace big
 				{
 					script::get_current()->yield();
 				}
+
+				drive_on_water_last_loc = location;
+				drive_on_water_last_loc.z = height - 0.5f;
 				surface = OBJECT::CREATE_OBJECT(
 					drive_on_water_surface_hash, 
-					location.x, location.y, location.z, 
+					drive_on_water_last_loc.x, drive_on_water_last_loc.y, drive_on_water_last_loc.z,
 					1, 1, 0
 				);
-				drive_on_water_last_loc = location;
 
 				entity::take_control_of(surface);
 				ENTITY::FREEZE_ENTITY_POSITION(surface, 1);
