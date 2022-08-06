7d8be6c8607cf4149dc7a0acda07a26d87277611
aa15032261 <aa15032261@gmail.com>
2022-08-06 23:56:17 +0800

    Removed unused CPed class.

--- BigBaseV2/src/gta/ped_factory.hpp
+++ BigBaseV2/src/gta/ped_factory.hpp
@@ -1,15 +1,6 @@
 #pragma once
 #include "fwddec.hpp"
 
-#pragma pack(push, 1)
-class CPed
-{
-public:
-	char m_padding[0x10B8];
-	CPlayerInfo *m_playerinfo;
-};
-#pragma pack(pop)
-
 class CPedFactory
 {
 public:
