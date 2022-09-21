0597d8728e37274ca2628d36292601fc542745c7
pozhiloy-enotik <49619526+pozhiloy-enotik@users.noreply.github.com>
2022-09-21 16:10:21 +0300

    improve Instant Refill and Infinite Boost (#452)

--- BigBaseV2/src/backend/looped/vehicle/boost_behavior.cpp
+++ BigBaseV2/src/backend/looped/vehicle/boost_behavior.cpp
@@ -10,17 +10,15 @@ namespace big
 
 		if (vehicle && VEHICLE::GET_HAS_ROCKET_BOOST(self::veh))
 		{
-			if (g->vehicle.boost_behavior == eBoostBehaviors::INSTANT_REFIL && vehicle->m_boost == 0.f) // No Boost Refil Time
+			if (g->vehicle.boost_behavior == eBoostBehaviors::INSTANT_REFIL && (vehicle->m_boost == 0.f || !vehicle->m_boost_state)) // Instant Refill
 			{
-				vehicle->m_boost = 1.f;
+				vehicle->m_boost_allow_recharge = true;
+				vehicle->m_boost = 3.f;
 			}
 			else if (g->vehicle.boost_behavior == eBoostBehaviors::INFINITE_BOOST) // Infinite Boost
 			{
-				if (vehicle->m_boost_state)
-				{
-					vehicle->m_boost_allow_recharge = true;
-					vehicle->m_boost = 1.f;
-				}
+				vehicle->m_boost_allow_recharge = true;
+				vehicle->m_boost = 3.f;
 			}
 		}
 	}
