2fd46fd15b602e8ebb70c9ded1f45cd632414ca4
xiaoxiao921 <xiaoxiao921@hotmail.fr>
2022-10-16 17:46:02 +0200

    wren: add reload script button

--- BigBaseV2/src/views/scripting/wren/view_wren.cpp
+++ BigBaseV2/src/views/scripting/wren/view_wren.cpp
@@ -5,6 +5,11 @@ namespace big
 {
     void view::wren()
     {
+        components::button("Reload Scripts", [&]
+        {
+            g_wren_manager->reload_scripts();
+        });
+
         for (const auto& btn : g_wren_manager->m_imgui_buttons)
         {
             components::button(btn.label.c_str(), [&]
