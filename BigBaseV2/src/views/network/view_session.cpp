23764b3e5cd38c9aced5d0d8fbcbafdef53ec55b
xiaoxiao921 <xiaoxiao921@hotmail.fr>
2022-10-26 13:55:29 +0200

    put `join_by_rockstar_id` in the session helper class

--- BigBaseV2/src/views/network/view_session.cpp
+++ BigBaseV2/src/views/network/view_session.cpp
@@ -4,52 +4,16 @@
 #include "pointers.hpp"
 #include "gta/joaat.hpp"
 #include "util/notify.hpp"
-#include "rage/rlSessionByGamerTaskResult.hpp"
 
 namespace big
 {
-	static void join_by_rockstar_id(uint64_t rid)
-	{
-		if (SCRIPT::GET_NUMBER_OF_THREADS_RUNNING_THE_SCRIPT_WITH_THIS_HASH(RAGE_JOAAT("maintransition")) != 0 ||
-			STREAMING::IS_PLAYER_SWITCH_IN_PROGRESS())
-		{
-			g_notification_service->push_error("RID Joiner", "Player switch in progress, wait a bit.");
-			return;
-		}
-
-		rage::rlGamerHandle player_handle(rid);
-		rage::rlSessionByGamerTaskResult result;
-		bool success = false;
-		int state = 0;
-		if (g_pointers->m_start_get_session_by_gamer_handle(0, &player_handle, 1, &result, 1, &success, &state))
-		{
-			while (state == 1)
-				script::get_current()->yield();
-
-			if (state == 3 && success)
-			{
-				g->session.join_queued = true;
-				g->session.info = result.m_session_info;
-				session::join_type({ eSessionType::NEW_PUBLIC });
-				if (SCRIPT::GET_NUMBER_OF_THREADS_RUNNING_THE_SCRIPT_WITH_THIS_HASH(RAGE_JOAAT("maintransition")) == 0)
-				{
-					g->session.join_queued = false;
-					g_notification_service->push_error("RID Joiner", "Unable to launch maintransition");
-				}
-				return;
-			}
-		}
-
-		g_notification_service->push_error("RID Joiner", "Target Player is offline?");
-	}
-
 	void view::session()
 	{
 		static uint64_t rid = 0;
 		ImGui::InputScalar("Input RID", ImGuiDataType_U64, &rid);
 		components::button("Join RID", []
 		{
-			join_by_rockstar_id(rid);
+			session::join_by_rockstar_id(rid);
 		});
 
 		components::sub_title("Session Switcher");
