5ef764af812da887e3ed32484e1411556e60bbf6
Yimura <24669514+Yimura@users.noreply.github.com>
2022-10-30 20:41:37 +0100

    feat(Script): Don't open Visual Studio if it's open already (#546)

--- script.bat
+++ script.bat
@@ -1,14 +1,15 @@
 @echo off
 git submodule update --init
 
-echo "Updating current branch and submodules"
+echo Updating current branch and submodules
 git pull && git submodule update
 
-echo "Generating project files"
+echo Generating project files
 premake5 vs2022
 IF %ERRORLEVEL% NEQ 0 (
   PAUSE
 )
 
 echo "Opening project..."
-start BigBaseV2.sln
+tasklist /FI "WINDOWTITLE eq BigBaseV2 - Microsoft Visual Studio" /FI "STATUS eq running" 2>NUL | find /I /N "devenv.exe" >NUL
+IF %ERRORLEVEL% NEQ 0 (start BigBaseV2.sln) ELSE (echo Visual Studio is already open, doing nothing. && PAUSE)
