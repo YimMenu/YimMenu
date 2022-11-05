69493dc4abe4a9bc32e6a4b22fd81f637172494b
tupoy-ya <tupoy-ya@users.noreply.github.com>
2022-11-05 23:45:30 +0500

    fix(script.bat): Remove `--build` option i hate DOS and windows.

--- script.bat
+++ script.bat
@@ -7,12 +7,10 @@ git pull && git submodule update
 echo Generating project files
 cmake -S. -Bbuild
 
-if ["%~1"]==["--build"] (
-    cmake --build build --config Release
-)
-else
-(
-    echo "Opening project..."
-    tasklist /FI "WINDOWTITLE eq BigBaseV2 - Microsoft Visual Studio" /FI "STATUS eq running" 2>NUL | find /I /N "devenv.exe" >NUL
-    IF %ERRORLEVEL% NEQ 0 (start build/BigBaseV2.sln) ELSE (echo Visual Studio is already open, doing nothing. && PAUSE)
+echo "Opening project..."
+tasklist /FI "WINDOWTITLE eq BigBaseV2 - Microsoft Visual Studio" /FI "STATUS eq running" 2>NUL |  find /I /N "devenv.exe" >NUL
+if %ERRORLEVEL% NEQ 0 (
+    start build/BigBaseV2.sln
+) else (
+    echo Visual Studio is already open, doing nothing. && PAUSE
 )
