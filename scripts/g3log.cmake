fbc024bf433cca1bc653dbbfbcf96d1606935609
tupoy-ya <tupoy-ya@users.noreply.github.com>
2022-11-09 23:45:21 +0500

    chore(g3log): Use YimMenu g3log fork.

--- scripts/g3log.cmake
+++ scripts/g3log.cmake
@@ -7,8 +7,8 @@ set(G3_SHARED_RUNTIME OFF CACHE INTERNAL "")
 
 FetchContent_Declare(
     g3log
-    GIT_REPOSITORY https://github.com/tupoy-ya/g3log.git
-    GIT_TAG        fdbdf0a75c5e3e0bac2f125a5bd902bb16eaa80b
+    GIT_REPOSITORY https://github.com/YimMenu/g3log.git
+    GIT_TAG        346502dd578c7b885970fb8a6ff4ceabae69fb35
     GIT_PROGRESS TRUE
 )
 message("g3log")
