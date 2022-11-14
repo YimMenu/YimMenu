7b21fac40900e2c6e7e564881615326b561c150c
Yimura <24669514+Yimura@users.noreply.github.com>
2022-11-14 18:12:42 +0100

    fix(Module): Replace 4 space indents with tabs

--- src/memory/module.cpp
+++ src/memory/module.cpp
@@ -14,26 +14,26 @@ namespace memory
 	handle module::get_export(std::string_view symbol_name)
 	{
 		if (!m_loaded)
-            return nullptr;
+			return nullptr;
 
-        const auto dosHeader = m_base.as<IMAGE_DOS_HEADER*>();
-        const auto ntHeader = m_base.add(dosHeader->e_lfanew).as<IMAGE_NT_HEADERS*>();
-        const auto imageDataDirectory = ntHeader->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT];
-        const auto exportDirectory = m_base.add(imageDataDirectory.VirtualAddress).as<IMAGE_EXPORT_DIRECTORY*>();
+		const auto dosHeader = m_base.as<IMAGE_DOS_HEADER*>();
+		const auto ntHeader = m_base.add(dosHeader->e_lfanew).as<IMAGE_NT_HEADERS*>();
+		const auto imageDataDirectory = ntHeader->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT];
+		const auto exportDirectory = m_base.add(imageDataDirectory.VirtualAddress).as<IMAGE_EXPORT_DIRECTORY*>();
 
-        const auto nameOffsetArray = m_base.add(exportDirectory->AddressOfNames).as<DWORD*>();
-        const auto ordinalArray = m_base.add(exportDirectory->AddressOfNameOrdinals).as<DWORD*>();
-        const auto functionOffsetArray = m_base.add(exportDirectory->AddressOfFunctions).as<DWORD*>();
+		const auto nameOffsetArray = m_base.add(exportDirectory->AddressOfNames).as<DWORD*>();
+		const auto ordinalArray = m_base.add(exportDirectory->AddressOfNameOrdinals).as<DWORD*>();
+		const auto functionOffsetArray = m_base.add(exportDirectory->AddressOfFunctions).as<DWORD*>();
 
-        for (std::size_t i = 0; i < exportDirectory->NumberOfFunctions; i++)
-        {
-            const auto functionName = m_base.add(nameOffsetArray[i]).as<const char*>();
-            if (strcmp(functionName, symbol_name.data()))
-                continue;
-            
-            return functionOffsetArray + ordinalArray[i];
-        }
-        return nullptr;
+		for (std::size_t i = 0; i < exportDirectory->NumberOfFunctions; i++)
+		{
+			const auto functionName = m_base.add(nameOffsetArray[i]).as<const char*>();
+			if (strcmp(functionName, symbol_name.data()))
+				continue;
+			
+			return functionOffsetArray + ordinalArray[i];
+		}
+		return nullptr;
 	}
 
 	bool module::loaded() const
@@ -68,7 +68,7 @@ namespace memory
 
 		m_base = mod;
 		const auto dosHeader = m_base.as<IMAGE_DOS_HEADER*>();
-        const auto ntHeader = m_base.add(dosHeader->e_lfanew).as<IMAGE_NT_HEADERS*>();
+		const auto ntHeader = m_base.add(dosHeader->e_lfanew).as<IMAGE_NT_HEADERS*>();
 
 		m_size = ntHeader->OptionalHeader.SizeOfImage;
 
