bbc1ebbdc3b17999f57914ecef1212b159f5cf79
Rimmuru <81404937+Rimmuru@users.noreply.github.com>
2022-11-16 21:02:59 +0000

    Update view_mobile.cpp

--- src/views/self/view_mobile.cpp
+++ src/views/self/view_mobile.cpp
@@ -12,6 +12,20 @@ namespace big
 
 		ImGui::Checkbox("Off Radar", &g->self.off_radar);
 
+		ImGui::Separator();	
+		
+		components::sub_title("Merryweather");
+
+		ImGui::Separator();
+
+		components::button("Request Ammo Drop", [] {
+			mobile::merry_weather::request_ammo_drop();
+		});
+
+		components::button("Helicopter Pickup", [] {
+			mobile::merry_weather::request_helicopter_pickup();
+		});
+
 		ImGui::Separator();
 
 		components::button("Mors Mutual Fix All Vehicles", [] {
