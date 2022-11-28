32aa02a572b4eec00a9a934756ec9f3af4a6edbc
Aure7138 <wyaure@163.com>
2022-11-28 21:50:38 +0800

    fix

--- src/backend/looped/self/fast_respawn.cpp
+++ src/backend/looped/self/fast_respawn.cpp
@@ -5,11 +5,13 @@ namespace big
 {
 	void looped::self_fast_respawn()
 	{
-		if (!g->self.fast_respawn) return;
-        if(PED::IS_PED_DEAD_OR_DYING(self::ped, true))
+		if (g->self.fast_respawn)
         {
-            PED::RESURRECT_PED(self::ped);
-            ENTITY::SET_ENTITY_COORDS_NO_OFFSET(self::ped, self::pos.x, self::pos.y, self::pos.z, 0, 0, 0);
+            if(PED::IS_PED_DEAD_OR_DYING(self::ped, true))
+            {
+                PED::RESURRECT_PED(self::ped);
+                ENTITY::SET_ENTITY_COORDS_NO_OFFSET(self::ped, self::pos.x, self::pos.y, self::pos.z, 0, 0, 0);
+            }
         }
 	}
 }
