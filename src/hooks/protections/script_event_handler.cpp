e4dce516709cb1282aafca7d7b2d2dd3c44e7b92
user <email@hostname>
2022-12-14 11:21:49 -0500

    feat(Toxic): Add new teleport locations fix(Protections): Update max interior index

--- src/hooks/protections/script_event_handler.cpp
+++ src/hooks/protections/script_event_handler.cpp
@@ -331,7 +331,7 @@ namespace big
 		}
 		case eRemoteEvent::InteriorControl:
 			int interior = (int)args[2];
-			if (interior < 0 || interior > 158) // the upper bound will change after an update
+			if (interior < 0 || interior > 161) // the upper bound will change after an update
 			{
 				if (auto plyr = g_player_service->get_by_id(player->m_player_id))
 					session::add_infraction(plyr, Infraction::TRIED_KICK_PLAYER);
