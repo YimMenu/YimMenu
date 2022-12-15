870459720295b13c0c58e50d10ff8e2aa738bc97
Yimura <24669514+Yimura@users.noreply.github.com>
2022-12-15 21:20:55 +0100

    fix(cmake): update git commit hash (#706)

--- scripts/gtav-classes.cmake
+++ scripts/gtav-classes.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
     gtav_classes
     GIT_REPOSITORY https://github.com/Yimura/GTAV-Classes.git
-    GIT_TAG        620948d00e390246f3f36b28595da80eb33b7d29
+    GIT_TAG        fa5e1d119c025f739075920447eedb7942f5e1fa
     GIT_PROGRESS TRUE
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
