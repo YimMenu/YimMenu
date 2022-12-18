b50151511a73f4b4fa636acd16a009fda2a74e48
Yimura <24669514+Yimura@users.noreply.github.com>
2022-12-19 00:12:04 +0100

    feat(crossmap): more accurate crossmap

--- src/crossmap.hpp
+++ src/crossmap.hpp
@@ -3,7 +3,7 @@
 
 namespace big
 {
-	constexpr auto NATIVE_COUNT = 6439;
+	constexpr auto NATIVE_COUNT = 6494;
     using crossmap = std::array<std::pair<rage::scrNativeHash, rage::scrNativeHash>, NATIVE_COUNT>;
     extern const crossmap g_crossmap;
 }
