bd571c111eb599318dfd9072f59b1005ea0a769e
user <email@hostname>
2022-12-19 12:09:51 -0500

    chore(Classes): Change to upstream

--- scripts/gtav-classes.cmake
+++ scripts/gtav-classes.cmake
@@ -2,8 +2,8 @@ include(FetchContent)
 
 FetchContent_Declare(
     gtav_classes
-    GIT_REPOSITORY https://github.com/maybegreat48/GTAV-Classes.git
-    GIT_TAG        63856f9c3f5c5e47bd90fede367ee313ee2236da
+    GIT_REPOSITORY https://github.com/Yimura/GTAV-Classes.git
+    GIT_TAG        5ab30af647d7e64404d76ea6168e7f78f5e365af
     GIT_PROGRESS TRUE
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
