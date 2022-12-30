69c61a9a3dd536918ced127ef27512e6dee9a39d
user <email@hostname>
2022-12-30 12:05:00 -0500

    fix(matchmaking): fix pool filter

--- src/services/matchmaking/matchmaking_service.cpp
+++ src/services/matchmaking/matchmaking_service.cpp
@@ -66,7 +66,7 @@ namespace big
 						m_found_sessions[i].is_valid = false;
 					}
 
-					if (g.session_browser.pool_filter_enabled && ((m_found_sessions[i].attributes.discriminator & (1 << 14)) == (1 << 14)) != (bool)g.session_browser.pool_filter_enabled)
+					if (g.session_browser.pool_filter_enabled && ((m_found_sessions[i].attributes.discriminator & (1 << 14)) == (1 << 14)) != (bool)g.session_browser.pool_filter)
 						m_found_sessions[i].is_valid = false;
 				}
 
