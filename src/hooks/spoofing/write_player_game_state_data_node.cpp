30298a03d0bb77f125977997f50d7bbb9e46b142
TheGreenBandit <106003542+TheGreenBandit@users.noreply.github.com>
2023-01-06 18:48:55 -0500

    change based on feedback

--- src/hooks/spoofing/write_player_game_state_data_node.cpp
+++ src/hooks/spoofing/write_player_game_state_data_node.cpp
@@ -1,10 +1,10 @@
 #include "hooking.hpp"
 #include <datanodes/player/CPlayerGameStateDataNode.hpp>
-#include "util/ped.hpp"
+#include "natives.hpp"
 
 namespace big
 {
-	bool is_player_in_cutscene(Ped player) { return NETWORK::NETWORK_IS_PLAYER_IN_MP_CUTSCENE(player); }
+	bool is_player_in_cutscene(Player player) { return NETWORK::NETWORK_IS_PLAYER_IN_MP_CUTSCENE(player); }
 
 	bool is_player_in_interior(Ped player) { return (INTERIOR::GET_INTERIOR_FROM_ENTITY(player) != 0); }
 
@@ -12,7 +12,7 @@ namespace big
 	{
 		auto ret = g_hooking->get_original<write_player_game_state_data_node>()(player, node);
 
-		if (g.spoofing.spoof_hide_god && !is_player_in_cutscene(self::ped) && !is_player_in_interior(self::ped))
+		if (g.spoofing.spoof_hide_god && !is_player_in_cutscene(self::id) && !is_player_in_interior(self::ped))
 		{
 			node->m_is_invincible = false;
 			node->m_bullet_proof = false;
