b8357a3a108398ca42808afbba2f0d0e0cd2d2bf
Johnny <41658571+Johnnyyyy1234@users.noreply.github.com>
2023-01-09 10:30:21 +1300

    Fixed formatting, replaced strcpy with memcpy, and revert variables
    Fixed all of the issues mentioned.

--- src/hooks/spoofing/write_player_gamer_data_node.cpp
+++ src/hooks/spoofing/write_player_gamer_data_node.cpp
@@ -3,40 +3,36 @@
 
 namespace big
 {
-    void hooks::write_player_gamer_data_node(rage::netObject* player, CPlayerGamerDataNode* node)
-    {
-        g_hooking->get_original<write_player_gamer_data_node>()(player, node);
+	void hooks::write_player_gamer_data_node(rage::netObject* player, CPlayerGamerDataNode* node)
+	{
+		g_hooking->get_original<write_player_gamer_data_node>()(player, node);
 
-        // move these variables to a separate function
-        const bool spoof_rockstar_id = g.spoofing.spoof_rockstar_id;
-        const bool spoof_rockstar_dev = spoof_rockstar_id && g.spoofing.spoof_rockstar_dev;
-        const bool spoof_rockstar_qa = spoof_rockstar_id && g.spoofing.spoof_rockstar_qa;
+		if (g.spoofing.spoof_crew_data)
+		{
+			node->m_clan_data.m_clan_member_id = 1;
+			node->m_clan_data.m_clan_id = 41564112;
+			node->m_clan_data.m_clan_id_2 = 41564112;
+			node->m_clan_data.m_clan_color = 420;
+			node->m_clan_data.m_clan_member_count = 15;
+			node->m_clan_data.m_clan_created_time = 420;
 
-        if (g.spoofing.spoof_crew_data)
-        {
-            node->m_clan_data.m_clan_member_id = 1;
-            node->m_clan_data.m_clan_id = 41564112;
-            node->m_clan_data.m_clan_id_2 = 41564112;
-            node->m_clan_data.m_clan_color = 420;
-            node->m_clan_data.m_clan_member_count = 15;
-            node->m_clan_data.m_clan_created_time = 420;
+			memcpy(node->m_clan_data.m_clan_tag, g.spoofing.crew_tag.c_str(), std::min(g.spoofing.crew_tag.size(), sizeof(node->m_clan_data.m_clan_tag)));
+        		node->m_clan_data.m_is_system_clan = g.spoofing.rockstar_crew;
+        		node->m_clan_data.m_is_clan_open = g.spoofing.square_crew_tag;
 
-            strcpy(node->m_clan_data.m_clan_tag, g.spoofing.crew_tag.c_str());
-            node->m_clan_data.m_is_system_clan = g.spoofing.rockstar_crew;
-            node->m_clan_data.m_is_clan_open = g.spoofing.square_crew_tag;
+			if (g.spoofing.rockstar_crew)
+			{
+				strcpy(node->m_clan_data.m_clan_name, "Rockstar");
+			}
+		}
 
-            if (g.spoofing.rockstar_crew)
-            {
-                strcpy(node->m_clan_data.m_clan_name, "Rockstar");
-            }
-        }
+		node->m_is_cheater = g.spoofing.spoof_cheater;
 
-        node->m_is_cheater = g.spoofing.spoof_cheater;
-        
-        if (g.spoofing.spoof_rockstar_id)
-	    {
-		    node->m_is_rockstar_dev = g.spoofing.spoof_rockstar_dev;
-		    node->m_is_rockstar_qa = g.spoofing.spoof_rockstar_qa;
-	    }
-    }
+		// spoof r* dev and qa only when spoofing rid because every player in the session will send metrics if you join with that enabled
+		if (g.spoofing.spoof_rockstar_id)
+		{
+			node->m_is_rockstar_dev = g.spoofing.spoof_rockstar_dev;
+			node->m_is_rockstar_qa = g.spoofing.spoof_rockstar_qa;
+		}
+	}
 }
