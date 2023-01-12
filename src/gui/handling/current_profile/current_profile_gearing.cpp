527e7d75a88814901c009fa52c51db5403ea73ff
Yimura <24669514+Yimura@users.noreply.github.com>
2023-01-12 22:21:29 +0100

    Some stuff

--- src/gui/handling/current_profile/current_profile_gearing.cpp
+++ src/gui/handling/current_profile/current_profile_gearing.cpp
@@ -4,17 +4,17 @@ namespace big
 {
 	void tab_current_profile::tab_gearing()
 	{
-		if (ImGui::BeginTabItem("Gearing"))
+		if (ImGui::BeginTabItem("HANDLING_TAB_GEARING"_T.data()))
 		{
-			ImGui::Text("Initial Drive Gears");
+			ImGui::Text("HANDLING_INITIAL_DRIVE_GEARS"_T.data());
 			int initial_drive_gears = g_local_player->m_vehicle->m_handling_data->m_initial_drive_gears;
 			if (ImGui::DragInt("###handling_drive_gears", &initial_drive_gears, .1f, 1, 16))
 				g_local_player->m_vehicle->m_handling_data->m_initial_drive_gears = initial_drive_gears;
 
-			ImGui::Text("Upshift Multiplier");
+			ImGui::Text("HANDLING_UPSHIFT_MULTIPLIER"_T.data());
 			ImGui::DragFloat("###handling_upshift", &g_local_player->m_vehicle->m_handling_data->m_upshift, .01f, 0.f, 10.f);
 
-			ImGui::Text("Downshift Multiplier");
+			ImGui::Text("HANDLING_DOWNSHIFT_MULTIPLIER"_T.data());
 			ImGui::DragFloat("###handling_downshift", &g_local_player->m_vehicle->m_handling_data->m_downshift, .01f, 0.f, 10.f);
 
 			ImGui::EndTabItem();
