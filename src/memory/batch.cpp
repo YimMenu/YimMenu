f6443ae9f1a86d96097901c91b5da6add41d7dff
Yimura <24669514+Yimura@users.noreply.github.com>
2023-01-18 19:41:09 +0100

    feat(Async): return boolean

--- src/memory/batch.cpp
+++ src/memory/batch.cpp
@@ -6,7 +6,7 @@
 #include <future> //std::async
 
 static std::mutex s_entry_mutex;
-static std::vector<std::future<void>> m_futures;
+static std::vector<std::future<bool>> g_futures;
 
 namespace memory
 {
@@ -15,10 +15,8 @@ namespace memory
 		m_entries.emplace_back(std::move(name), std::move(pattern), std::move(callback));
 	}
 
-	void scan_pattern_and_execute_callback(range region, memory::batch::entry entry)
+	bool scan_pattern_and_execute_callback(range region, memory::batch::entry entry)
 	{
-		bool all_found = true;
-
 		if (auto result = region.scan(entry.m_pattern))
 		{
 			if (entry.m_callback)
@@ -26,41 +24,37 @@ namespace memory
 				std::lock_guard<std::mutex> lock(s_entry_mutex); // Acquire a lock on the mutex to synchronize access.
 
 				std::invoke(std::move(entry.m_callback), result);
-				std::stringstream file_out{}; //I hate this, but not logging it except on fail really helps on speed.
-				file_out << "Found '" << entry.m_name << "' GTA5.exe+" << HEX_TO_UPPER(result.as<DWORD64>() - region.begin().as<DWORD64>());
-				big::g_log->file_out(file_out);
-				file_out.clear();
-			}		
-			else
-			{
-				all_found = false;
-				LOG(WARNING) << "Failed to find '" << entry.m_name << "'.";
+				LOG(INFO) << "Found '" << entry.m_name << "' GTA5.exe+" << HEX_TO_UPPER(result.as<DWORD64>() - region.begin().as<DWORD64>());
+
+				return true;
 			}
 		}
-		else
-		{
-			all_found = false;
-			LOG(WARNING) << "Failed to find '" << entry.m_name << "'.";
-		}
+		
+		LOG(WARNING) << "Failed to find '" << entry.m_name << "'.";
 
-		if (!all_found)
-		{
-			LOG(WARNING) << "Failed to find some patterns.";
-			//Unload here.
-		}
+		return false;
 	}
 
-	void batch::run(range region)
+	bool batch::run(range region)
 	{
 		for (auto& entry : m_entries)
 		{
-			m_futures.emplace_back(std::async(std::launch::async, scan_pattern_and_execute_callback, region, entry)); //Save the return.
+			g_futures.emplace_back(
+				std::async(&scan_pattern_and_execute_callback, region, entry));
 		}
 
-		std::for_each(m_futures.begin(), m_futures.end(),
-		std::mem_fn(&std::future<void>::wait));
+		bool found_all_patterns = true;
+		for (auto& future : g_futures)
+		{
+			future.wait();
+			
+			if (!future.get())
+				found_all_patterns = false;
+		}
 
 		m_entries.clear();
-		m_futures.clear();
+		g_futures.clear();
+
+		return found_all_patterns;
 	}
 }
