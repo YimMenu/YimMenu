2789c11310820ca0e37094a8e4b84dc19419fffb
Yimura <24669514+Yimura@users.noreply.github.com>
2023-01-18 23:22:25 +0100

    feat(AsyncLogger): Bump to version with license

--- scripts/async-logger.cmake
+++ scripts/async-logger.cmake
@@ -4,7 +4,7 @@ message("AsyncLogger")
 FetchContent_Declare(
     AsyncLogger
     GIT_REPOSITORY https://github.com/Yimura/AsyncLogger.git
-    GIT_TAG v0.0.4
+    GIT_TAG v0.0.5
     GIT_PROGRESS TRUE
 )
 FetchContent_MakeAvailable(AsyncLogger)
