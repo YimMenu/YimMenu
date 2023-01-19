6ff72c239c9f50107409ac7fddd08656527b8fb4
TheGreenBandit <106003542+TheGreenBandit@users.noreply.github.com>
2023-01-18 22:38:51 -0500

    added some debug options

--- src/views/debug/view_debug_locals.cpp
+++ src/views/debug/view_debug_locals.cpp
@@ -1,4 +1,6 @@
 #include "view_debug.hpp"
+#include "gui/components/components.hpp"
+#include "natives.hpp"
 
 namespace big
 {
@@ -6,7 +8,20 @@ namespace big
 	{
 		if (ImGui::BeginTabItem("Locals"))
 		{
-
+			if (components::button("Load MP Map"))
+				DLC::ON_ENTER_MP();
+			ImGui::SameLine();
+			if (components::button("Load SP Map"))
+				DLC::ON_ENTER_SP();
+			if (components::button("Skip Cutscene"))
+				CUTSCENE::STOP_CUTSCENE_IMMEDIATELY();
+			if (components::button("Refresh Interior"))
+			{
+				Interior interior = INTERIOR::GET_INTERIOR_AT_COORDS(self::pos.x, self::pos.y, self::pos.z);
+				INTERIOR::REFRESH_INTERIOR(interior);
+			}
+			if (ImGui::IsItemHovered())
+				ImGui::SetTooltip("You Will Have To Refresh Again When Exiting Interior.");
 			ImGui::EndTabItem();
 		}
 	}
