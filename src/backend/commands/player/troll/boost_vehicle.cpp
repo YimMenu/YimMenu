b685f15007aae87417246ca372b1c14315c1aa07
Jay <121067410+JDMJay@users.noreply.github.com>
2023-01-24 22:19:10 +0000

    .

--- src/backend/commands/player/troll/boost_vehicle.cpp
+++ src/backend/commands/player/troll/boost_vehicle.cpp
@@ -2,14 +2,6 @@
 #include "natives.hpp"
 #include "pointers.hpp"
 #include "util/teleport.hpp"
-
-namespace big
-{
-	class boost_vehicle : player_command
-	{
-		using player_command::player_command;
-
-		virtual void execute(player_ptr player, const std::vector<std::uint64_t>& _args, const std::shared_ptr<command_context> ctx)
 		{
             Ped ped = PLAYER::GET_PLAYER_PED_SCRIPT_INDEX(player->id());
             if (!PED::IS_PED_IN_ANY_VEHICLE(ped, true))
@@ -23,6 +15,32 @@ namespace big
                     {
                         g_notification_service->push_warning("Toxic", "Failed to take control of player vehicle.");
                     }
+                }
+            }
+        }
+    };
+namespace big
+{
+	class boost_vehicle : player_command
+	{
+		using player_command::player_command;
+
+		virtual void execute(player_ptr player, const std::vector<std::uint64_t>& _args, const std::shared_ptr<command_context> ctx)
+        {
+            Ped ped = PLAYER::GET_PLAYER_PED_SCRIPT_INDEX(player->id());
+            if (!PED::IS_PED_IN_ANY_VEHICLE(ped, true))
+            {
+                g_notification_service->push_warning("Toxic", "Target player is not in a vehicle.");
+            }
+            else {
+                Vehicle vehicle = PED::GET_VEHICLE_PED_IS_USING(ped);
+
+                if (entity::take_control_of(vehicle)) {
+                    VEHICLE::SET_VEHICLE_FORWARD_SPEED(vehicle, 79);
+                }
+                else
+                {
+                    g_notification_service->push_warning("Toxic", "Failed to take control of player vehicle.");
                 }
             }
         }
