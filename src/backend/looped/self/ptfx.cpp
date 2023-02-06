8b088762391d79b94e29edf5883f3819f98fa8e6
Primexz <johann@pepebot.info>
2023-02-06 19:44:48 +0100

    fix(ptfx): draw esp at vehicle tires

--- src/backend/looped/self/ptfx.cpp
+++ src/backend/looped/self/ptfx.cpp
@@ -10,7 +10,7 @@ namespace big
 	{
 		using looped_command::looped_command;
 
-		PedBones ptfx_bones[5] = {
+		PedBones ptfx_ped_bones[5] = {
 			PedBones::SKEL_Head,
 			PedBones::SKEL_L_Hand,
 			PedBones::SKEL_R_Hand,
@@ -18,20 +18,40 @@ namespace big
 			PedBones::SKEL_R_Foot
 		};
 
-		void show_ptfx_effect(const char* fx_name, const char* name)
+		const char* ptfx_vehicle_pos[4] = {
+			"wheel_lf",
+			"wheel_lr",
+			"wheel_rf",
+			"wheel_rr"
+		};
+
+		void show_player_ptfx_effect(const char* fx_name, const char* name)
 		{
-			for (const auto& ptfx_bone : ptfx_bones)
+			STREAMING::REQUEST_NAMED_PTFX_ASSET(fx_name);			
+			for (const auto& ptfx_bone : ptfx_ped_bones)
 			{
-				STREAMING::REQUEST_NAMED_PTFX_ASSET(fx_name);
 				GRAPHICS::USE_PARTICLE_FX_ASSET(fx_name);
-				GRAPHICS::START_NETWORKED_PARTICLE_FX_NON_LOOPED_ON_PED_BONE(name, self::ped, 0.00f, 0.00f, 0.00f, 0.00f, 0.00f, 0.0f, (int)ptfx_bone, g.self.ptfx_effects.size, 1, 1, 1);
+				GRAPHICS::START_NETWORKED_PARTICLE_FX_NON_LOOPED_ON_PED_BONE(name, self::ped, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, (int)ptfx_bone, g.self.ptfx_effects.size, 1, 1, 1);
 				STREAMING::REMOVE_PTFX_ASSET();
+			}
+		}
+
+		void show_vehicle_ptfx_effect(const char* fx_name, const char* name)
+		{
+			for (const auto& ptfx_bone : ptfx_vehicle_pos)
+			{
+				GRAPHICS::USE_PARTICLE_FX_ASSET(fx_name);
+				Vector3 bone_pos = ENTITY::GET_WORLD_POSITION_OF_ENTITY_BONE(self::veh, ENTITY::GET_ENTITY_BONE_INDEX_BY_NAME(self::veh, ptfx_bone));
+				GRAPHICS::START_NETWORKED_PARTICLE_FX_NON_LOOPED_AT_COORD(name, bone_pos.x, bone_pos.y, bone_pos.z, 0.f, 0.f, 0.f, g.self.ptfx_effects.size, 0.f, 0.f, 0.f, 0);
 			}
 		}
 
 		virtual void on_tick() override
 		{
-			show_ptfx_effect(g.self.ptfx_effects.asset, g.self.ptfx_effects.effect);			
+			if(self::veh == 0)
+				show_player_ptfx_effect(g.self.ptfx_effects.asset, g.self.ptfx_effects.effect);
+			else
+				show_vehicle_ptfx_effect(g.self.ptfx_effects.asset, g.self.ptfx_effects.effect);
 		}
 	};
 
