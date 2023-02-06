5f8f507861c14a2afdc77331ccf9e73100151b9f
Aure7138 <wyaure@163.com>
2023-02-06 12:29:38 +0800

    indent

--- src/util/heist.hpp
+++ src/util/heist.hpp
@@ -5,13 +5,13 @@
 
 namespace big::heist::cayo
 {
-	const int crew_cut			= 1977693 + 823 + 56 + (1 + 0 * 1);
-	const int bag_size			= 262145 + 29732;
-	const int target_value		= 262145 + 29982;
-	const int multiplier_normal	= 262145 + 29988;
-	const int multiplier_hard	= 262145 + 29989;
-	const int fencing_fee		= 262145 + 29991;
-	const int pavel_fee			= 262145 + 29992;
+	const int crew_cut = 1977693 + 823 + 56 + (1 + 0 * 1);
+	const int bag_size = 262145 + 29732;
+	const int target_value = 262145 + 29982;
+	const int multiplier_normal = 262145 + 29988;
+	const int multiplier_hard = 262145 + 29989;
+	const int fencing_fee = 262145 + 29991;
+	const int pavel_fee = 262145 + 29992;
 
 	const int take = 45687 + 1392 + 53;
 	const int team_lives = 48647 + 868 + (1 + 0 * 1);
@@ -45,15 +45,15 @@ namespace big::heist::cayo
 
 namespace big::heist::casino
 {
-	const int crew_cut					= 1970895 + 1497 + 736 + 92 + (1 + 0 * 1);	
-	const int multiplier_normal			= 262145 + 28786;
-	const int multiplier_hard			= 262145 + 28787;
-	const int buyer_multiplier_easy		= 262145 + 28788;
-	const int buyer_multiplier_normal	= 262145 + 28789;
-	const int buyer_multiplier_hard		= 262145 + 28790;
-	const int lester_cut				= 262145 + 28791;							
-	const int target_value				= 262145 + 28805;
-	const int ai_cut					= 262145 + 28817;
+	const int crew_cut = 1970895 + 1497 + 736 + 92 + (1 + 0 * 1);	
+	const int multiplier_normal = 262145 + 28786;
+	const int multiplier_hard = 262145 + 28787;
+	const int buyer_multiplier_easy = 262145 + 28788;
+	const int buyer_multiplier_normal = 262145 + 28789;
+	const int buyer_multiplier_hard = 262145 + 28790;
+	const int lester_cut = 262145 + 28791;							
+	const int target_value = 262145 + 28805;
+	const int ai_cut = 262145 + 28817;
 
 	const int take = 19707 + 2686;
 	const int team_lives = 26133 + 1325 + (1 + 0 * 1);
@@ -87,19 +87,19 @@ namespace big::heist::casino
 
 namespace big::heist::doomsday
 {
-	const int crew_cut				= 1966831 + 812 + 50 + (1 + 0 * 1);
-	const int target_value			= 262145 + 9133;
-	const int multiplier_normal		= 262145 + 9202;
-	const int multiplier_hard		= 262145 + 9203;
+	const int crew_cut = 1966831 + 812 + 50 + (1 + 0 * 1);
+	const int target_value = 262145 + 9133;
+	const int multiplier_normal = 262145 + 9202;
+	const int multiplier_hard = 262145 + 9203;
 }
 
 namespace big::heist::apart
 {
-	const int crew_cut			= 1937658 + 3008 + (1 + 0 * 1); //https://www.unknowncheats.me/forum/2923446-post841.html
-	const int target_value		= 262145 + 9128;
-	const int multiplier_easy	= 262145 + 9199;
-	const int multiplier_normal	= 262145 + 9200;
-	const int multiplier_hard	= 262145 + 9201;
+	const int crew_cut = 1937658 + 3008 + (1 + 0 * 1); //https://www.unknowncheats.me/forum/2923446-post841.html
+	const int target_value = 262145 + 9128;
+	const int multiplier_easy = 262145 + 9199;
+	const int multiplier_normal = 262145 + 9200;
+	const int multiplier_hard = 262145 + 9201;
 }
 
 namespace big::heist::agency_vip_contract
