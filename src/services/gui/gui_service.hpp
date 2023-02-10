f24f17100f47c7df6e11867fec68a144bcb4503f
Seanghost117 <36561158+Seanghost117@users.noreply.github.com>
2023-02-10 08:13:17 -0700

    added delete and kill nearby peds and updated ped header

--- src/services/gui/gui_service.hpp
+++ src/services/gui/gui_service.hpp
@@ -30,6 +30,7 @@ namespace big
 		TRAIN,
 		WATER,
 		BLACKHOLE,
+		NEARBY,
 
 		NETWORK,
 		SESSION,
@@ -88,6 +89,7 @@ namespace big
 				{ tabs::TRAIN, { "Train", view::train }},
 				{ tabs::WATER, { "Water", view::water }},
 				{ tabs::BLACKHOLE, { "Blackhole", view::blackhole }},
+				{ tabs::NEARBY, { "Nearby", view::nearby }},
 		}}},
 			{tabs::NETWORK, { "Network", nullptr, {
 				{ tabs::SPOOFING, { "Spoofing", view::spoofing }},
--- src/views/world/view_nearby.cpp
+++ src/views/world/view_nearby.cpp
@@ -1,12 +1,15 @@
 #include "views/view.hpp"
 #include "util/local_player.hpp"
+#include "util/entity.hpp"
+#include "util/ped.hpp"
 
 namespace big
 {
 	void view::nearby()
 	{
 		components::sub_title("Peds");
-		
+		components::button("Delete", [] { for (auto peds : entity::get_entities(false, true)) entity::delete_entity(peds); }); ImGui::SameLine();
+		components::button("Kill", [] { for (auto peds : entity::get_entities(false, true)) ped::kill_ped(peds); }); 
 
 		components::sub_title("Vehicles");
 		
