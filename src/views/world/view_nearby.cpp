4fbb4d072255bf3ca17faa7c3a51cc70b356a127
Seanghost117 <36561158+Seanghost117@users.noreply.github.com>
2023-02-10 17:02:06 -0700

    fixed a problem where the vehicle options crashed + added color

--- src/views/world/view_nearby.cpp
+++ src/views/world/view_nearby.cpp
@@ -9,9 +9,9 @@ namespace big
 	void view::nearby()
 	{
 		components::sub_title("Peds");
-		components::button("Delete", [] { for (auto peds : entity::get_entities(false, true)) entity::delete_entity(peds); }); ImGui::SameLine();
-		components::button("Kill", [] { for (auto peds : entity::get_entities(false, true)) ped::kill_ped(peds); }, ImVec2(0, 0), ImVec4(0.95f, 0.2f, 0.2f, 1.f)); ImGui::SameLine();
-		components::button("Kill Enemies", [] { for (auto peds : entity::get_entities(false, true)) ped::kill_ped_by_relation(peds, 4 || 5); }, ImVec2(0, 0), ImVec4(1.f, 0.1f, 0.1f, 1.f));
+		components::button("Delete", [] { for (auto peds : entity::get_entities(false, true)) entity::delete_entity(peds); }, ImVec2(110, 0)); ImGui::SameLine();
+		components::button("Kill", [] { for (auto peds : entity::get_entities(false, true)) ped::kill_ped(peds); }, ImVec2(110, 0), ImVec4(0.95f, 0.2f, 0.2f, 1.f)); ImGui::SameLine();
+		components::button("Kill Enemies", [] { for (auto peds : entity::get_entities(false, true)) ped::kill_ped_by_relation(peds, 4 || 5); }, ImVec2(110, 0), ImVec4(1.f, 0.1f, 0.1f, 1.f));
 		
 		components::command_checkbox<"pedsignore">(); ImGui::SameLine(140.f);
 		components::command_checkbox<"pedrain">(); 
@@ -20,10 +20,30 @@ namespace big
 
 		ImGui::Separator();
 		components::sub_title("Vehicles");
-		components::button("Delete", [] { for (auto vehs : entity::get_entities(true, false)) entity::delete_entity(vehs); }, ImVec2(110, 0)); ImGui::SameLine();
-		components::button("Max Upgrade", [] { for (auto vehs : entity::get_entities(true, false)) vehicle::max_vehicle(vehs); }, ImVec2(110, 0)); ImGui::SameLine();
-		components::button("Downgrade", [] { for (auto vehs : entity::get_entities(true, false)) vehicle::downgrade(vehs); }, ImVec2(110, 0)); 
 
+		components::button("Delete", [] {
+			for (auto vehs : entity::get_entities(true, false))
+				if (entity::take_control_of(vehs)) {
+					entity::delete_entity(vehs);
+					script::get_current()->yield();
+				}
+		}, ImVec2(110, 0)); ImGui::SameLine();
+
+		components::button("Max Upgrade", [] {
+			for (auto vehs : entity::get_entities(true, false))
+				if (entity::take_control_of(vehs)) {
+					vehicle::max_vehicle(vehs);
+					script::get_current()->yield();
+				}
+		}, ImVec2(110, 0), ImVec4(0.02745f, 0.4745f, 0.10196f, 1.f)); ImGui::SameLine();
+
+		components::button("Downgrade", [] {
+			for (auto vehs : entity::get_entities(true, false))
+				if (entity::take_control_of(vehs)) {
+					vehicle::downgrade(vehs);
+					script::get_current()->yield();
+				}
+		}, ImVec2(110, 0), ImVec4(0.4549f, 0.03529f, 0.03529f, 1.f));
 		
 	}
 }
