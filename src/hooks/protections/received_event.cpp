8338a7def32b05c56373f5dc7e6cb6732feca216
yasmasdas <114929405+yasmasdas@users.noreply.github.com>
2023-02-14 12:12:29 +0100

    type > 8

--- src/hooks/protections/received_event.cpp
+++ src/hooks/protections/received_event.cpp
@@ -393,7 +393,7 @@ namespace big
 					return;
 				}
 			}
-			else if (type > 9 || type < 0) {
+			else if (type > ScriptEntityChangeType::SetVehicleExclusiveDriver || type < ScriptEntityChangeType::BlockingOfNonTemporaryEvents) {
 				notify::crash_blocked(source_player, "invalid script entity change type");
 				g_pointers->m_send_event_ack(event_manager, source_player, target_player, event_index, event_handled_bitset);
 				return;
