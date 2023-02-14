3c9402362707a0b5d08091cb1e1ec06e47395a42
Aure7138 <wyaure@163.com>
2023-02-14 22:47:14 +0800

    commit

--- src/pointers.hpp
+++ src/pointers.hpp
@@ -151,10 +151,6 @@ namespace big
 		PVOID m_invalid_mods_crash_detour{};
 		PVOID m_constraint_attachment_crash{};
 		PVOID m_invalid_decal_crash{};
-		PVOID m_fragment_physics_crash;
-		PVOID m_fragment_physics_crash_2;
-		PVOID m_infinite_train_crash;
-		functions::get_next_carriage m_get_next_carriage;
 
 		int64_t** m_send_chat_ptr{};
 		functions::send_chat_message m_send_chat_message{};
@@ -229,6 +225,12 @@ namespace big
 
 		functions::send_packet m_send_packet;
 		functions::connect_to_peer m_connect_to_peer;
+    
+		PVOID m_fragment_physics_crash;
+		PVOID m_fragment_physics_crash_2;
+
+		PVOID m_infinite_train_crash;
+		functions::get_next_carriage m_get_next_carriage;
 
 		functions::get_entity_attached_to m_get_entity_attached_to;
 
