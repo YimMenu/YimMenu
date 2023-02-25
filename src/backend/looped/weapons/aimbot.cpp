0c6db96a8e7901db95064713ce00e4ce809ed88b
Seanghost117 <36561158+Seanghost117@users.noreply.github.com>
2023-02-25 10:38:35 -0700

    brackets ig

--- src/backend/looped/weapons/aimbot.cpp
+++ src/backend/looped/weapons/aimbot.cpp
@@ -27,63 +27,68 @@ namespace big
 						continue; // If the entity is further than our preset distance then just skip it
 
 					if (PED::IS_PED_A_PLAYER(ped) && g.weapons.aimbot.on_player) // check if its a player
+					{
 						goto aimbot_handler;
-
+					}
 					else if (((relation == 4) || (relation == 5)) && g.weapons.aimbot.on_enemy) // relation 4 and 5 are for enemies
+					{
 						goto aimbot_handler;
-
-					else if(((type == 6 && !PED::IS_PED_MODEL(ped, rage::joaat("s_m_y_uscg_01"))) || type == 27 || // s_m_y_uscg_01 = us coast guard 1 (techniaclly military)
+					}
+					else if (((type == 6 && !PED::IS_PED_MODEL(ped, rage::joaat("s_m_y_uscg_01"))) || type == 27 || // s_m_y_uscg_01 = us coast guard 1 (techniaclly military)
 						PED::IS_PED_MODEL(ped, rage::joaat("s_m_y_ranger_01")) || PED::IS_PED_MODEL(ped, rage::joaat("s_f_y_ranger_01"))) // ranger models
 						&& g.weapons.aimbot.on_police)
+					{
 						goto aimbot_handler;
-
+					}
 					else if (g.weapons.aimbot.on_npc && !PED::IS_PED_A_PLAYER(ped))
 
 					// Update aim lock coords 
-					aimbot_handler: // Jump to here to handle instead of continue statements
-					aim_lock = ENTITY::GET_WORLD_POSITION_OF_ENTITY_BONE(ped, PED::GET_PED_BONE_INDEX(ped, g.weapons.aimbot.selected_bone));
-					if ((aim_lock.x != 0) && (aim_lock.y != 0) && (aim_lock.z != 0)) // Ensure none of the coords are = to 0
-					{
-						Vector2 screen_dim, movement;
-						GRAPHICS::GET_SCREEN_COORD_FROM_WORLD_COORD(aim_lock.x, aim_lock.y, aim_lock.z, &screen_dim.x, &screen_dim.y);
-						if ((screen_dim.x >= 0) && (screen_dim.y >= 0)) // Make sure updated screen dim is greater than 0
+					aimbot_handler: 
+					{  // Jump to here to handle instead of continue statements
+						aim_lock = ENTITY::GET_WORLD_POSITION_OF_ENTITY_BONE(ped, PED::GET_PED_BONE_INDEX(ped, g.weapons.aimbot.selected_bone));
+						if ((aim_lock.x != 0) && (aim_lock.y != 0) && (aim_lock.z != 0)) // Ensure none of the coords are = to 0
 						{
-							auto& io = ImGui::GetIO();
-							ImVec2 center(io.DisplaySize.x / 2.f, io.DisplaySize.y / 2.f); // Use ImGui to get the display size
-							//Screen dim is a float between 0-1, multiply the float by screen coords
-							screen_dim.x *= io.DisplaySize.x; 
-							screen_dim.y *= io.DisplaySize.y;
+							Vector2 screen_dim, movement;
+							GRAPHICS::GET_SCREEN_COORD_FROM_WORLD_COORD(aim_lock.x, aim_lock.y, aim_lock.z, &screen_dim.x, &screen_dim.y);
+							if ((screen_dim.x >= 0) && (screen_dim.y >= 0)) // Make sure updated screen dim is greater than 0
+							{
+								auto& io = ImGui::GetIO();
+								ImVec2 center(io.DisplaySize.x / 2.f, io.DisplaySize.y / 2.f); // Use ImGui to get the display size
+								//Screen dim is a float between 0-1, multiply the float by screen coords
+								screen_dim.x *= io.DisplaySize.x;
+								screen_dim.y *= io.DisplaySize.y;
 
-							if (screen_dim.x > center.x) //If the location is greater than the center (right side)
-							{ // Get the amount of distance we need to move, so center of the screen - our location
-								movement.x = -(center.x - screen_dim.x);
-								if (movement.x + center.x > center.x * 2) movement.x = 0;
-							} 
-							else
-							{ // if the location is on the left side
-								movement.x = screen_dim.x - center.x;
-								if (movement.x + center.x < 0) movement.x = 0;
-							}
+								if (screen_dim.x > center.x) //If the location is greater than the center (right side)
+								{ // Get the amount of distance we need to move, so center of the screen - our location
+									movement.x = -(center.x - screen_dim.x);
+									if (movement.x + center.x > center.x * 2) movement.x = 0;
+								}
+								else
+								{ // if the location is on the left side
+									movement.x = screen_dim.x - center.x;
+									if (movement.x + center.x < 0) movement.x = 0;
+								}
 
-							// Same thing here but for y, so switch right with up and left with down
-							if (screen_dim.y > center.y)
-							{ 
-								movement.y = -(center.y - screen_dim.y);
-								if (movement.y + center.y > center.y * 2) movement.x = 0;
-							}
-							else
-							{ 
-								movement.y = screen_dim.y - center.y;
-								if (movement.y + center.y < 0) movement.y = 0;
-							}
+								// Same thing here but for y, so switch right with up and left with down
+								if (screen_dim.y > center.y)
+								{
+									movement.y = -(center.y - screen_dim.y);
+									if (movement.y + center.y > center.y * 2) movement.x = 0;
+								}
+								else
+								{
+									movement.y = screen_dim.y - center.y;
+									if (movement.y + center.y < 0) movement.y = 0;
+								}
 
-							if (sqrt(pow(movement.x, 2) + pow(movement.y, 2)) < local_fov_change)
-							{ // sqrt of movment x and y ^ 2, handles fov math
-								local_fov_change = sqrt(pow(movement.x, 2) + pow(movement.y, 2));
-								mouse_movement.x = movement.x;
-								mouse_movement.y = movement.y;
-							}
+								if (sqrt(pow(movement.x, 2) + pow(movement.y, 2)) < local_fov_change)
+								{ // sqrt of movment x and y ^ 2, handles fov math
+									local_fov_change = sqrt(pow(movement.x, 2) + pow(movement.y, 2));
+									mouse_movement.x = movement.x;
+									mouse_movement.y = movement.y;
+								}
 
+							}
 						}
 					}
 				}
