ece1c73b2d57efc087d674a8825c81e72a7d5001
TheGreenBandit <106003542+TheGreenBandit@users.noreply.github.com>
2023-03-06 10:38:46 -0500

    Use riot mode as tested

--- src/backend/looped/world/nearby/combative.cpp
+++ src/backend/looped/world/nearby/combative.cpp
@@ -1,6 +1,5 @@
 #include "backend/looped_command.hpp"
 #include "natives.hpp"
-#include "util/entity.hpp"
 
 namespace big
 {
@@ -9,37 +8,15 @@ namespace big
 	{
 		using looped_command::looped_command;
 
-		int id = 0;
-
-		virtual void on_tick() override
+		virtual void on_enable() override
 		{
-			for (auto ped : entity::get_entities(false, true))
-			{
-				id = PED::GET_PED_GROUP_INDEX(ped);
-				if (!PED::IS_PED_A_PLAYER(ped))
-				{
-					PED::SET_RELATIONSHIP_BETWEEN_GROUPS(5, id, id);
-				}
-				int time = 0;
-				if (!PED::IS_PED_SHOOTING(ped) || !PED::IS_PED_IN_COMBAT(ped, ped) && time <1000)
-				{
-					TASK::TASK_COMBAT_HATED_TARGETS_AROUND_PED(ped, 100.f, 0);
-				}
-			}
+			MISC::SET_RIOT_MODE_ENABLED(true);
 		}
 		virtual void on_disable() override
 		{
-			for (auto ped : entity::get_entities(false, true))
-			{
-				bool b = PED::IS_PED_A_PLAYER(ped);
-				if (!b)
-				{
-					PED::SET_RELATIONSHIP_BETWEEN_GROUPS(255, id, id);
-				}
-			}
-		}
-	};
-
+			MISC::SET_RIOT_MODE_ENABLED(false);
+                }
+        };
 	combative g_combative("pedscombat", "Combative", "Nearby peds attack eachother.", g.world.nearby.combative);
 
 }
