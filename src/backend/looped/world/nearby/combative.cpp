9f6d94ffcc496f9a4ef2e844c82fdae5d0ba053a
TheGreenBandit <106003542+TheGreenBandit@users.noreply.github.com>
2023-03-09 18:56:23 -0800

    resolve

--- src/backend/looped/world/nearby/combative.cpp
+++ src/backend/looped/world/nearby/combative.cpp
@@ -12,11 +12,14 @@ namespace big
 		{
 			MISC::SET_RIOT_MODE_ENABLED(true);
 		}
+		
+		virtual void on_tick() override {}
+		
 		virtual void on_disable() override
 		{
 			MISC::SET_RIOT_MODE_ENABLED(false);
                 }
         };
-	combative g_combative("riotmode", "Riot Mode", "Make nearby peds attack eachother.", g.world.nearby.combative);
+	combative g_combative("riotmode", "Riot Mode", "Make nearby peds attack eachother", g.world.nearby.combative);
 
 }
