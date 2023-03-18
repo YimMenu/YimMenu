685a72cd6f3efaa9dc586dcfdf190ecd9bdf7f2b
Seanghost117 <36561158+Seanghost117@users.noreply.github.com>
2023-03-18 09:19:58 -0700

    all dat for one bracket
    thats so CRAZY

--- src/backend/looped/weapons/increase_damage.cpp
+++ src/backend/looped/weapons/increase_damage.cpp
@@ -10,7 +10,8 @@ namespace big
 
 		virtual void on_tick() override
 		{
-			if (g.weapons.increased_damage != 1) {
+			if (g.weapons.increased_damage != 1) 
+			{
 				Hash weapon{}; 
 				WEAPON::GET_CURRENT_PED_WEAPON(self::ped, &weapon, 0);
 				WEAPON::SET_WEAPON_DAMAGE_MODIFIER(weapon, g.weapons.increased_damage);
