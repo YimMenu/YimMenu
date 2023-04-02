7c1a4542aee55546356091f5d8482d0bca70bb3e
Yimura <24669514+Yimura@users.noreply.github.com>
2023-04-02 23:47:30 +0200

    chore: imgui remove random space

--- scripts/imgui.cmake
+++ scripts/imgui.cmake
@@ -18,7 +18,7 @@ if(NOT imgui_POPULATED)
     )
 
     add_library(imgui STATIC ${SRC_IMGUI} )
-    source_group(TREE ${imgui_SOURCE_DIR} PREFIX "imgui" FILES ${SRC_IMGUI} )
+    source_group(TREE ${imgui_SOURCE_DIR} PREFIX "imgui" FILES ${SRC_IMGUI})
     target_include_directories(imgui PRIVATE
         "${imgui_SOURCE_DIR}"
         "${imgui_SOURCE_DIR}/backends"
