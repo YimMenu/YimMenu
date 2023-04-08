048ce493cd9a6bd7fe1e3a79867fd514a7aa61d5
DayibBaba <79384354+DayibBaba@users.noreply.github.com>
2023-04-08 12:52:48 +0200

    Refactor

--- src/services/locals/locals_service.cpp
+++ src/services/locals/locals_service.cpp
@@ -1,31 +1,33 @@
 #include "locals_service.hpp"
-#include "pointers.hpp"
+
 #include "core/data/all_script_names.hpp"
+#include "pointers.hpp"
 
-namespace big{
+namespace big
+{
 
-    bool locals_service::is_script_thread_running(GtaThread* thread){
-        if(thread)
-        {
-            return thread->m_context.m_state == rage::eThreadState::running || thread->m_context.m_state == rage::eThreadState::idle;
-        }
-        return false;
-    }
+	bool locals_service::is_script_thread_running(GtaThread* thread)
+	{
+		if (thread)
+		{
+			return thread->m_context.m_state == rage::eThreadState::running || thread->m_context.m_state == rage::eThreadState::idle;
+		}
+		return false;
+	}
 
-    bool locals_service::does_script_exist(std::string script_name){
-        for(auto s : all_script_names)
-            if(script_name == s) return true;
-        return false;
-    }
+	bool locals_service::does_script_exist(std::string script_name)
+	{
+		for (auto s : all_script_names)
+			if (script_name == s)
+				return true;
+		return false;
+	}
 
-	GtaThread* locals_service::find_script_thread (Hash hash)
+	GtaThread* locals_service::find_script_thread(Hash hash)
 	{
 		for (auto thread : *g_pointers->m_script_threads)
 		{
-			if (thread
-				&& thread->m_context.m_thread_id
-				&& thread->m_handler
-				&& thread->m_script_hash == hash)
+			if (thread && thread->m_context.m_thread_id && thread->m_handler && thread->m_script_hash == hash)
 			{
 				return thread;
 			}
@@ -34,7 +36,7 @@ namespace big{
 		return nullptr;
 	}
 
-    bool locals_service::load()
+	bool locals_service::load()
 	{
 		std::string path = std::getenv("appdata");
 		path += this->file_location;
@@ -49,20 +51,25 @@ namespace big{
 			nlohmann::json j;
 			file >> j;
 			this->m_locals.clear();
-			for(const auto& l : j.items()){
-				if(!l.key().empty()){
+			for (const auto& l : j.items())
+			{
+				if (!l.key().empty())
+				{
 					local new_local{"", "", 0, 0, 0, 0};
-					new_local.m_base_address = j[l.key()]["base_address"];
+					new_local.m_base_address       = j[l.key()]["base_address"];
 					new_local.m_script_thread_name = j[l.key()]["script_thread_name"];
-					new_local.m_freeze = j[l.key()]["freeze"];
-					new_local.m_name = j[l.key()]["name"];
-					new_local.m_value = j[l.key()]["value"];
-					if(!j[l.key()]["offsets"].is_null()){
-						for(const auto& offset : j[l.key()]["offsets"].items()){
-							if(!offset.key().empty()){
-								local_offset new_offset{0,0};
+					new_local.m_freeze             = j[l.key()]["freeze"];
+					new_local.m_name               = j[l.key()]["name"];
+					new_local.m_value              = j[l.key()]["value"];
+					if (!j[l.key()]["offsets"].is_null())
+					{
+						for (const auto& offset : j[l.key()]["offsets"].items())
+						{
+							if (!offset.key().empty())
+							{
+								local_offset new_offset{0, 0};
 								new_offset.m_offset = j[l.key()]["offsets"][offset.key()]["offset"];
-								if(!j[l.key()]["offsets"][offset.key()]["size"].is_null())
+								if (!j[l.key()]["offsets"][offset.key()]["size"].is_null())
 									new_offset.m_size = j[l.key()]["offsets"][offset.key()]["size"];
 								new_local.m_offsets.push_back(new_offset);
 							}
@@ -84,26 +91,28 @@ namespace big{
 
 	void locals_service::save()
 	{
-        std::map<std::string, local> locals_with_names;
-        for (auto& l : m_locals){
-            locals_with_names.insert(std::pair<std::string, local>(
-                l.m_name.empty() ?  std::string(l.m_script_thread_name + "_" + std::to_string(l.m_base_address)) : l.m_name, l));
-        }
+		std::map<std::string, local> locals_with_names;
+		for (auto& l : m_locals)
+		{
+			locals_with_names.insert(std::pair<std::string, local>(l.m_name.empty() ? std::string(l.m_script_thread_name + "_" + std::to_string(l.m_base_address)) : l.m_name, l));
+		}
 
 		nlohmann::json j;
-        for(auto& l : locals_with_names){
-            j[l.first]["script_thread_name"] = l.second.m_script_thread_name;
-            j[l.first]["base_address"] = l.second.m_base_address;
-            j[l.first]["freeze"] = l.second.m_freeze;
-            j[l.first]["name"] = l.second.m_name;
-            j[l.first]["value"] = l.second.m_value;
+		for (auto& l : locals_with_names)
+		{
+			j[l.first]["script_thread_name"] = l.second.m_script_thread_name;
+			j[l.first]["base_address"]       = l.second.m_base_address;
+			j[l.first]["freeze"]             = l.second.m_freeze;
+			j[l.first]["name"]               = l.second.m_name;
+			j[l.first]["value"]              = l.second.m_value;
 
-            for (int i = 0; i < l.second.m_offsets.size(); i++)
-            {
-                j[l.first]["offsets"][std::to_string(i)]["offset"] = l.second.m_offsets[i].m_offset;
-                if(l.second.m_offsets[i].m_size > 0) j[l.first]["offsets"][std::to_string(i)]["size"] = l.second.m_offsets[i].m_size;
-            };
-        }
+			for (int i = 0; i < l.second.m_offsets.size(); i++)
+			{
+				j[l.first]["offsets"][std::to_string(i)]["offset"] = l.second.m_offsets[i].m_offset;
+				if (l.second.m_offsets[i].m_size > 0)
+					j[l.first]["offsets"][std::to_string(i)]["size"] = l.second.m_offsets[i].m_size;
+			};
+		}
 
 		std::string path = std::getenv("appdata");
 		path += this->file_location;
--- src/services/locals/locals_service.hpp
+++ src/services/locals/locals_service.hpp
@@ -1,12 +1,13 @@
 #pragma once
 
-#include "script_local.hpp"
 #include "gta/script_thread.hpp"
+#include "script_local.hpp"
 
-namespace big{
+namespace big
+{
 
-    struct local_offset{
-
+	struct local_offset
+	{
 		local_offset(int offset, int size = 0)
 		{
 			m_offset = offset;
@@ -17,28 +18,29 @@ namespace big{
 
 		int m_offset = 0;
 		int m_size   = 0;
-    };
+	};
 
 
-    struct local{
-        GtaThread* m_script_thread;
-        std::string m_script_thread_name;
-        int m_base_address;
+	struct local
+	{
+		GtaThread* m_script_thread;
+		std::string m_script_thread_name;
+		int m_base_address;
 		bool m_freeze = false;
 		std::string m_name;
 		std::vector<local_offset> m_offsets;
 		int m_value;
-        int m_freeze_value;
+		int m_freeze_value;
 
 		local(const char* script_thread_name, const char* name, const int base_address, const bool freeze, const int (*offsets)[2], int offset_count)
 		{
 			m_internal_id = ++m_instance_count;
 
-            m_script_thread_name = script_thread_name;
-			m_base_address = base_address;
-			m_freeze       = freeze;
-			m_name         = std::string(name);
-			m_value        = 0;
+			m_script_thread_name = script_thread_name;
+			m_base_address       = base_address;
+			m_freeze             = freeze;
+			m_name               = std::string(name);
+			m_value              = 0;
 
 			for (int i = 0; i < offset_count; i++)
 				m_offsets.push_back(local_offset(offsets[i][0], offsets[i][1]));
@@ -49,14 +51,13 @@ namespace big{
 			return m_internal_id;
 		}
 
-        private:
+	private:
 		inline static int m_instance_count;
-
 		int m_internal_id;
-    };
-    
+	};
 
-    class locals_service
+
+	class locals_service
 	{
 		const char* file_location = "\\BigBaseV2\\locals.json";
 
@@ -65,9 +66,9 @@ namespace big{
 		void loop();
 		void save();
 
-        static bool does_script_exist(std::string script);
-        static GtaThread* find_script_thread(Hash hash);
-        static bool is_script_thread_running(GtaThread* thread);
+		static bool does_script_exist(std::string script);
+		static GtaThread* find_script_thread(Hash hash);
+		static bool is_script_thread_running(GtaThread* thread);
 
 		std::vector<local> m_locals;
 		bool m_running = false;
