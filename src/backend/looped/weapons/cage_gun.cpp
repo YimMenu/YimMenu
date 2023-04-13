f575a3f6ac18e183a6f6866926399377e805b637
Rxann <80224521+Rxann@users.noreply.github.com>
2023-04-13 17:29:34 -0400

    fix small problem

--- src/backend/looped/weapons/cage_gun.cpp
+++ src/backend/looped/weapons/cage_gun.cpp
@@ -7,8 +7,6 @@ namespace big
 {
 	void looped::weapons_cage_gun()
 	{
-		const bool bCageGun = g.weapons.custom_weapon == CustomWeapon::CAGE_GUN;
-
 		if (const bool bCageGun = g.weapons.custom_weapon == CustomWeapon::CAGE_GUN; bCageGun)
 		{
 			if (PAD::IS_DISABLED_CONTROL_PRESSED(0, (int)ControllerInputs::INPUT_AIM))
