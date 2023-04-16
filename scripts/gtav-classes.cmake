e3b1e2e86201f05750278947eed35c26348f20da
DayibBaba <79384354+DayibBaba@users.noreply.github.com>
2023-04-16 20:09:11 +0200

    Update gtav-classes.cmake

--- scripts/gtav-classes.cmake
+++ scripts/gtav-classes.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
     gtav_classes
     GIT_REPOSITORY https://github.com/Yimura/GTAV-Classes.git
-    GIT_TAG        cf7c1a71b413788371000b56d40c4e8b81c47a2c
+    GIT_TAG        57548ccd8742f0355fc674e8f4dedee119d640e5
     GIT_PROGRESS TRUE
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
