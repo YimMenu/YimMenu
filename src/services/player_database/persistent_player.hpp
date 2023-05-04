89f00487ae367ff1249db0d52557d3ce70969f42
binded-zz <binded2@gmail.com>
2023-05-04 16:59:47 -0400

    Update persistent_player.hpp

--- src/services/player_database/persistent_player.hpp
+++ src/services/player_database/persistent_player.hpp
@@ -52,6 +52,7 @@ namespace big
 		int block_join_reason     = 1;
 		bool is_modder            = false;
 		bool is_friends           = false;
+		
 		std::unordered_set<int> infractions;
 		std::optional<CommandAccessLevel> command_access_level = std::nullopt;
 		PlayerOnlineStatus online_state                        = PlayerOnlineStatus::UNKNOWN;
