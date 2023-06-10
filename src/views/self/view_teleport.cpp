33c6ab4e82727e025cd237f69d45f132c03df3f4
DayibBaba <79384354+DayibBaba@users.noreply.github.com>
2023-06-10 13:22:56 +0200

    Formatting

--- src/views/self/view_teleport.cpp
+++ src/views/self/view_teleport.cpp
@@ -25,12 +25,12 @@ namespace big
 
 		static float new_location[3];
 		static float increment = 1;
-		
+
 		components::small_text("Custom teleport");
 		ImGui::SetNextItemWidth(200);
 		ImGui::InputFloat3("##Customlocation", new_location);
 		ImGui::SameLine();
-		components::button("Teleport", []{
+		components::button("Teleport", [] {
 			teleport::teleport_player_to_coords(g_player_service->get_self(), {new_location[0], new_location[1], new_location[2]});
 		});
 
@@ -40,22 +40,34 @@ namespace big
 		ImGui::InputFloat("Distance", &increment);
 
 		ImGui::BeginGroup();
-		components::button("Forward", []{teleport::teleport_player_to_coords(g_player_service->get_self(), ENTITY::GET_OFFSET_FROM_ENTITY_IN_WORLD_COORDS(self::ped, 0, increment, 0));});
-		components::button("Backward", []{teleport::teleport_player_to_coords(g_player_service->get_self(), ENTITY::GET_OFFSET_FROM_ENTITY_IN_WORLD_COORDS(self::ped, 0, -increment, 0));});
+		components::button("Forward", [] {
+			teleport::teleport_player_to_coords(g_player_service->get_self(), ENTITY::GET_OFFSET_FROM_ENTITY_IN_WORLD_COORDS(self::ped, 0, increment, 0));
+		});
+		components::button("Backward", [] {
+			teleport::teleport_player_to_coords(g_player_service->get_self(), ENTITY::GET_OFFSET_FROM_ENTITY_IN_WORLD_COORDS(self::ped, 0, -increment, 0));
+		});
 		ImGui::EndGroup();
 
 		ImGui::SameLine();
 
 		ImGui::BeginGroup();
-		components::button("Left", []{teleport::teleport_player_to_coords(g_player_service->get_self(), ENTITY::GET_OFFSET_FROM_ENTITY_IN_WORLD_COORDS(self::ped, -increment, 0, 0));});
-		components::button("Right", []{teleport::teleport_player_to_coords(g_player_service->get_self(), ENTITY::GET_OFFSET_FROM_ENTITY_IN_WORLD_COORDS(self::ped, increment, 0, 0));});
+		components::button("Left", [] {
+			teleport::teleport_player_to_coords(g_player_service->get_self(), ENTITY::GET_OFFSET_FROM_ENTITY_IN_WORLD_COORDS(self::ped, -increment, 0, 0));
+		});
+		components::button("Right", [] {
+			teleport::teleport_player_to_coords(g_player_service->get_self(), ENTITY::GET_OFFSET_FROM_ENTITY_IN_WORLD_COORDS(self::ped, increment, 0, 0));
+		});
 		ImGui::EndGroup();
 
 		ImGui::SameLine();
 
 		ImGui::BeginGroup();
-		components::button("Up", []{teleport::teleport_player_to_coords(g_player_service->get_self(), ENTITY::GET_OFFSET_FROM_ENTITY_IN_WORLD_COORDS(self::ped, 0, 0, increment));});
-		components::button("Down", []{teleport::teleport_player_to_coords(g_player_service->get_self(), ENTITY::GET_OFFSET_FROM_ENTITY_IN_WORLD_COORDS(self::ped, 0, 0, -increment));});
+		components::button("Up", [] {
+			teleport::teleport_player_to_coords(g_player_service->get_self(), ENTITY::GET_OFFSET_FROM_ENTITY_IN_WORLD_COORDS(self::ped, 0, 0, increment));
+		});
+		components::button("Down", [] {
+			teleport::teleport_player_to_coords(g_player_service->get_self(), ENTITY::GET_OFFSET_FROM_ENTITY_IN_WORLD_COORDS(self::ped, 0, 0, -increment));
+		});
 		ImGui::EndGroup();
 
 		ImGui::Separator();
