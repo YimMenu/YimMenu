63ca45dda341b30c91c358127b550ce94ea062f4
DayibBaba <79384354+DayibBaba@users.noreply.github.com>
2023-06-26 15:49:51 +0200

    Removed unnecessary .data() call

--- src/views/players/player/player_toxic.cpp
+++ src/views/players/player/player_toxic.cpp
@@ -47,7 +47,7 @@ namespace big
 			components::player_command_button<"fakeban">(g_player_service->get_selected(), {});
 
 			static int wanted_level;
-			components::small_text("WANTED_LVL"_T.data());
+			components::small_text("WANTED_LVL");
 			ImGui::SliderInt("##wantedlevelslider", &wanted_level, 0, 5);
 			ImGui::SameLine();
 			components::player_command_button<"wanted">(g_player_service->get_selected(), {(uint64_t)wanted_level}, "Set");
