9170e523e2389ffbf3b98cd61e972c5910ab4feb
Quentin E. / iDeath <xiaoxiao921@hotmail.fr>
2023-06-27 02:39:40 +0200

    vector3: to_string

--- src/lua/bindings/vector.hpp
+++ src/lua/bindings/vector.hpp
@@ -5,12 +5,12 @@ namespace lua::vector
 {
 	static void bind(sol::state& state)
 	{
-		// clang-format off
-		state.new_usertype<Vector3>("vec3", sol::constructors<Vector3(float, float, float)>(), 
-		"x", &Vector3::x,
-		"y", &Vector3::y,
-		"z", &Vector3::z
+		//clang-format off
+		state.new_usertype<Vector3>("vec3",
+			sol::constructors<Vector3(float, float, float)>(),
+			"x", &Vector3::x, "y", &Vector3::y, "z", &Vector3::z,
+			"__tostring", &Vector3::to_string
 		);
-		// clang-format on
+		//clang-format on
 	}
 }
