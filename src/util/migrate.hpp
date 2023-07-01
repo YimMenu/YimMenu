c32d429f116268fb5abd6d8d570a01f8b4be2ad6
Yimura <24669514+Yimura@users.noreply.github.com>
2023-07-01 23:12:37 +0200

    fix(migrate): don't attempt to copy if BigBaseV2 folder doesn't exist

--- src/util/migrate.hpp
+++ src/util/migrate.hpp
@@ -8,7 +8,7 @@ namespace big
         std::filesystem::path old_dir = std::getenv("appdata");
         old_dir /= "BigBaseV2";
 
-        if (!exists(current_dir))
+        if (exists(old_dir) && !exists(current_dir))
         {
             LOG(INFO) << "YimMenu folder does not exist, migrating to new folder.";
             std::filesystem::copy(old_dir, current_dir, std::filesystem::copy_options::recursive);
