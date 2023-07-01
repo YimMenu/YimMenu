eaab129b1960f85633a1be94d7753f7ef84fae47
Yimura <24669514+Yimura@users.noreply.github.com>
2023-07-01 22:56:46 +0200

    chore: clang-format

--- src/byte_patch_manager.hpp
+++ src/byte_patch_manager.hpp
@@ -4,12 +4,12 @@ extern "C" std::uint64_t g_sound_overload_ret_addr;
 
 namespace big
 {
-    class byte_patch_manager
-    {
+	class byte_patch_manager
+	{
 	public:
 		byte_patch_manager();
 		~byte_patch_manager();
-    };
+	};
 
-    inline byte_patch_manager* g_byte_patch_manager;
+	inline byte_patch_manager* g_byte_patch_manager;
 }
