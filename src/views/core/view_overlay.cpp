6dc7da4dbeea74610cd1340e3c9962b58623d535
Andreas Maerten <24669514+Yimura@users.noreply.github.com>
2023-07-05 23:54:06 +0200

    Fix windows that shouldn't move (#1617)
    * fix(overlay): don't move while menu closed
    * fix(CmdExecutor): don't move by user input

--- src/views/core/view_overlay.cpp
+++ src/views/core/view_overlay.cpp
@@ -15,7 +15,12 @@ namespace big
 		ImGui::SetNextWindowPos(ImVec2(10.0f, 10.0f), ImGuiCond_FirstUseEver, ImVec2(0.0f, 0.0f));
 		ImGui::SetNextWindowBgAlpha(0.5f);
 
-		if (ImGui::Begin("overlay", nullptr, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoFocusOnAppearing | ImGuiWindowFlags_NoNav))
+		auto window_flags = ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoFocusOnAppearing | ImGuiWindowFlags_NoNav;
+		if (!g_gui->is_open())
+		{
+			window_flags |= ImGuiWindowFlags_NoMouseInputs;
+		}
+		if (ImGui::Begin("overlay", nullptr, window_flags))
 		{
 			ImGui::Text("YimMenu");
 
