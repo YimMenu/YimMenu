65ca3b736ca83a4b5414ef179f56bc494c83ea94
alyxme <62597693+alyxme@users.noreply.github.com>
2023-07-10 16:23:05 +0500

    Loop checkbox for debug animations
    Added Loop checkbox for debug animations

--- src/views/debug/view_debug_animations.cpp
+++ src/views/debug/view_debug_animations.cpp
@@ -7,11 +7,12 @@ namespace big
 {
 	void debug::animations()
 	{
+		static bool loopCHKBX = false;
 		if (ImGui::BeginTabItem("Animations"))
 		{
 			static std::string current_dict, current_anim;
 			static std::vector<std::string> selected_dict_anim_list{};
-
+			
 			static auto reload_anim_list = []() -> void {
 				selected_dict_anim_list.clear();
 				auto range = animations::all_anims.equal_range(current_dict);
@@ -27,10 +28,10 @@ namespace big
 				animations::fetch_all_anims();
 			});
 
-			ImGui::SetNextItemWidth(400);
+			ImGui::SetNextItemWidth(800);
 			components::input_text_with_hint("##dictionaryfilter", "Dictionary", &current_dict);
 
-			if (animations::has_anim_list_been_populated() && ImGui::BeginListBox("##dictionaries", ImVec2(400, 200)))
+			if (animations::has_anim_list_been_populated() && ImGui::BeginListBox("##dictionaries", ImVec2(800, 200)))
 			{
 				for (auto& entry : animations::all_dicts)
 				{
@@ -47,29 +48,32 @@ namespace big
 
 				ImGui::EndListBox();
 			}
-
-			if (selected_dict_anim_list.size() > 0 && ImGui::BeginListBox("##animations", ImVec2(400, 200)))
+			ImGui::Checkbox("Loop", &loopCHKBX);
+			if (selected_dict_anim_list.size() > 0 && ImGui::BeginListBox("##animations", ImVec2(800, 400)))
 			{
 				for (auto& entry : selected_dict_anim_list)
 				{
-					if (ImGui::Selectable(entry.data(), entry == current_anim))
+					if (!loopCHKBX && ImGui::Selectable(entry.data(), entry == current_anim))
 					{
 						current_anim = entry;
-
 						g_fiber_pool->queue_job([=] {
-							TASK::CLEAR_PED_TASKS_IMMEDIATELY(self::ped);
 							ped::ped_play_animation(self::ped, current_dict, current_anim, 4.f, -4.f, -1, 0, 0, false);
-						});
+							});
+					}
+					else if (loopCHKBX && ImGui::Selectable(entry.data(), entry == current_anim))
+					{
+						current_anim = entry;
+						g_fiber_pool->queue_job([=] {
+							ped::ped_play_animation(self::ped, current_dict, current_anim, 4.f, -4.f, -1, 1, 0, false);
+							});
 					}
 				}
 
 				ImGui::EndListBox();
 			}
-
 			components::button("Stop", [] {
 				TASK::CLEAR_PED_TASKS(self::ped);
 			});
-
 			ImGui::EndTabItem();
 		}
 	}
