ac8c9bc65baf3284ba519e671890e14df5e88098
user <email@hostname>
2023-07-11 19:19:30 -0400

    feat(protections): actually fix parachute crash

--- src/hooks/protections/serialize_take_off_ped_variation_task.cpp
+++ src/hooks/protections/serialize_take_off_ped_variation_task.cpp
@@ -5,12 +5,32 @@
 
 namespace big
 {
+	constexpr uint32_t valid_parachute_models[] = {
+	    RAGE_JOAAT("p_parachute_s"),
+	    RAGE_JOAAT("vw_p_para_bag_vine_s"),
+	    RAGE_JOAAT("reh_p_para_bag_reh_s_01a"),
+	    RAGE_JOAAT("xm_prop_x17_scuba_tank"),
+	    RAGE_JOAAT("lts_p_para_bag_pilot2_s"),
+	    RAGE_JOAAT("lts_p_para_bag_lts_s"),
+	    RAGE_JOAAT("p_para_bag_tr_s_01a"),
+	    RAGE_JOAAT("p_para_bag_xmas_s"),
+	};
+
+	bool is_valid_parachute_model(rage::joaat_t hash)
+	{
+		for (auto& model : valid_parachute_models)
+			if (model == hash)
+				return true;
+
+		return false;
+	}
+
 	void hooks::serialize_take_off_ped_variation_task(ClonedTakeOffPedVariationInfo* info, rage::CSyncDataBase* serializer)
 	{
 		g_hooking->get_original<hooks::serialize_take_off_ped_variation_task>()(info, serializer);
-		if (info->m_prop_hash != 0 && info->m_variation_component == 5 && info->m_prop_hash != RAGE_JOAAT("p_parachute_s"))
+		if (!is_valid_parachute_model(info->m_prop_hash))
 		{
-			// notify::crash_blocked(g.m_syncing_player, "invalid parachute"); false positives
+			notify::crash_blocked(g.m_syncing_player, "invalid parachute");
 			info->m_prop_hash = 0;
 		}
 	}
