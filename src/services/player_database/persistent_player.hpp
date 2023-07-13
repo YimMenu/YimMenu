bbcd7ae91411810917159bd312c6d231f6ee2a2b
user <email@hostname>
2023-07-13 19:03:08 -0400

    fix(player_database): default state should be UNKNOWN, not INVALID

--- src/services/player_database/persistent_player.hpp
+++ src/services/player_database/persistent_player.hpp
@@ -50,7 +50,7 @@ namespace big
 		std::unordered_set<int> infractions;
 		std::string notes                                      = "";
 		std::optional<CommandAccessLevel> command_access_level = std::nullopt;
-		GSType session_type                                    = GSType(-1);
+		GSType session_type                                    = GSType(-2);
 		int64_t session_id                                     = -1;
 
 		NLOHMANN_DEFINE_TYPE_INTRUSIVE_WITH_DEFAULT(persistent_player, name, rockstar_id, block_join, block_join_reason, is_modder, notify_online, infractions, notes, command_access_level)
