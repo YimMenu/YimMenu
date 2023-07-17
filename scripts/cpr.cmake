809ab4fa68712f5b79b27f7169d87a4c984381ca
Bugisoft <58910128+BugisoftRSG@users.noreply.github.com>
2023-07-18 00:38:08 +0200

    Update cpr.cmake
    Disables the cpr/curl tests
    
    No more 10 min tests

--- scripts/cpr.cmake
+++ scripts/cpr.cmake
@@ -1,14 +1,19 @@
 include(FetchContent)
 
-set(BUILD_TESTING_BEFORE ${BUILD_TESTING})
-set(CURL_DISABLE_TESTS OFF) 
 FetchContent_Declare(
     cpr
     GIT_REPOSITORY https://github.com/libcpr/cpr.git
     GIT_TAG        1986262ba4e0cb052161e9e7919aef5ef08217f0
     GIT_PROGRESS TRUE
-) 
+)
+  
+FetchContent_GetProperties(cpr)
+if(NOT cpr_POPULATED)
+  FetchContent_Populate(cpr)
+  set(BUILD_CPR_TESTS OFF CACHE INTERNAL "")
+  set(BUILD_CPR_TESTS_SSL OFF CACHE INTERNAL "")
+  set(BUILD_CPR_TESTS_HTTP2 OFF CACHE INTERNAL "")
+endif()
+ 
 message("cpr")
 FetchContent_MakeAvailable(cpr)
-
-set(BUILD_TESTING ${BUILD_TESTING_BEFORE} CACHE INTERNAL "" FORCE)
