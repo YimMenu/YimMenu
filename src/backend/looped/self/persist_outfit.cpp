1d7838851972a0c7e1d2acd2c57ef617337d4def
gir489 <100792176+gir489returns@users.noreply.github.com>
2023-07-18 04:16:26 -0400

    Custom teleport added euler angles and added persist outfit (#1743)
    * Addressed #1694: Added Euler angles to telelocation JSON object.
    * Added Persist Outfits to resolve #1669

---
+++ src/backend/looped/self/persist_outfit.cpp
@@ -0,0 +1,37 @@
+#include "backend/looped/looped.hpp"
+#include "services/outfit/outfit_service.hpp"
+#include "natives.hpp"
+#include "file_manager.hpp"
+#include "logger/logger.hpp"
+
+namespace big
+{
+	void looped::self_persist_outfit()
+	{
+		if (g.self.persist_outfit.empty())
+			return; //Off
+
+		if (g_local_player == nullptr || g_local_player->m_player_info == nullptr || g_local_player->m_player_info->m_game_state == eGameState::InMPCutscene || STREAMING::IS_PLAYER_SWITCH_IN_PROGRESS() || DLC::GET_IS_LOADING_SCREEN_ACTIVE())
+			return; //Dead or Loading
+
+		if (PED::GET_PED_DRAWABLE_VARIATION(self::ped, 5) == 0 && PED::GET_PED_DRAWABLE_VARIATION(self::ped, 4) >= 14 && PED::GET_PED_DRAWABLE_VARIATION(self::ped, 4) <= 18)
+			return; //Showering
+
+		if (g.self.persist_outfits_mis && NETWORK::NETWORK_IS_ACTIVITY_SESSION())
+			return; //Missioning it up
+
+		static nlohmann::json outfit{};
+		static std::string persisting_outfit = "";
+
+		if (persisting_outfit != g.self.persist_outfit)
+		{
+			persisting_outfit        = g.self.persist_outfit;
+			folder saved_outfit_path = g_file_manager.get_project_folder("saved_outfits");
+			std::ifstream i(saved_outfit_path.get_file(persisting_outfit).get_path());
+			outfit.clear();
+			i >> outfit;
+		}
+
+		outfit_service::apply_outfit(outfit, false);
+	}
+}
