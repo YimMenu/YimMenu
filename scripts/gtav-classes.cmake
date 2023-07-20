eca59af3d9d0d370216052bdc77b311ebfcd5bf2
Bugisoft <58910128+BugisoftRSG@users.noreply.github.com>
2023-07-20 18:43:45 +0200

    bump gtav-classes

--- scripts/gtav-classes.cmake
+++ scripts/gtav-classes.cmake
@@ -2,8 +2,8 @@ include(FetchContent)
 
 FetchContent_Declare(
     gtav_classes
-    GIT_REPOSITORY https://github.com/BugisoftRSG/GTAV-Classes.git
-    GIT_TAG        a179997ee5facadfede007967ab9c99379e69f0e
+    GIT_REPOSITORY https://github.com/YimMenu/GTAV-Classes.git
+    GIT_TAG        7ff656ae2b733bcf2bc621592f602654403eba67
     GIT_PROGRESS TRUE
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
