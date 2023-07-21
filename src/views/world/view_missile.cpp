baae0ae578af4b8a02ecb6439aa44328268e38a2
Rxann <80224521+Rxann@users.noreply.github.com>
2023-07-20 20:06:21 -0400

    fix issues

--- src/views/world/view_missile.cpp
+++ src/views/world/view_missile.cpp
@@ -19,25 +19,28 @@ namespace big
 			auto self = g_player_service->get_self();
 			if (ImGui::Selectable(self->get_name(), self->id() == victim->id()))
 				victim = self;
-			for (auto p : g_player_service->players() | std::ranges::views::values)
+			for (auto player : g_player_service->players() | std::ranges::views::values)
 			{
-				if (ImGui::Selectable(p->get_name(), p->id() == victim->id()))
+				if (ImGui::Selectable(player->get_name(), player->id() == victim->id()))
 				{
-					victim = p;
+					victim = player;
 					if (g.player.spectating)
+					{
 						g_player_service->set_selected(victim);
+					}
 				}
 			}
 			ImGui::EndCombo();
 		}
 		if (ImGui::Button("Launch"))
 		{
-			auto target = g_pointers->m_gta.m_ptr_to_handle(victim->get_ped());
-			auto firer  = self::ped;
-			g_fiber_pool->queue_job([] {
-				auto& pos = *victim->get_ped()->get_position();
-				MISC::SHOOT_SINGLE_BULLET_BETWEEN_COORDS(pos.x, pos.y, pos.z + 100.0f, pos.x, pos.y, pos.z, 9999, true, RAGE_JOAAT("VEHICLE_WEAPON_SPACE_ROCKET"), self::ped, true, false, 90.0f);
-			});
+			if (victim != nullptr)
+			{
+				g_fiber_pool->queue_job([] {
+					auto& pos = *victim->get_ped()->get_position();
+					MISC::SHOOT_SINGLE_BULLET_BETWEEN_COORDS(pos.x, pos.y, pos.z + 100.0f, pos.x, pos.y, pos.z, 9999, true, RAGE_JOAAT("VEHICLE_WEAPON_SPACE_ROCKET"), self::ped, true, false, 90.0f);
+				});
+			}
 		}
 	}
 
