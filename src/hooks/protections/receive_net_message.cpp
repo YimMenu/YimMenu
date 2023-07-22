ee699afb062753a37a293bd0aaa210b64f39e40d
user <email@hostname>
2023-07-22 13:37:54 -0400

    feat(protections): add protections for our new kick

--- src/hooks/protections/receive_net_message.cpp
+++ src/hooks/protections/receive_net_message.cpp
@@ -229,6 +229,25 @@ namespace big
 
 				break;
 			}
+			case rage::eNetMessage::MsgRadioStationSyncRequest:
+			{
+				if (player->block_radio_requests)
+					return true;
+
+				if (player->m_radio_request_rate_limit.process())
+				{
+					if (player->m_radio_request_rate_limit.exceeded_last_process())
+					{
+						session::add_infraction(player, Infraction::TRIED_KICK_PLAYER);
+						g_notification_service->push_error("PROTECTIONS"_T.data(),
+						    std::vformat("OOM_KICK"_T, std::make_format_args(player->get_name())));
+						player->block_radio_requests = true;
+					}
+					return true;
+				}
+
+				break;
+			}
 			}
 		}
 		else
@@ -236,6 +255,7 @@ namespace big
 			switch (msgType)
 			{
 			case rage::eNetMessage::MsgScriptMigrateHost: return true;
+			case rage::eNetMessage::MsgRadioStationSyncRequest: return true;
 			}
 		}
 
