372684e1147dde464d4cf89f619184444a3d2c38
Yimura <24669514+Yimura@users.noreply.github.com>
2023-07-23 22:19:43 +0200

    fix: formatting

--- src/backend/commands/system/window_hook.cpp
+++ src/backend/commands/system/window_hook.cpp
@@ -8,25 +8,29 @@ namespace big
 	{
 		using looped_command::looped_command;
 
-        memory::byte_patch* m_window_hook_patch;
+		memory::byte_patch* m_window_hook_patch;
 
 		virtual void on_enable() override
 		{
-            static auto patch = (m_window_hook_patch = memory::byte_patch::make(g_pointers->m_gta.m_window_hook.as<void*>(), std::to_array({0xC3, 0x90, 0x90, 0x90})).get(), true);
-		
-            m_window_hook_patch->apply();
-            UnhookWindowsHookEx(*g_pointers->m_gta.m_window_hook.add(45).rip().as<HHOOK*>());
-        }
+			static auto patch = (m_window_hook_patch = memory::byte_patch::make(g_pointers->m_gta.m_window_hook.as<void*>(), std::to_array({0xC3, 0x90, 0x90, 0x90}))
+			                                               .get(),
+			    true);
+
+			m_window_hook_patch->apply();
+			UnhookWindowsHookEx(*g_pointers->m_gta.m_window_hook.add(45).rip().as<HHOOK*>());
+		}
 
 		virtual void on_tick() override
-		{}
+		{
+		}
 
 		virtual void on_disable() override
 		{
-            SetWindowsHookExA(13, g_pointers->m_gta.m_window_hook.add(18).rip().as<HOOKPROC>(), GetModuleHandleA("GTA5.exe"), 0);
-            m_window_hook_patch->restore();
+			SetWindowsHookExA(13, g_pointers->m_gta.m_window_hook.add(18).rip().as<HOOKPROC>(), GetModuleHandleA("GTA5.exe"), 0);
+			m_window_hook_patch->restore();
 		}
 	};
 
-	window_hook g_window_hook("windowhook", "GTA Window Hook", "Only enable this if you know what you are doing, this will prevent AHK scripts from working.", g.debug.window_hook);
+	window_hook g_window_hook("windowhook", "GTA Window Hook", "Only enable this if you know what you are doing, this will prevent AHK scripts from working.",
+	    g.debug.window_hook);
 }
