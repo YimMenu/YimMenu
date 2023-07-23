c71e0b292cfd5ad735208017544cc0b7cfb02a5f
Rxann <80224521+Rxann@users.noreply.github.com>
2023-07-23 16:29:47 -0400

    update + rename

--- src/backend/commands/session/wipe_session.cpp
+++ src/backend/commands/session/wipe_session.cpp
@@ -1,27 +1,21 @@
 #include "backend/player_command.hpp"
+#include "network/CNetworkPlayerMgr.hpp"
+#include "pointers.hpp"
 #include "services/players/player_service.hpp"
 
 namespace big
 {
-	class wipe_session : command
+	class empty_session : command
 	{
 		using command::command;
 		virtual void execute(const std::vector<uint64_t>&, const std::shared_ptr<command_context> ctx)
 		{
-			g_notification_service->push_warning("Kick All Players From Session", "Starting...");
-			g_player_service->iterate([](auto& plyr) {
-				if (g_player_service->get_self()->is_host())
-				{
-					dynamic_cast<player_command*>(command::get(RAGE_JOAAT("breakup")))->call(plyr.second, {});
-				}
-				else
-				{
-					dynamic_cast<player_command*>(command::get(RAGE_JOAAT("nfkick")))->call(plyr.second, {});
-					dynamic_cast<player_command*>(command::get(RAGE_JOAAT("endkick")))->call(plyr.second, {});
-				}
+			g_player_service->iterate([](const player_entry& player) {
+				auto mgr = *g_pointers->m_gta.m_network_player_mgr;
+				mgr->RemovePlayer(player.second->get_net_game_player());
+				g_notification_service->push_success("Empty Session", "Completed");
 			});
-			g_notification_service->push_success("Kick All Players From Session", "Completed!");
 		}
 	};
-	wipe_session g_wipe_session("kicksession", "Kick All Players From Session", "Kicks everyone from the lobby", 0);
+	empty_session g_empty_session("emptysession", "Remove All Players From Session", "Removes everyone from the session", 0);
 }
