fe29abba6bf6d9c808f2a4293ad094aeab646668
Yimura <24669514+Yimura@users.noreply.github.com>
2023-07-24 23:29:30 +0200

    feat(Commands): added float command class

--- src/gui/components/components.hpp
+++ src/gui/components/components.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "backend/command.hpp"
+#include "backend/float_command.hpp"
 #include "backend/int_command.hpp"
 #include "backend/looped_command.hpp"
 #include "backend/player_command.hpp"
@@ -89,6 +90,22 @@ namespace big
 				return ImGui::Text("INVALID COMMAND");
 
 			ImGui::SliderInt(label_override.value_or(command->get_label()).data(),
+			    &command->get_value(),
+			    command->get_lower_bound(),
+			    command->get_upper_bound());
+
+			if (ImGui::IsItemHovered())
+				ImGui::SetTooltip(command->get_description().c_str());
+		}
+
+		template<template_str cmd_str>
+		static void command_float_slider(std::optional<const std::string_view> label_override = std::nullopt)
+		{
+			static float_command* command = (float_command*)command::get(rage::consteval_joaat(cmd_str.value));
+			if (command == nullptr)
+				return ImGui::Text("INVALID COMMAND");
+
+			ImGui::SliderFloat(label_override.value_or(command->get_label()).data(),
 			    &command->get_value(),
 			    command->get_lower_bound(),
 			    command->get_upper_bound());
