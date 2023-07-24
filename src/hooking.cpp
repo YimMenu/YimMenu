e93eb69dd8cba43ef78ef57a097738b0444179e5
Quentin E. / iDeath <xiaoxiao921@hotmail.fr>
2023-07-24 02:05:31 +0200

    refactor(detour_hook): don't use detour hook pointers, should speed up hooks ticking (skip a deref compared to before)

--- src/hooking.cpp
+++ src/hooking.cpp
@@ -26,7 +26,7 @@ namespace big
 		// aka the ones that still don't have their m_target assigned
 		for (auto& detour_hook_helper : m_detour_hook_helpers)
 		{
-			detour_hook_helper->m_detour_hook->set_target_and_create_hook(detour_hook_helper->m_on_hooking_available());
+			detour_hook_helper.m_detour_hook->set_target_and_create_hook(detour_hook_helper.m_on_hooking_available());
 		}
 
 		detour_hook_helper::add<hooks::run_script_threads>("SH", g_pointers->m_gta.m_run_script_threads);
@@ -140,9 +140,9 @@ namespace big
 		m_swapchain_hook.enable();
 		m_og_wndproc = WNDPROC(SetWindowLongPtrW(g_pointers->m_hwnd, GWLP_WNDPROC, LONG_PTR(&hooks::wndproc)));
 
-		for (const auto& detour_hook_helper : m_detour_hook_helpers)
+		for (auto& detour_hook_helper : m_detour_hook_helpers)
 		{
-			detour_hook_helper->m_detour_hook->enable();
+			detour_hook_helper.m_detour_hook->enable();
 		}
 
 		MH_ApplyQueued();
@@ -154,9 +154,9 @@ namespace big
 	{
 		m_enabled = false;
 
-		for (const auto& detour_hook_helper : m_detour_hook_helpers)
+		for (auto& detour_hook_helper : m_detour_hook_helpers)
 		{
-			detour_hook_helper->m_detour_hook->disable();
+			detour_hook_helper.m_detour_hook->disable();
 		}
 
 		SetWindowLongPtrW(g_pointers->m_hwnd, GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(m_og_wndproc));
@@ -164,16 +164,11 @@ namespace big
 
 		MH_ApplyQueued();
 
-		for (const auto& detour_hook_helper : m_detour_hook_helpers)
-		{
-			delete detour_hook_helper;
-		}
 		m_detour_hook_helpers.clear();
 	}
 
 	hooking::detour_hook_helper::~detour_hook_helper()
 	{
-		delete m_detour_hook;
 	}
 
 	void hooking::detour_hook_helper::enable_hook_if_hooking_is_already_running()
