16e8e571f04b6d9d94f9be8c0d1823dc1088ae31
Quentin <xiaoxiao921@hotmail.fr>
2023-07-24 09:40:28 +0200

    refactor(detour_hook): don't use detour hook pointers, should speed up hooks ticking (skip a deref compared to before) (#1827)

--- src/hooking.hpp
+++ src/hooking.hpp
@@ -192,55 +192,53 @@ namespace big
 
 			ret_ptr_fn m_on_hooking_available = nullptr;
 
-			detour_hook* m_detour_hook = nullptr;
-
-			~detour_hook_helper();
+			detour_hook* m_detour_hook;
 
 			void enable_hook_if_hooking_is_already_running();
 
 			template<auto detour_function>
 			struct hook_to_detour_hook_helper
 			{
-				static inline detour_hook* m_detour_hook;
+				static inline detour_hook m_detour_hook;
 			};
-
-			template<auto detour_function>
-			static detour_hook_helper* add_internal(detour_hook* dh)
-			{
-				auto d           = new detour_hook_helper();
-				d->m_detour_hook = dh;
-
-				m_detour_hook_helpers.push_back(d);
-				hook_to_detour_hook_helper<detour_function>::m_detour_hook = dh;
-
-				return d;
-			}
 
 		public:
 			template<auto detour_function>
 			static void add(const std::string& name, void* target)
 			{
-				auto d = add_internal<detour_function>(new detour_hook(name, target, detour_function));
+				hook_to_detour_hook_helper<detour_function>::m_detour_hook.set_instance(name, target, detour_function);
 
-				d->enable_hook_if_hooking_is_already_running();
+				detour_hook_helper d{};
+				d.m_detour_hook = &hook_to_detour_hook_helper<detour_function>::m_detour_hook;
+
+				d.enable_hook_if_hooking_is_already_running();
+
+				m_detour_hook_helpers.push_back(d);
 			}
 
 			template<auto detour_function>
 			static void* add_lazy(const std::string& name, detour_hook_helper::ret_ptr_fn on_hooking_available)
 			{
-				auto d                    = add_internal<detour_function>(new detour_hook(name, detour_function));
-				d->m_on_hooking_available = on_hooking_available;
+				hook_to_detour_hook_helper<detour_function>::m_detour_hook.set_instance(name, detour_function);
 
-				d->enable_hook_if_hooking_is_already_running();
+				detour_hook_helper d{};
+				d.m_detour_hook          = &hook_to_detour_hook_helper<detour_function>::m_detour_hook;
+				d.m_on_hooking_available = on_hooking_available;
+
+				d.enable_hook_if_hooking_is_already_running();
+
+				m_detour_hook_helpers.push_back(d);
 
 				return nullptr;
 			}
+
+			~detour_hook_helper();
 		};
 
 		template<auto detour_function>
 		static auto get_original()
 		{
-			return detour_hook_helper::hook_to_detour_hook_helper<detour_function>::m_detour_hook->get_original<decltype(detour_function)>();
+			return detour_hook_helper::hook_to_detour_hook_helper<detour_function>::m_detour_hook.get_original<decltype(detour_function)>();
 		}
 
 	private:
@@ -251,7 +249,7 @@ namespace big
 
 		WNDPROC m_og_wndproc = nullptr;
 
-		static inline std::vector<detour_hook_helper*> m_detour_hook_helpers;
+		static inline std::vector<detour_hook_helper> m_detour_hook_helpers;
 	};
 
 	inline hooking* g_hooking{};
