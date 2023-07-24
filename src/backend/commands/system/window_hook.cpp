0289576677dd22f1d7953a897106daa98f609805
Andreas Maerten <24669514+Yimura@users.noreply.github.com>
2023-07-24 16:13:09 +0200

    fix(byte_patch): sometimes the page protections get modified between `apply` and `restore`: make sure the protections are fine for us to memcpy. (#1831)
    * feat(BytePatch): add is_active member function
    * fix(byte_patch): sometimes the page protections get modified between the apply and restore of the byte_patch, make sure the protections are fine for us to memcpy
    
    Co-authored-by: Quentin E. / iDeath <xiaoxiao921@hotmail.fr>

--- src/backend/commands/system/window_hook.cpp
+++ src/backend/commands/system/window_hook.cpp
@@ -8,23 +8,19 @@ namespace big
 	{
 		using bool_command::bool_command;
 
-		memory::byte_patch* m_window_hook_patch;
-
 		virtual void refresh() override
 		{
-			static auto patch = (m_window_hook_patch = memory::byte_patch::make(g_pointers->m_gta.m_window_hook.as<void*>(), std::to_array({0xC3, 0x90, 0x90, 0x90}))
-			                                               .get(),
-			    true);
+			static auto& window_hook_patch = memory::byte_patch::make(g_pointers->m_gta.m_window_hook.as<void*>(), std::to_array({0xC3, 0x90, 0x90, 0x90}));
 
 			if (m_toggle)
 			{
-				m_window_hook_patch->apply();
+				window_hook_patch->apply();
 				UnhookWindowsHookEx(*g_pointers->m_gta.m_window_hook.add(45).rip().as<HHOOK*>());
 			}
 			else
 			{
 				SetWindowsHookExA(13, g_pointers->m_gta.m_window_hook.add(18).rip().as<HOOKPROC>(), GetModuleHandleA("GTA5.exe"), 0);
-				m_window_hook_patch->restore();
+				window_hook_patch->restore();
 			}
 		}
 	};
