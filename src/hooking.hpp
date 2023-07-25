13be3d3ab1fb12b986a106a7411732e369eb4e87
MegaMemeMoment <140447070+MegaMemeMoment@users.noreply.github.com>
2023-07-25 15:52:21 +0200

    fix(hooks): return types (#1834)
    fixes a bunch of return types that were seemingly copied from IDA without manual verification

--- src/hooking.hpp
+++ src/hooking.hpp
@@ -70,7 +70,7 @@ namespace big
 		static bool init_native_tables(rage::scrProgram* program);
 		static rage::eThreadState script_vm(uint64_t* start_stack, uint64_t** scr_globals, rage::scrProgram* program, rage::scrThreadContext* ctx);
 
-		static void network_player_mgr_init(CNetworkPlayerMgr* _this, uint64_t a2, uint32_t a3, uint32_t a4[4]);
+		static bool network_player_mgr_init(CNetworkPlayerMgr* _this, uint64_t a2, uint32_t a3, uint32_t a4[4]);
 		static void network_player_mgr_shutdown(CNetworkPlayerMgr* _this);
 
 		static bool fragment_physics_crash_2(float* a1, float* a2);
@@ -82,21 +82,21 @@ namespace big
 		static bool scripted_game_event(CScriptedGameEvent* scripted_game_event, CNetGamePlayer* player);
 
 		static bool receive_net_message(void* netConnectionManager, void* a2, rage::netConnection::InFrame* frame);
-		static void get_network_event_data(int64_t unk, rage::CEventNetwork* net_event);
+		static rage::CEventNetwork* get_network_event_data(int64_t unk, rage::CEventNetwork* net_event);
 
 		static void* assign_physical_index(CNetworkPlayerMgr* netPlayerMgr, CNetGamePlayer* player, uint8_t new_index);
 
 		//SYNC
-		static bool received_clone_create(CNetworkObjectMgr* mgr, CNetGamePlayer* src, CNetGamePlayer* dst, eNetObjType object_type, int32_t object_id, int32_t object_flag, rage::datBitBuffer* buffer, int32_t timestamp);
+		static void received_clone_create(CNetworkObjectMgr* mgr, CNetGamePlayer* src, CNetGamePlayer* dst, eNetObjType object_type, int32_t object_id, int32_t object_flag, rage::datBitBuffer* buffer, int32_t timestamp);
 		static eAckCode received_clone_sync(CNetworkObjectMgr* mgr, CNetGamePlayer* src, CNetGamePlayer* dst, eNetObjType object_type, uint16_t object_id, rage::datBitBuffer* bufer, uint16_t unk, uint32_t timestamp);
 		static bool can_apply_data(rage::netSyncTree* tree, rage::netObject* object);
 
 		static void write_player_gamer_data_node(rage::netObject* player, CPlayerGamerDataNode* node);
-		static bool write_player_game_state_data_node(rage::netObject* player, CPlayerGameStateDataNode* node);
+		static void write_player_game_state_data_node(rage::netObject* player, CPlayerGameStateDataNode* node);
 
 		static void invalid_mods_crash_detour(int64_t a1, int64_t a2, int a3, char a4);
-		static uint64_t invalid_decal(uintptr_t a1, int a2);
-		static uint64_t task_parachute_object_0x270(uint64_t _this, int a2, int a3);
+		static void invalid_decal(uintptr_t a1, int a2);
+		static int task_parachute_object_0x270(uint64_t _this, int a2, int a3);
 
 		static bool update_presence_attribute_int(void* presence_data, int profile_index, char* attr, uint64_t value);
 		static bool update_presence_attribute_string(void* presence_data, int profile_index, char* attr, char* value);
@@ -122,13 +122,13 @@ namespace big
 
 		static int nt_query_virtual_memory(void* _this, HANDLE handle, PVOID base_addr, int info_class, MEMORY_BASIC_INFORMATION* info, int size, size_t* return_len);
 		static void queue_dependency(void* dependency);
-		static void prepare_metric_for_sending(rage::datBitBuffer* bit_buffer, int unk, int time, rage::rlMetric* metric);
+		static bool prepare_metric_for_sending(rage::datBitBuffer* bit_buffer, int unk, int time, rage::rlMetric* metric);
 
 		static bool received_array_update(rage::netArrayHandlerBase* array, CNetGamePlayer* sender, rage::datBitBuffer* buffer, int size, int16_t cycle);
 
 		static bool receive_pickup(rage::netObject* netobject, void* unk, CPed* ped);
 
-		static bool write_player_camera_data_node(rage::netObject* player, CPlayerCameraDataNode* node);
+		static void write_player_camera_data_node(rage::netObject* player, CPlayerCameraDataNode* node);
 
 		static rage::netGameEvent* send_player_card_stats(rage::netGameEvent* a1, CPlayerCardStats* stats);
 		static void serialize_stats(CStatsSerializationContext* context, rage::joaat_t* stats, uint32_t stat_count);
@@ -136,7 +136,7 @@ namespace big
 		static void write_player_creation_data_node(rage::netObject* player, CPlayerCreationDataNode* node);
 		static void write_player_appearance_data_node(rage::netObject* player, CPlayerAppearanceDataNode* node);
 
-		static void task_jump_constructor(uint64_t a1, int a2);
+		static __int64 task_jump_constructor(uint64_t a1, int a2);
 
 		static CBaseModelInfo* get_model_info(rage::joaat_t hash, uint32_t* a2);
 
@@ -147,11 +147,11 @@ namespace big
 
 		static int netfilter_handle_message(__int64 filter, char* message, int flags);
 
-		static void log_error_message_box(rage::joaat_t joaated_error_code, char a2);
+		static void log_error_message_box(rage::joaat_t joaated_error_code, bool a2);
 
-		static void send_non_physical_player_data(CNetGamePlayer* player, __int64 message, int flags, void* a4, CNetGamePlayer* a5);
+		static bool send_non_physical_player_data(CNetGamePlayer* player, __int64 message, int flags, void* a4, CNetGamePlayer* a5);
 
-		static int64_t update_timecycle_keyframe_data(int64_t timecycleManager, TimecycleKeyframeData* timecycleKeyframeData);
+		static void update_timecycle_keyframe_data(int64_t timecycleManager, TimecycleKeyframeData* timecycleKeyframeData);
 
 		static void* allocate_memory_reliable(rage::netConnection* cxn, int required_memory);
 
--- src/hooks/protections/send_non_physical_player_data.cpp
+++ src/hooks/protections/send_non_physical_player_data.cpp
@@ -7,7 +7,7 @@
 
 namespace big
 {
-	void hooks::send_non_physical_player_data(CNetGamePlayer* player, __int64 message, int flags, void* a4, CNetGamePlayer* a5)
+	bool hooks::send_non_physical_player_data(CNetGamePlayer* player, __int64 message, int flags, void* a4, CNetGamePlayer* a5)
 	{
 		auto plyr         = g_player_service->get_by_id(player->m_player_id);
 		auto data         = *(CNonPhysicalPlayerData**)(message + 0x10);
@@ -19,8 +19,10 @@ namespace big
 			g_notification_service->push("BLOCK_JOIN"_T.data(), std::vformat("BLOCK_JOIN_PREVENT_PLAYER_JOIN"_T, std::make_format_args(plyr->get_name())));
 		}
 
-		g_hooking->get_original<hooks::send_non_physical_player_data>()(player, message, flags, a4, a5);
+		bool result = g_hooking->get_original<hooks::send_non_physical_player_data>()(player, message, flags, a4, a5);
 
 		data->m_bubble_id = old_bubble_id;
+
+		return result;
 	}
 }
