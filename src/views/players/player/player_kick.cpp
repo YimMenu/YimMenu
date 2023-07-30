45078d6a5d821f5daa849e11ea427e1e3a4ddf97
Niuu <patryk230206@gmail.com>
2023-07-30 18:03:15 +0200

    crash function with invalid mod hakuchou2

--- src/views/players/player/player_kick.cpp
+++ src/views/players/player/player_kick.cpp
@@ -18,7 +18,7 @@ namespace big
 				ImGui::Text("Host and breakup kick require session host");
 
 			ImGui::BeginDisabled(!g_player_service->get_self()->is_host());
-
+			
 			components::player_command_button<"hostkick">(g_player_service->get_selected());
 			components::player_command_button<"breakup">(g_player_service->get_selected());
 
@@ -38,6 +38,8 @@ namespace big
 			components::player_command_button<"endkick">(g_player_service->get_selected());
 			ImGui::SameLine();
 			components::player_command_button<"desync">(g_player_service->get_selected());
+			ImGui::SameLine();
+			components::player_command_button<"crash">(g_player_service->get_selected());
 
 			ImGui::EndListBox();
 		}
