b7532faa2a31be6f4e6389872df7aa10c32befea
gir489returns <gir489returns@outlook.com>
2023-08-05 15:30:29 -0400

    Finalized code for KEEP_VEHICLE_FIXED.

--- scripts/gtav-classes.cmake
+++ scripts/gtav-classes.cmake
@@ -2,8 +2,8 @@ include(FetchContent)
 
 FetchContent_Declare(
     gtav_classes
-    GIT_REPOSITORY https://github.com/gir489returns/GTAV-Classes.git
-    GIT_TAG        2b5023e50b83077ceef504f449372ec806f0ef69
+    GIT_REPOSITORY https://github.com/Yimura/GTAV-Classes.git
+    GIT_TAG        9da68ec82769c684068023c00ac5e5acd7836c97
     GIT_PROGRESS TRUE
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
--- src/util/vehicle.hpp
+++ src/util/vehicle.hpp
@@ -160,26 +160,7 @@ namespace big::vehicle
 			return false;
 		}
 
-		if (VEHICLE::IS_VEHICLE_BUMPER_BOUNCING(veh, TRUE) || VEHICLE::GET_VEHICLE_NUM_OF_BROKEN_OFF_PARTS(veh) > 0)
-		{
-			VEHICLE::SET_VEHICLE_FIXED(veh);
-		}
-
-		if (!VEHICLE::IS_VEHICLE_WINDOW_INTACT(veh, 7)) //Rear window
-			VEHICLE::FIX_VEHICLE_WINDOW(veh, 7);
-
-		if (VEHICLE::GET_DOES_VEHICLE_HAVE_DAMAGE_DECALS(veh))
-		{
-			int clan_logo_counter = GRAPHICS::DOES_VEHICLE_HAVE_CREW_EMBLEM(veh, 0) * 100;
-			GRAPHICS::REMOVE_DECALS_FROM_VEHICLE(veh);
-			while (clan_logo_counter-- > 0 && !GRAPHICS::DOES_VEHICLE_HAVE_CREW_EMBLEM(veh, 0))
-			{
-				vehicle_helper::add_clan_logo_to_vehicle(veh, self::ped);
-				script::get_current()->yield(10ms);
-			}
-		}
-
-		VEHICLE::SET_VEHICLE_DEFORMATION_FIXED(veh);
+		VEHICLE::SET_VEHICLE_FIXED(veh);
 		VEHICLE::SET_VEHICLE_DIRT_LEVEL(veh, 0.f);
 
 		return true;
