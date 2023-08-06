e1c0f97cd3424e8fca885bafd54196de5d247dc5
gir489 <100792176+gir489returns@users.noreply.github.com>
2023-08-06 04:36:48 -0400

    Fixed KERS boost not being handled by vehicle_boost_behavior. (#1922)

--- scripts/gtav-classes.cmake
+++ scripts/gtav-classes.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
     gtav_classes
     GIT_REPOSITORY https://github.com/Yimura/GTAV-Classes.git
-    GIT_TAG        468161a36d95a355d9783eef5b245f3f1542e2bb
+    GIT_TAG        9da68ec82769c684068023c00ac5e5acd7836c97
     GIT_PROGRESS TRUE
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
