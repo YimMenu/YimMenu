ef3354f641cc0ff73f31f96ce965966a456947d5
gir489returns <gir489returns@outlook.com>
2023-08-06 14:06:23 -0400

    Fixed regression issue with rocket boosting while using Hold for Boost scenario.

--- src/backend/looped/vehicle/boost_behavior.cpp
+++ src/backend/looped/vehicle/boost_behavior.cpp
@@ -36,22 +36,19 @@ namespace big
 			{
 				if (PAD::IS_CONTROL_PRESSED(0, (int)ControllerInputs::INPUT_VEH_ROCKET_BOOST) || PAD::IS_CONTROL_PRESSED(0, (int)ControllerInputs::INPUT_VEH_FLY_BOOST))
 				{
-					if (is_rocket)
+					if (is_rocket && vehicle->m_boost_state)
 					{
-						if (vehicle->m_boost_state)
-						{
-							vehicle->m_boost_allow_recharge = true;
-							vehicle->m_boost                = 3.f;
-						}
-						else if (vehicle->m_boost_state)
-						{
-							vehicle->m_boost_state = false;
-						}
+						vehicle->m_boost_allow_recharge = true;
+						vehicle->m_boost                = 3.f;
 					}
-					else
+					else if (is_kers)
 					{
 						vehicle->m_kers_boost = vehicle->m_kers_boost_max - 0.01f;
 					}
+				}
+				else if (is_rocket && vehicle->m_boost_state)
+				{
+					vehicle->m_boost_state = false;
 				}
 			}
 		}
