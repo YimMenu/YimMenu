91e9b752dce5ed0035b6edde88faaab3a1ce0db1
user <email@hostname>
2023-08-10 17:53:38 -0400

    fix classes

--- scripts/gtav-classes.cmake
+++ scripts/gtav-classes.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
     gtav_classes
     GIT_REPOSITORY https://github.com/maybegreat48/GTAV-Classes.git
-    GIT_TAG        e0635298dbfba4eaea0739ff140c85e44efd6a87
+    GIT_TAG        49e6213a3a1735e90f17939a5409ea8aabdd0fd4
     GIT_PROGRESS TRUE
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
