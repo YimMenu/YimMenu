c1871b8a284406f967748e90c86b14c9ba8685ea
Niuu <patryk230206@gmail.com>
2023-08-28 20:39:58 +0200

    Uh mistype

--- src/backend/looped/self/toggle_passive.cpp
+++ src/backend/looped/self/toggle_passive.cpp
@@ -12,16 +12,16 @@ namespace big
 		virtual void on_tick() override
 		{
 			*scr_globals::tuneables.at(27371).as<int*>() = 0; // End Passive Time = 0s
-			*script_global(1574582).as<bool*>()          = true;
+			*script_global(1574582).as<bool*>() = true;
 		}
 
 		virtual void on_disable() override
 		{
 			NETWORK::SET_LOCAL_PLAYER_AS_GHOST(false, false);
-			*script_global(1574582).as<bool*>()          = false;
+			*script_global(1574582).as<bool*>() = false;
 			*scr_globals::tuneables.at(27371).as<int*>() = 30000;
-			PED::SET_PED_CONFIG_FLAG(self::ped, 342, true); // Disable NotAllowedToJackAnyPlayers
-			PED::SET_PED_CONFIG_FLAG(self::ped, 122, true); // Disable DisableMelee
+			PED::SET_PED_CONFIG_FLAG(self::ped, 342, false); // Disable NotAllowedToJackAnyPlayers
+			PED::SET_PED_CONFIG_FLAG(self::ped, 122, false); // Disable DisableMelee
 			PLAYER::SET_PLAYER_VEHICLE_DEFENSE_MODIFIER(self::ped, 1.f);
 		}
 	};
