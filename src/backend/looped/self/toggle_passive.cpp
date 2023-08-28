1e1517958c7a5ce6d7bf1a47a961f713f5dca942
Niuu <patryk230206@gmail.com>
2023-08-28 20:28:56 +0200

    i forgot to hit ctrl s

--- src/backend/looped/self/toggle_passive.cpp
+++ src/backend/looped/self/toggle_passive.cpp
@@ -1,7 +1,7 @@
 #include "backend/looped_command.hpp"
+#include "core/scr_globals.hpp"
 #include "natives.hpp"
 #include "script_global.hpp"
-#include "core/scr_globals.hpp"
 
 namespace big
 {
@@ -12,16 +12,17 @@ namespace big
 		virtual void on_tick() override
 		{
 			*scr_globals::tuneables.at(27371).as<int*>() = 0; // End Passive Time = 0s
-			*script_global(1574582).as<bool*>() = true;
+			*script_global(1574582).as<bool*>()          = true;
 		}
 
 		virtual void on_disable() override
 		{
 			NETWORK::SET_LOCAL_PLAYER_AS_GHOST(false, false);
-			*script_global(1574582).as<bool*>() = false;
+			*script_global(1574582).as<bool*>()          = false;
 			*scr_globals::tuneables.at(27371).as<int*>() = 30000;
 			PED::SET_PED_CONFIG_FLAG(self::ped, 342, true); // Disable NotAllowedToJackAnyPlayers
 			PED::SET_PED_CONFIG_FLAG(self::ped, 122, true); // Disable DisableMelee
+			PLAYER::SET_PLAYER_VEHICLE_DEFENSE_MODIFIER(self::ped, 1.f);
 		}
 	};
 
