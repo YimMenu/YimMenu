c2dd37c81507446110447cbda8abbae2fa75f0fa
gir489 <100792176+gir489returns@users.noreply.github.com>
2023-09-11 12:06:24 -0400

    Add proper OOB calculation for CSectorDataNode. (#2103)

--- scripts/gtav-classes.cmake
+++ scripts/gtav-classes.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
     gtav_classes
     GIT_REPOSITORY https://github.com/Yimura/GTAV-Classes.git
-    GIT_TAG        0f88abb93d1499c04f440b77095ae4e02e5144ae
+    GIT_TAG        3c7763fcf996f53f891e40f12bbfa8115fd612a7
     GIT_PROGRESS TRUE
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
