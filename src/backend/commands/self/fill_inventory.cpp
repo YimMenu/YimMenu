57b8abb64b5d979a5d5b7c1af83353d3f06321b7
gir489 <gir489returns@gmail.com>
2023-09-22 19:25:07 -0400

    Fixed fillsnacks not working.

--- src/backend/commands/self/fill_inventory.cpp
+++ src/backend/commands/self/fill_inventory.cpp
@@ -9,20 +9,20 @@ namespace big
 
 		virtual void execute(const command_arguments&, const std::shared_ptr<command_context> ctx) override
 		{
-			STATS::STAT_SET_INT(rage::joaat(self::char_index + "NO_BOUGHT_YUM_SNACKS"), 30, true);
-			STATS::STAT_SET_INT(rage::joaat(self::char_index + "NO_BOUGHT_HEALTH_SNACKS"), 15, true);
-			STATS::STAT_SET_INT(rage::joaat(self::char_index + "NO_BOUGHT_EPIC_SNACKS"), 5, true);
-			STATS::STAT_SET_INT(rage::joaat(self::char_index + "NUMBER_OF_CHAMP_BOUGHT"), 5, true);
-			STATS::STAT_SET_INT(rage::joaat(self::char_index + "NUMBER_OF_ORANGE_BOUGHT"), 10, true);
-			STATS::STAT_SET_INT(rage::joaat(self::char_index + "NUMBER_OF_BOURGE_BOUGHT"), 10, true);
-			STATS::STAT_SET_INT(rage::joaat(self::char_index + "NUMBER_OF_SPRUNK_BOUGHT"), 10, true);
-			STATS::STAT_SET_INT(rage::joaat(self::char_index + "MP_CHAR_ARMOUR_1_COUNT"), 10, true);
-			STATS::STAT_SET_INT(rage::joaat(self::char_index + "MP_CHAR_ARMOUR_2_COUNT"), 10, true);
-			STATS::STAT_SET_INT(rage::joaat(self::char_index + "MP_CHAR_ARMOUR_3_COUNT"), 10, true);
-			STATS::STAT_SET_INT(rage::joaat(self::char_index + "MP_CHAR_ARMOUR_4_COUNT"), 10, true);
-			STATS::STAT_SET_INT(rage::joaat(self::char_index + "MP_CHAR_ARMOUR_5_COUNT"), 10, true);
-			STATS::STAT_SET_INT(rage::joaat(self::char_index + "CIGARETTES_BOUGHT"), 20, true);
-			STATS::STAT_SET_INT(rage::joaat(self::char_index + "BREATHING_APPAR_BOUGHT"), 20, true);
+			STATS::STAT_SET_INT(rage::joaat("MP" + std::to_string(self::char_index) + "_NO_BOUGHT_YUM_SNACKS"), 30, true);
+			STATS::STAT_SET_INT(rage::joaat("MP" + std::to_string(self::char_index) + "_NO_BOUGHT_HEALTH_SNACKS"), 15, true);
+			STATS::STAT_SET_INT(rage::joaat("MP" + std::to_string(self::char_index) + "_NO_BOUGHT_EPIC_SNACKS"), 5, true);
+			STATS::STAT_SET_INT(rage::joaat("MP" + std::to_string(self::char_index) + "_NUMBER_OF_CHAMP_BOUGHT"), 5, true);
+			STATS::STAT_SET_INT(rage::joaat("MP" + std::to_string(self::char_index) + "_NUMBER_OF_ORANGE_BOUGHT"), 10, true);
+			STATS::STAT_SET_INT(rage::joaat("MP" + std::to_string(self::char_index) + "_NUMBER_OF_BOURGE_BOUGHT"), 10, true);
+			STATS::STAT_SET_INT(rage::joaat("MP" + std::to_string(self::char_index) + "_NUMBER_OF_SPRUNK_BOUGHT"), 10, true);
+			STATS::STAT_SET_INT(rage::joaat("MP" + std::to_string(self::char_index) + "_MP_CHAR_ARMOUR_1_COUNT"), 10, true);
+			STATS::STAT_SET_INT(rage::joaat("MP" + std::to_string(self::char_index) + "_MP_CHAR_ARMOUR_2_COUNT"), 10, true);
+			STATS::STAT_SET_INT(rage::joaat("MP" + std::to_string(self::char_index) + "_MP_CHAR_ARMOUR_3_COUNT"), 10, true);
+			STATS::STAT_SET_INT(rage::joaat("MP" + std::to_string(self::char_index) + "_MP_CHAR_ARMOUR_4_COUNT"), 10, true);
+			STATS::STAT_SET_INT(rage::joaat("MP" + std::to_string(self::char_index) + "_MP_CHAR_ARMOUR_5_COUNT"), 10, true);
+			STATS::STAT_SET_INT(rage::joaat("MP" + std::to_string(self::char_index) + "_CIGARETTES_BOUGHT"), 20, true);
+			STATS::STAT_SET_INT(rage::joaat("MP" + std::to_string(self::char_index) + "_BREATHING_APPAR_BOUGHT"), 20, true);
 		}
 	};
 
