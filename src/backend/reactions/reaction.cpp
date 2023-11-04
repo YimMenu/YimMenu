7fb2f816e12917722b8f5aa924caa430de90a675
Yimura <24669514+Yimura@users.noreply.github.com>
2023-11-04 22:17:02 +0100

    refactor(Reactions): replace snprintf strings with std::format

--- src/backend/reactions/reaction.cpp
+++ src/backend/reactions/reaction.cpp
@@ -64,25 +64,20 @@ namespace big
 		if (announce_in_chat)
 		{
 			g_fiber_pool->queue_job([player, this] {
-				char chat[255];
-				snprintf(chat,
-				    sizeof(chat),
-				    std::format("{} {}", g.session.chat_output_prefix, m_announce_message).data(),
-				    player->get_name());
+				auto chat = std::format("{} {}", g.session.chat_output_prefix, m_announce_message);
 
 				if (g_hooking->get_original<hooks::send_chat_message>()(*g_pointers->m_gta.m_send_chat_ptr,
 				        g_player_service->get_self()->get_net_data(),
-				        chat,
+				        chat.data(),
 				        is_team_only))
-					notify::draw_chat(chat, g_player_service->get_self()->get_name(), is_team_only);
+					notify::draw_chat(chat.c_str(), g_player_service->get_self()->get_name(), is_team_only);
 			});
 		}
 
 		if (notify)
 		{
-			char notification[500]{}; // I don't like using sprintf but there isn't an alternative afaik
-			snprintf(notification, sizeof(notification), m_notify_message, player->get_name());
-			g_notification_service->push_warning("PROTECTIONS"_T.data(), notification);
+			g_notification_service->push_warning("PROTECTIONS"_T.data(),
+			    std::vformat(m_notify_message, std::make_format_args(player->get_name())));
 		}
 
 		process_common(player);
