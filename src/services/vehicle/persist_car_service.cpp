1ef147a93f340729a7e3bd67fb8dc2d367632ae7
dynamoNg <139460769+lonelybud@users.noreply.github.com>
2023-11-12 22:33:52 +0530

    remove is_invincible_key related comment code

--- src/services/vehicle/persist_car_service.cpp
+++ src/services/vehicle/persist_car_service.cpp
@@ -154,7 +154,6 @@ namespace big
 
 				ENTITY::SET_ENTITY_VISIBLE(object, attachment.is_visible, 0);
 				ENTITY::SET_ENTITY_COLLISION(object, attachment.has_collision, true);
-				// ENTITY::SET_ENTITY_INVINCIBLE(object, attachment.is_invincible);
 			}
 		}
 
@@ -183,7 +182,6 @@ namespace big
 
 			ENTITY::SET_ENTITY_VISIBLE(vehicle_to_attach, attachment.is_visible, 0);
 			ENTITY::SET_ENTITY_COLLISION(vehicle_to_attach, attachment.has_collision, true);
-			// ENTITY::SET_ENTITY_INVINCIBLE(vehicle_to_attach, attachment.is_invincible);
 			VEHICLE::SET_VEHICLE_IS_CONSIDERED_BY_PLAYER(vehicle_to_attach, false);
 		}
 
@@ -231,12 +229,6 @@ namespace big
 			bool has_collision = vehicle_json[has_collision_key];
 			ENTITY::SET_ENTITY_COLLISION(vehicle, has_collision, true);
 		}
-
-		// if (!vehicle_json[is_invincible_key].is_null())
-		// {
-		// 	bool is_invincible = vehicle_json[is_invincible_key];
-		// 	ENTITY::SET_ENTITY_INVINCIBLE(vehicle, is_invincible);
-		// }
 
 		if (!vehicle_json[custom_secondary_color_key].is_null())
 		{
@@ -363,7 +355,6 @@ namespace big
 		bool has_collision          = ENTITY::GET_ENTITY_COLLISION_DISABLED(object);
 		bool is_visible             = ENTITY::IS_ENTITY_VISIBLE(object);
 		CObject* cobject            = (CObject*)g_pointers->m_gta.m_handle_to_ptr(vehicle);
-		// bool is_invincible          = misc::has_bit_set(&(int&)cobject->m_damage_bits, 8);
 
 		Vector3 rotation;
 		rotation.x = (object_rotation.x - vehicle_rotation.x);
@@ -473,10 +464,8 @@ namespace big
 		bool has_collision                  = ENTITY::GET_ENTITY_COLLISION_DISABLED(vehicle);
 		bool is_visible                     = ENTITY::IS_ENTITY_VISIBLE(vehicle);
 		CVehicle* cvehicle                  = (CVehicle*)g_pointers->m_gta.m_handle_to_ptr(vehicle);
-		// bool is_invincible                  = misc::has_bit_set(&(int&)cvehicle->m_damage_bits, 8);
 		vehicle_json[has_collision_key]     = !has_collision;
 		vehicle_json[is_visible_key]        = is_visible;
-		// vehicle_json[is_invincible_key]     = is_invincible;
 		vehicle_json[wheel_color_key]       = wheel_color;
 		vehicle_json[tire_can_burst]        = VEHICLE::GET_VEHICLE_TYRES_CAN_BURST(vehicle);
 		vehicle_json[drift_tires]           = VEHICLE::GET_DRIFT_TYRES_SET(vehicle);
--- src/services/vehicle/persist_car_service.hpp
+++ src/services/vehicle/persist_car_service.hpp
@@ -20,7 +20,6 @@ namespace big
 		static constexpr auto model_attachments_key = "model_attachments";
 
 		static constexpr auto vehicle_attachments_key = "vehicle_attachments";
-		// static constexpr auto is_invincible_key       = "is_invincible";
 		static constexpr auto is_visible_key    = "is_visible";
 		static constexpr auto has_collision_key = "has_collision";
 
