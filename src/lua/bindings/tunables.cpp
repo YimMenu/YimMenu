b202f53f407142ca0d488d6bd55c72fd517cce3b
Quentin <xiaoxiao921@hotmail.fr>
2023-11-14 18:16:37 +0100

    feat(lua): add tunable overloads for getting / setting values through the already joaated tunable value.

--- src/lua/bindings/tunables.cpp
+++ src/lua/bindings/tunables.cpp
@@ -7,11 +7,13 @@ namespace lua::tunables
 	void bind(sol::state& state)
 	{
 		auto ns         = state["tunables"].get_or_create<sol::table>();
-		ns["get_int"]   = get<int>;
-		ns["get_float"] = get<float>;
-		ns["get_bool"]  = get<bool>;
-		ns["set_int"]   = set<int>;
-		ns["set_float"] = set<float>;
-		ns["set_bool"]  = set<bool>;
+
+		ns["get_int"]   = sol::overload(get<int>, get_already_joaated<int>);
+		ns["get_float"] = sol::overload(get<float>, get_already_joaated<float>);
+		ns["get_bool"]  = sol::overload(get<bool>, get_already_joaated<bool>);
+
+		ns["set_int"]   = sol::overload(set<int>, set_already_joaated<int>);
+		ns["set_float"] = sol::overload(set<float>, set_already_joaated<float>);
+		ns["set_bool"]  = sol::overload(set<bool>, set_already_joaated<bool>);
 	}
 }
