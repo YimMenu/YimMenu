7d2bc409fa8d2d86b5d3984cd08cc5efd31ec723
Yimura <24669514+Yimura@users.noreply.github.com>
2023-11-26 20:54:26 +0100

    feat(Metrics): more bad metrics and remove duplicate

--- src/hooks/info/prepare_metric_for_sending.cpp
+++ src/hooks/info/prepare_metric_for_sending.cpp
@@ -3,41 +3,52 @@
 
 namespace big
 {
-	const auto bad_metrics = std::to_array<const std::string>({
+	const auto bad_metrics = std::unordered_set<std::string>({
+	    "REPORTER",
+	    "REPORT_INVALIDMODEL",
+	    "MEM_NEW",
+	    "DEBUGGER_ATTACH",
 	    "DIG",
+	    "XP_LOSS",
 	    "AWARD_XP",
 	    "CF",
 	    "CC",
 	    "CNR",
 	    "SCRIPT",
+	    "CHEAT",
 	    "AUX_DEUX",
+	    "WEATHER",
 	    "HARDWARE_OS",
 	    "HARDWARE_GPU",
 	    "HARDWARE_MOBO",
 	    "HARDWARE_MEM",
-	    "HARDWARE_MEM",
+	    "HARDWARE_CPU",
 	    "PCSETTINGS",
 	    "CASH_CREATED",
 	    "DR_PS",
 	    "UVC",
 	    "W_L",
 	    "ESVCS",
+	    "IDLEKICK",
 	    "GSCB",
 	    "GSINV",
 	    "GSCW",
 	    "GSINT",
 	    "EARN",
 	    "GARAGE_TAMPER",
-	    "TAMPER",
+	    "LAST_VEH",
+	    "FAIL_SERV",
+	    "CCF_UPDATE",
+	    "CODE_CRC",
+	    "COLLECTIBLE",
+	    "FIRST_VEH",
 	});
 
 	bool hooks::prepare_metric_for_sending(rage::json_serializer* serializer, int unk, int time, rage::rlMetric* metric)
 	{
 		const auto ret = g_hooking->get_original<prepare_metric_for_sending>()(serializer, unk, time, metric);
 
-		const auto is_bad_metric = std::find_if(bad_metrics.begin(), bad_metrics.end(), [metric](auto bad_metric) {
-			return metric->get_name() == bad_metric;
-		}) != bad_metrics.end();
+		const auto is_bad_metric = bad_metrics.contains(metric->get_name());
 		if (is_bad_metric)
 		{
 			LOG(WARNING) << "BAD METRIC: " << metric->get_name() << "; DATA: " << serializer->get_string();
