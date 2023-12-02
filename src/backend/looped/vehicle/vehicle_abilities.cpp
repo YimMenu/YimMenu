07081001506914aaa16778c8d1d36c473467d199
thehorizon84 <hoztonchingchong@gmail.com>
2023-12-03 00:03:41 +0100

    hotfix

--- src/backend/looped/vehicle/vehicle_abilities.cpp
+++ src/backend/looped/vehicle/vehicle_abilities.cpp
@@ -1,19 +1,18 @@
 #include "backend/looped_command.hpp"
 #include "util/vehicle.hpp"
 #include "util/misc.hpp"
+#include "gta/enums.hpp"
 
 #include <vehicle/CVehicleModelInfo.hpp>
 
 namespace big
 {
-	enum eVehicleAbilities : int16_t
-	{
-		JUMP       = 1 << 5,
-		ROCKET     = 1 << 6,
-		PARACHUTE  = 1 << 8,
-		RAMP_BUGGY = 1 << 9
+	const std::vector<int16_t> bits = {
+		static_cast<int16_t>(eVehicleAbilities::JUMP), 
+		static_cast<int16_t>(eVehicleAbilities::ROCKET), 
+		static_cast<int16_t>(eVehicleAbilities::PARACHUTE), 
+		static_cast<int16_t>(eVehicleAbilities::RAMP_BUGGY)
 	};
-	const std::vector<int16_t> bits = {JUMP, ROCKET, PARACHUTE, RAMP_BUGGY};
 
 	bool dont_disable[4] = {};
 
@@ -23,7 +22,7 @@ namespace big
 
 		virtual void on_enable() override
 		{
-			CVehicle* vehicle_ptr = (CVehicle*)g_pointers->m_gta.m_handle_to_ptr(self::veh);
+			CVehicle* vehicle_ptr = static_cast<CVehicle*>(g_pointers->m_gta.m_handle_to_ptr(self::veh));
 
 			if (vehicle_ptr == nullptr) 
 				memset(dont_disable, 0, 4);
@@ -43,7 +42,7 @@ namespace big
 
 		virtual void on_tick() override
 		{
-			CVehicle* vehicle_ptr = (CVehicle*)g_pointers->m_gta.m_handle_to_ptr(self::veh);
+			CVehicle* vehicle_ptr = static_cast<CVehicle*>(g_pointers->m_gta.m_handle_to_ptr(self::veh));
 
 			if (vehicle_ptr == nullptr)
 				return;
@@ -51,29 +50,29 @@ namespace big
 			CVehicleModelInfo* vehicle_model_info = static_cast<CVehicleModelInfo*>(vehicle_ptr->m_model_info);
 
 			if (g.vehicle.ability_jump)
-				misc::set_bits((int*)vehicle_model_info->m_ability_flag, JUMP);
+				misc::set_bits((int*)vehicle_model_info->m_ability_flag, bits[0]);
 			else
-				misc::clear_bits((int*)vehicle_model_info->m_ability_flag, JUMP);
+				misc::clear_bits((int*)vehicle_model_info->m_ability_flag, bits[0]);
 
 			if (g.vehicle.ability_rocket)
-				misc::set_bits((int*)vehicle_model_info->m_ability_flag, ROCKET);
+				misc::set_bits((int*)vehicle_model_info->m_ability_flag, bits[1]);
 			else
-				misc::clear_bits((int*)vehicle_model_info->m_ability_flag, ROCKET);
+				misc::clear_bits((int*)vehicle_model_info->m_ability_flag, bits[1]);
 
 			if (g.vehicle.ability_parachute)
-				misc::set_bits((int*)vehicle_model_info->m_ability_flag, PARACHUTE);
+				misc::set_bits((int*)vehicle_model_info->m_ability_flag, bits[2]);
 			else
-				misc::clear_bits((int*)vehicle_model_info->m_ability_flag, PARACHUTE);
+				misc::clear_bits((int*)vehicle_model_info->m_ability_flag, bits[2]);
 			
 			if (g.vehicle.ability_ramp)
-				misc::set_bits((int*)vehicle_model_info->m_ability_flag, RAMP_BUGGY);
+				misc::set_bits((int*)vehicle_model_info->m_ability_flag, bits[3]);
 			else
-				misc::clear_bits((int*)vehicle_model_info->m_ability_flag, RAMP_BUGGY);
+				misc::clear_bits((int*)vehicle_model_info->m_ability_flag, bits[3]);
 		}
 
 		virtual void on_disable() override
 		{
-			CVehicle* vehicle_ptr = (CVehicle*)g_pointers->m_gta.m_handle_to_ptr(self::veh);
+			CVehicle* vehicle_ptr = static_cast<CVehicle*>(g_pointers->m_gta.m_handle_to_ptr(self::veh));
 
 			if (vehicle_ptr == nullptr)
 				return;
@@ -95,8 +94,8 @@ namespace big
 
 	vehicle_ability g_vehicle_ability("modifyvehicleability", "MODIFY_VEHICLE_ABILITY", "MODIFY_VEHICLE_ABILITY_DESC", g.vehicle.modify_vehicle_ability);
 
-	bool_command g_rocket_ability("rocketability", "BACKEND_LOOPED_VEHICLE_ABILITY_ROCKET", "BACKEND_LOOPED_VEHICLE_ABILITY_ROCKET_DESC", g.vehicle.ability_rocket);
 	bool_command g_jump_ability("jumpability", "BACKEND_LOOPED_VEHICLE_ABILITY_JUMP", "BACKEND_LOOPED_VEHICLE_ABILITY_JUMP_DESC", g.vehicle.ability_jump);
+	bool_command g_rocket_ability("rocketability", "BACKEND_LOOPED_VEHICLE_ABILITY_ROCKET", "BACKEND_LOOPED_VEHICLE_ABILITY_ROCKET_DESC", g.vehicle.ability_rocket);
 	bool_command g_parachute_ability("parachuteability", "BACKEND_LOOPED_VEHICLE_ABILITY_PARACHUTE", "BACKEND_LOOPED_VEHICLE_ABILITY_PARACHUTE_DESC", g.vehicle.ability_parachute);
 	bool_command g_ramp_ability("rampability", "BACKEND_LOOPED_VEHICLE_ABILITY_RAMP", "BACKEND_LOOPED_VEHICLE_ABILITY_RAMP_DESC", g.vehicle.ability_ramp);
 }
