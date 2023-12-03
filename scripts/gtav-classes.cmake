564994e64e742f377dcdfcfab937b1fbdf589a4a
thehorizon84 <hoztonchingchong@gmail.com>
2023-12-03 14:47:07 +0100

    FIX

--- scripts/gtav-classes.cmake
+++ scripts/gtav-classes.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
     gtav_classes
     GIT_REPOSITORY https://github.com/Yimura/GTAV-Classes.git
-    GIT_TAG        9e76175d28b3de21c24a69e71bee779d704d8304
+    GIT_TAG        b93fcb4848c935b6b11ce9d5e82fb38aba3a59f4
     GIT_PROGRESS TRUE
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
