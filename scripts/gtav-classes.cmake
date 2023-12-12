0798a413f93ab3371593327e712984155c2b783a
horizon <153565784+horizon-84@users.noreply.github.com>
2023-12-12 15:46:59 +0100

    Updated vehicle_abilities.cpp to work with latest GTAV-Classes PR (#2509)

--- scripts/gtav-classes.cmake
+++ scripts/gtav-classes.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
     gtav_classes
     GIT_REPOSITORY https://github.com/Yimura/GTAV-Classes.git
-    GIT_TAG        b93fcb4848c935b6b11ce9d5e82fb38aba3a59f4
+    GIT_TAG        e52b8c65379f3304cf7b72dc87561c9d3924d4b4
     GIT_PROGRESS TRUE
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
