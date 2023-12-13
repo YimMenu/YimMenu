2f5c50d7c7d348b2a7a72b49d4bd07adeb760c16
gir489returns <gir489returns@outlook.com>
2023-12-13 17:32:46 -0500

    Update GTAV-Classes repo.

--- scripts/gtav-classes.cmake
+++ scripts/gtav-classes.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
     gtav_classes
     GIT_REPOSITORY https://github.com/Yimura/GTAV-Classes.git
-    GIT_TAG        08cc88c83a988f1acfd90f89fa336ba548146831
+    GIT_TAG        760585b6ca9fd60ade494f9226190963d3b743d9
     GIT_PROGRESS TRUE
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
