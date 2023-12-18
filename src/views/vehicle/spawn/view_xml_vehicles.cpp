b2c0a6e832acfae07cc69be61a0d060fe338b2de
TheGreenBandit <106003542+TheGreenBandit@users.noreply.github.com>
2023-12-18 06:35:35 -0500

    simple search function

--- src/views/vehicle/spawn/view_xml_vehicles.cpp
+++ src/views/vehicle/spawn/view_xml_vehicles.cpp
@@ -13,15 +13,20 @@ namespace big
 		components::button("Reload", [] {
 			g_xml_vehicles_service->fetch_xml_files();
 		});
+		static char search[32];
+		ImGui::InputText("Search", search, sizeof(search));
 		if (ImGui::BeginListBox("##xmllist", {300, static_cast<float>(*g_pointers->m_gta.m_resolution_y - 188 - 38 * 4)}))
 		{
 			for (auto& [name, doc] : g_xml_vehicles_service->m_all_xml_vehicles)
 			{
-				components::selectable(name, false, [&doc] {
-					auto handle = g_xml_vehicles_service->spawn_from_xml(doc, self::pos);
-					if (g.spawn_vehicle.spawn_inside)
-						vehicle::teleport_into_vehicle(handle);
-				});
+				if (name.find(search) != std::string::npos)
+				{
+					components::selectable(name, false, [&doc] {
+						auto handle = g_xml_vehicles_service->spawn_from_xml(doc, self::pos);
+						if (g.spawn_vehicle.spawn_inside)
+							vehicle::teleport_into_vehicle(handle);
+					});
+				}
 			}
 
 			ImGui::EndListBox();
