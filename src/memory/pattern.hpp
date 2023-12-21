c1810d2f6bf1085121f523688b99ae29b0cbf62f
Quentin <xiaoxiao921@hotmail.fr>
2023-12-21 03:54:02 +0100

    fix(debug): fix debug build

--- src/memory/pattern.hpp
+++ src/memory/pattern.hpp
@@ -16,7 +16,6 @@ namespace memory
 
 	public:
 		pattern(std::string_view ida_sig);
-		explicit pattern(const void* bytes, std::string_view mask);
 
 		inline pattern(const char* ida_sig) :
 		    pattern(std::string_view(ida_sig))
