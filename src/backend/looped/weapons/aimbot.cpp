28c9ee2e7708f3fecc104ec380d8c3bcf4aefdb2
navmodder <singh.navjot@live.com>
2024-01-27 23:31:06 -0500

    Improved Aimbot

--- src/backend/looped/weapons/aimbot.cpp
+++ src/backend/looped/weapons/aimbot.cpp
@@ -16,75 +16,92 @@ namespace big
 		virtual void on_tick() override
 		{
 			float local_fov_change = g.weapons.aimbot.fov;
-			for (auto ped : entity::get_entities(false, true))
+			if (PED::GET_PED_CONFIG_FLAG(self::ped, 78, 0))
 			{
-				if (!ENTITY::IS_ENTITY_DEAD(ped, 0))
+				Entity closest_entity{};
+				float distance = 1;
+				for (auto ped : entity::get_entities(false, true))
 				{
-					int relation = PED::GET_RELATIONSHIP_BETWEEN_PEDS(ped, self::ped); // relation for enemy check
-					int type     = PED::GET_PED_TYPE(ped); // for police check, cop types are 6, swat is 27
-					Vector3 world_position = ENTITY::GET_ENTITY_COORDS(ped, false);
+					Vector3 pos = ENTITY::GET_ENTITY_COORDS(ped, 1);
+					rage::fvector2 screen_pos;
+					HUD::GET_HUD_SCREEN_POSITION_FROM_WORLD_POSITION(pos.x, pos.y, pos.z, &screen_pos.x, &screen_pos.y);
 
-					if (SYSTEM::VDIST2(self::pos.x,
-					        self::pos.y,
-					        self::pos.z,
-					        world_position.x,
-					        world_position.y,
-					        world_position.z)
-					    > (g.weapons.aimbot.distance * g.weapons.aimbot.distance))
-						continue; // If the entity is further than our preset distance then just skip it
-
-					if (PED::IS_PED_A_PLAYER(ped) && g.weapons.aimbot.on_player) // check if its a player
+					if (entity::distance_to_middle_of_screen(screen_pos) < distance && ENTITY::IS_ENTITY_ON_SCREEN(ped) && ped != self::ped)
 					{
-						goto aimbot_handler;
-					}
-					else if (((relation == 4) || (relation == 5)) && g.weapons.aimbot.on_enemy) // relation 4 and 5 are for enemies
-					{
-						goto aimbot_handler;
-					}
-					else if (((type == 6 && !PED::IS_PED_MODEL(ped, rage::joaat("s_m_y_uscg_01"))) || type == 27 || // s_m_y_uscg_01 = us coast guard 1 (technically military)
-					             PED::IS_PED_MODEL(ped, rage::joaat("s_m_y_ranger_01")) || PED::IS_PED_MODEL(ped, rage::joaat("s_f_y_ranger_01"))) // ranger models
-					    && g.weapons.aimbot.on_police)
-					{
-						goto aimbot_handler;
-					}
-					else if (g.weapons.aimbot.on_npc && !PED::IS_PED_A_PLAYER(ped))
-
-					// Update aim lock coords
-					aimbot_handler:
-					{
-						if (!ENTITY::HAS_ENTITY_CLEAR_LOS_TO_ENTITY_ADJUST_FOR_COVER(self::ped, ped, 17))
-							continue;
-
-						// Jump to here to handle instead of continue statements
-						target_entity = ped;
-						aim_lock = ENTITY::GET_ENTITY_BONE_POSTION(ped, PED::GET_PED_BONE_INDEX(ped, g.weapons.aimbot.selected_bone));
+						closest_entity = ped;
+						distance       = entity::distance_to_middle_of_screen(screen_pos);
 					}
 				}
-			}
-			if (!target_entity || ENTITY::IS_ENTITY_DEAD(target_entity, 0))
-			{
-				return;
-			}
-			if (PLAYER::IS_PLAYER_FREE_AIMING(self::id))
-			{
-				Vector3 camera_target;
 
+				target_entity = closest_entity;
+
+				int relation = PED::GET_RELATIONSHIP_BETWEEN_PEDS(target_entity, self::ped);
+				int type     = PED::GET_PED_TYPE(target_entity);
+
+				if (!target_entity || ENTITY::IS_ENTITY_DEAD(target_entity, 0))
+				{
+					return;
+				}
+				Vector3 world_position        = ENTITY::GET_ENTITY_COORDS(target_entity, 1);
+				float distance_between_target = SYSTEM::VDIST2(self::pos.x,
+				    self::pos.y,
+				    self::pos.z,
+				    world_position.x,
+				    world_position.y,
+				    world_position.z);
+
+				if (distance_between_target > (g.weapons.aimbot.distance * g.weapons.aimbot.distance))
+				{
+					return;
+				}
+				if (!ENTITY::HAS_ENTITY_CLEAR_LOS_TO_ENTITY(self::ped, target_entity, 17))
+				{
+					return;
+				}
+				if (PED::IS_PED_A_PLAYER(target_entity))
+				{
+					if (!g.weapons.aimbot.on_player)
+						return;
+				}
+				else if (type == ePedType::PED_TYPE_ARMY || type == ePedType::PED_TYPE_SWAT || type == ePedType::PED_TYPE_COP)
+				{
+					if (!g.weapons.aimbot.on_police)
+						return;
+				}
+				else if (((relation == 4) || (relation == 5))) // relation 4 and 5 are for enemies
+				{
+					if (!g.weapons.aimbot.on_enemy)
+						return;
+				}
+				else
+				{
+					if (!g.weapons.aimbot.on_npc)
+						return;
+				}
+
+				aim_lock = ENTITY::GET_WORLD_POSITION_OF_ENTITY_BONE(target_entity,
+				    PED::GET_PED_BONE_INDEX(target_entity, g.weapons.aimbot.selected_bone));
+
+				GRAPHICS::DRAW_MARKER(3, aim_lock.x, aim_lock.y, aim_lock.z + 0.3f, 0, 0, 0, 0, 180, 0, 0.3f, 0.3f, 0.3f, 255, 255, 255, 255, 1, 1, 0, 0, 0, 0, 0);
+
+
+				Vector3 camera_target;
 				if (g.weapons.aimbot.smoothing)
 				{
 					//Avoid buggy cam
 					if (!initalized)
 					{
-						Vector3 cam_coords               = CAM::GET_GAMEPLAY_CAM_COORD();
-						Vector3 cam_rot                  = CAM::GET_GAMEPLAY_CAM_ROT(0);
-						Vector3 cam_direction            = math::rotation_to_direction(cam_rot);
-						float distance                   = 150.f;
-						Vector3 multiply                 = cam_direction * distance;
-						Vector3 front_cam                = cam_coords + multiply;
-						camera_target                    = front_cam - CAM::GET_GAMEPLAY_CAM_COORD();
-						smooth_factor                    = camera_target;
-						initalized                       = true;
+						Vector3 cam_coords    = CAM::GET_FINAL_RENDERED_CAM_COORD();
+						Vector3 cam_rot       = CAM::GET_GAMEPLAY_CAM_ROT(0);
+						Vector3 cam_direction = math::rotation_to_direction(cam_rot);
+						float distance        = 150.f;
+						Vector3 multiply      = cam_direction * distance;
+						Vector3 front_cam     = cam_coords + multiply;
+						camera_target         = front_cam - CAM::GET_FINAL_RENDERED_CAM_COORD();
+						smooth_factor         = camera_target;
+						initalized            = true;
 					}
-					Vector3 target = aim_lock - CAM::GET_GAMEPLAY_CAM_COORD();
+					Vector3 target = aim_lock - CAM::GET_FINAL_RENDERED_CAM_COORD();
 					smooth_factor.x += (target.x - smooth_factor.x) * g.weapons.aimbot.smoothing_speed / 10.f;
 					smooth_factor.y += (target.y - smooth_factor.y) * g.weapons.aimbot.smoothing_speed / 10.f;
 					smooth_factor.z += (target.z - smooth_factor.z) * g.weapons.aimbot.smoothing_speed / 10.f;
@@ -93,20 +110,21 @@ namespace big
 				}
 				else
 				{
-					camera_target = aim_lock - CAM::GET_GAMEPLAY_CAM_COORD();
+					camera_target = aim_lock - CAM::GET_FINAL_RENDERED_CAM_COORD();
 				}
 				//  We actually need this. For some unknow reasons it gets entity or something there.
 				//  Then it will start leading to 0,0,0 coords.Aim will start pointing at 0,0,0 as well.
 				if (aim_lock.x == 0.f && aim_lock.y == 0.f && aim_lock.z == 0.f)
 					return;
 
-				constexpr float RADPI = 180.0f / std::numbers::pi;
-				float magnitude       = std::hypot(camera_target.x, camera_target.y, camera_target.z);
-				float camera_heading  = atan2f(camera_target.x, camera_target.y) * RADPI;
+				float RADPI          = 180.0f / std::numbers::pi;
+				float camera_heading = atan2f(camera_target.x, camera_target.y) * RADPI;
+				float magnitude      = sqrtf(camera_target.x * camera_target.x + camera_target.y * camera_target.y
+                    + camera_target.z * camera_target.z);
 
 				float camera_pitch = asinf(camera_target.z / magnitude) * RADPI;
-				float self_heading = ENTITY::GET_ENTITY_HEADING(self::ped);
-				float self_pitch   = ENTITY::GET_ENTITY_PITCH(self::ped);
+				float self_heading = ENTITY::GET_ENTITY_HEADING(self::veh ? self::veh : self::ped);
+				float self_pitch   = ENTITY::GET_ENTITY_PITCH(self::veh ? self::veh : self::ped);
 				if (camera_heading >= 0.0f && camera_heading <= 180.0f)
 				{
 					camera_heading = 360.0f - camera_heading;
@@ -128,8 +146,8 @@ namespace big
 			}
 			else
 			{
-				target_entity           = 0;
-				initalized = false;
+				target_entity = 0;
+				initalized    = false;
 			}
 		}
 		virtual void on_disable() override
