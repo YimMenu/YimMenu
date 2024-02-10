7de98628da798ff51e30af416d0bdc96e26f3375
gir489returns <gir489returns@outlook.com>
2024-02-10 12:04:51 -0500

    Added final solution to the font problem.

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -40,7 +40,10 @@ namespace big
 		if (!font_file_path.exists())
 			font_file_path = windows_fonts.get_file("./msyh.ttf");
 		if (!font_file_path.exists())
-			throw std::runtime_error("Failed to retrieve msyh font.");
+		{
+			LOG(WARNING) << "Failed to find msyh font, falling back to Arial!";
+			font_file_path = windows_fonts.get_file("./arial.ttf");
+		}
 		auto font_file            = std::ifstream(font_file_path.get_path(), std::ios::binary | std::ios::ate);
 		const auto font_data_size = static_cast<int>(font_file.tellg());
 		const auto font_data      = std::make_unique<uint8_t[]>(font_data_size);
