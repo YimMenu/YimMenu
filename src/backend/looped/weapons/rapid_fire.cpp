YimMenu/YimMenu

commit ad1dfc71d2d5b2a9fb16f1c2009e4bc60bd65231

refactor: move Request Model logic to helper function (#2669)

src/byte_patch_manager.cpp
@@ -7,6 +7,7 @@
 #include "util/explosion_anti_cheat_bypass.hpp"
 #include "util/police.hpp"
 #include "util/vehicle.hpp"
+#include "util/world_model.hpp"
 
 extern "C" void sound_overload_detour();
 uint64_t g_sound_overload_ret_addr;
@@ -20,6 +21,10 @@ namespace big
 		    memory::byte_patch::make(g_pointers->m_gta.m_max_wanted_level.add(5).rip().as<uint32_t*>(), 0).get();
 		police::m_max_wanted_level_2 =
 		    memory::byte_patch::make(g_pointers->m_gta.m_max_wanted_level.add(14).rip().as<uint32_t*>(), 0).get();
+
+		// Patch World Model Spawn Bypass
+		world_model_bypass::m_world_model_spawn_bypass =
+		    memory::byte_patch::make(g_pointers->m_gta.m_world_model_spawn_bypass.as<PVOID*>(), 0).get();
 
 		// Patch blocked explosions
 		explosion_anti_cheat_bypass::m_can_blame_others =

src/gta_pointers.hpp
@@ -42,6 +42,8 @@ namespace big
 	{
 		memory::handle m_max_wanted_level;
 
+		memory::handle m_world_model_spawn_bypass;
+
 		memory::handle m_blame_explode;
 
 		memory::handle m_explosion_patch;
@@ -100,7 +102,6 @@ namespace big
 		float* m_gravity_level;
 		functions::set_gravity_level m_set_gravity_level;
 
-		PVOID m_world_model_spawn_bypass;
 		PVOID m_native_return;
 		PVOID m_get_label_text;
 		functions::check_chat_profanity* m_check_chat_profanity;

src/pointers.cpp
@@ -1,8 +1,8 @@
 #include "pointers.hpp"
 
 #include "gta_pointers_layout_info.hpp"
-#include "sc_pointers_layout_info.hpp"
 #include "memory/all.hpp"
+#include "sc_pointers_layout_info.hpp"
 
 namespace big
 {
@@ -157,15 +157,6 @@ namespace big
             [](memory::handle ptr)
             {
                 g_pointers->m_gta.m_swapchain = ptr.add(3).rip().as<IDXGISwapChain**>();
-            }
-        },
-        // World Model Spawn Bypass
-        {
-            "WMSB",
-            "48 85 C0 0F 84 ? ? ? ? 8B 48 50",
-            [](memory::handle ptr)
-            {
-                g_pointers->m_gta.m_world_model_spawn_bypass = ptr.as<PVOID>();
             }
         },
         // Native Return Spoofer
@@ -1515,6 +1506,15 @@ namespace big
             [](memory::handle ptr)
             {
                 g_pointers->m_gta.m_max_wanted_level = ptr;
+            }
+        },
+        // World Model Spawn Bypass
+        {
+            "WMSB",
+            "48 85 C0 0F 84 ? ? ? ? 8B 48 50",
+            [](memory::handle ptr)
+            {
+                g_pointers->m_gta.m_world_model_spawn_bypass = ptr;
             }
         },
         // Blame Explode

src/backend/commands/spawn/spawn_vehicle.cpp
@@ -25,8 +25,8 @@ namespace big
 
 		virtual void execute(const command_arguments& args, const std::shared_ptr<command_context> ctx) override
 		{
-            const auto hash = args.get<rage::joaat_t>(0);
-			if (!STREAMING::IS_MODEL_IN_CDIMAGE(hash) || !STREAMING::IS_MODEL_A_VEHICLE(hash))
+			const auto hash = args.get<rage::joaat_t>(0);
+			if (!entity::request_model(hash))
 			{
 				ctx->report_error("BACKEND_SPAWN_VEHICLE_INVALID_MODEL"_T.data());
 				return;
@@ -61,8 +61,6 @@ namespace big
 	};
 
 	spawn_vehicle g_spawn_vehicle("spawn", "GUI_TAB_SPAWN_VEHICLE", "BACKEND_SPAWN_VEHICLE_DESC", 1);
-	bool_command g_spawn_maxed("spawnmaxed", "SPAWN_MAXED", "SPAWN_MAXED_DESC",
-	    g.spawn_vehicle.spawn_maxed);
-	bool_command g_spawn_inside("spawnin", "SPAWN_IN", "SPAWN_IN_DESC",
-	    g.spawn_vehicle.spawn_inside);
+	bool_command g_spawn_maxed("spawnmaxed", "SPAWN_MAXED", "SPAWN_MAXED_DESC", g.spawn_vehicle.spawn_maxed);
+	bool_command g_spawn_inside("spawnin", "SPAWN_IN", "SPAWN_IN_DESC", g.spawn_vehicle.spawn_inside);
 }

src/util/entity.cpp
@@ -56,7 +56,7 @@ namespace big::entity
 				ENTITY::DELETE_ENTITY(&vehicle);
 			}
 		}
-		
+
 		ENTITY::DETACH_ENTITY(ent, 1, 1);
 		ENTITY::SET_ENTITY_COORDS_NO_OFFSET(ent, 7000.f, 7000.f, 15.f, 0, 0, 0);
 		if (!ENTITY::IS_ENTITY_A_MISSION_ENTITY(ent))
@@ -193,33 +193,71 @@ namespace big::entity
 
 	bool load_ground_at_3dcoord(Vector3& location)
 	{
-	    constexpr float max_ground_check = 1000.f;
-	    constexpr int max_attempts = 300;
-	    float ground_z = location.z;
-	    int current_attempts = 0;
-	    bool found_ground;
+		constexpr float max_ground_check = 1000.f;
+		constexpr int max_attempts       = 300;
+		float ground_z                   = location.z;
+		int current_attempts             = 0;
+		bool found_ground;
+		float height;
 
-	    do {
-	        found_ground = MISC::GET_GROUND_Z_FOR_3D_COORD(location.x, location.y, max_ground_check, &ground_z, FALSE, FALSE);
-	        STREAMING::REQUEST_COLLISION_AT_COORD(location.x, location.y, location.z);
+		do
+		{
+			found_ground = MISC::GET_GROUND_Z_FOR_3D_COORD(location.x, location.y, max_ground_check, &ground_z, FALSE, FALSE);
+			STREAMING::REQUEST_COLLISION_AT_COORD(location.x, location.y, location.z);
 
-	        if (current_attempts % 10 == 0)
-	        {
-	            location.z += 25.f;
-	        }
+			if (current_attempts % 10 == 0)
+			{
+				location.z += 25.f;
+			}
 
-	        ++current_attempts;
+			++current_attempts;
 
-	        script::get_current()->yield();
-	    } while (!found_ground && current_attempts < max_attempts);
+			script::get_current()->yield();
+		} while (!found_ground && current_attempts < max_attempts);
 
-	    if (!found_ground)
-	    {
-	        return false;
-	    }
+		if (!found_ground)
+		{
+			return false;
+		}
 
-	    location.z = ground_z + 1.f;
-	    return true;
+		if (WATER::GET_WATER_HEIGHT(location.x, location.y, location.z, &height))
+		{
+			location.z = height;
+		}
+		else
+		{
+			location.z = ground_z + 1.f;
+		}
+
+		return true;
+	}
+
+	bool request_model(rage::joaat_t hash)
+	{
+		if (STREAMING::HAS_MODEL_LOADED(hash))
+		{
+			return true;
+		}
+
+		bool has_loaded;
+
+		if (STREAMING::IS_MODEL_VALID(hash) && STREAMING::IS_MODEL_IN_CDIMAGE(hash))
+		{
+			do
+			{
+				has_loaded = STREAMING::HAS_MODEL_LOADED(hash);
+				if (has_loaded)
+					break;
+
+				STREAMING::REQUEST_MODEL(hash);
+
+				script::get_current()->yield();
+			} while (!has_loaded);
+
+			return true;
+		}
+
+		return false;
 	}
 
 	double distance_to_middle_of_screen(const rage::fvector2& screen_pos)

src/util/entity.hpp
@@ -18,6 +18,7 @@ namespace big::entity
 	bool network_has_control_of_entity(rage::netObject* net_object);
 	std::vector<Entity> get_entities(bool vehicles, bool peds, bool props = false, bool include_self_veh = false);
 	bool load_ground_at_3dcoord(Vector3& location);
+	bool request_model(rage::joaat_t hash);
 	double distance_to_middle_of_screen(const rage::fvector2& screen_pos);
 	Entity get_entity_closest_to_middle_of_screen(rage::fwEntity** pointer = nullptr, std::vector<Entity> ignore_entities = {}, bool include_veh = true, bool include_ped = true, bool include_prop = true, bool include_players = true);
 }

src/util/ped.hpp
@@ -446,24 +446,19 @@ namespace big::ped
 
 	inline bool change_player_model(const Hash hash)
 	{
-		for (uint8_t i = 0; !STREAMING::HAS_MODEL_LOADED(hash) && i < 100; i++)
+		if (entity::request_model(hash))
 		{
-			STREAMING::REQUEST_MODEL(hash);
+			self::ped = PLAYER::PLAYER_PED_ID();
+			PLAYER::SET_PLAYER_MODEL(self::id, hash);
 			script::get_current()->yield();
+			STREAMING::SET_MODEL_AS_NO_LONGER_NEEDED(hash);
+			for (int i = 0; i < 12; i++)
+			{
+				PED::SET_PED_COMPONENT_VARIATION(self::ped, i, PED::GET_PED_DRAWABLE_VARIATION(self::ped, i), PED::GET_PED_TEXTURE_VARIATION(self::ped, i), PED::GET_PED_PALETTE_VARIATION(self::ped, i));
+			}
+			return true;	
 		}
-		if (!STREAMING::HAS_MODEL_LOADED(hash))
-		{
-			return false;
-		}
-		PLAYER::SET_PLAYER_MODEL(self::id, hash);
-		self::ped = PLAYER::PLAYER_PED_ID();
-		script::get_current()->yield();
-		STREAMING::SET_MODEL_AS_NO_LONGER_NEEDED(hash);
-		for (int i = 0; i < 12; i++)
-		{
-			PED::SET_PED_COMPONENT_VARIATION(self::ped, i, PED::GET_PED_DRAWABLE_VARIATION(self::ped, i), PED::GET_PED_TEXTURE_VARIATION(self::ped, i), PED::GET_PED_PALETTE_VARIATION(self::ped, i));
-		}
-		return true;
+		return false;
 	}
 
 	inline bool steal_outfit(const Ped target)
@@ -512,29 +507,22 @@ namespace big::ped
 
 	inline Ped spawn(ePedType pedType, Hash hash, Hash clone, Vector3 location, float heading, bool is_networked = true)
 	{
-		for (uint8_t i = 0; !STREAMING::HAS_MODEL_LOADED(hash) && i < 100; i++)
+		if (entity::request_model(hash))
 		{
-			STREAMING::REQUEST_MODEL(hash);
-			script::get_current()->yield();
+			Ped ped = PED::CREATE_PED(pedType, hash, location.x, location.y, location.z, heading, is_networked, false);
+
+		    script::get_current()->yield();
+
+		    if (clone)
+		    {
+		        PED::CLONE_PED_TO_TARGET(clone, ped);
+		    }
+
+		    STREAMING::SET_MODEL_AS_NO_LONGER_NEEDED(hash);
+
+		    return ped;	
 		}
-
-		if (!STREAMING::HAS_MODEL_LOADED(hash))
-		{
-			return 0;
-		}
-
-		auto ped = PED::CREATE_PED(pedType, hash, location.x, location.y, location.z, heading, is_networked, false);
-
-		script::get_current()->yield();
-
-		if (clone)
-		{
-			PED::CLONE_PED_TO_TARGET(clone, ped);
-		}
-
-		STREAMING::SET_MODEL_AS_NO_LONGER_NEEDED(hash);
-
-		return ped;
+		return 0;
 	}
 
 	inline void set_ped_random_component_variation(Ped ped)

src/util/vehicle.cpp
@@ -6,8 +6,8 @@ namespace big::vehicle
 	{
 		switch (speed_unit)
 		{
-		case SpeedUnit::KMPH: return mps * 3.6f; break;
-		case SpeedUnit::MIPH: return mps * 2.2369f; break;
+		case SpeedUnit::KMPH: return mps * 3.6f;
+		case SpeedUnit::MIPH: return mps * 2.2369f;
 		}
 
 		return mps;
@@ -17,8 +17,8 @@ namespace big::vehicle
 	{
 		switch (speed_unit)
 		{
-		case SpeedUnit::KMPH: return speed / 3.6f; break;
-		case SpeedUnit::MIPH: return speed / 2.2369f; break;
+		case SpeedUnit::KMPH: return speed / 3.6f;
+		case SpeedUnit::MIPH: return speed / 2.2369f;
 		}
 
 		return speed;
@@ -77,7 +77,7 @@ namespace big::vehicle
 
 			if (driver_ped != 0)
 			{
-				if (PED::GET_PED_TYPE(driver_ped) == ePedType::PED_TYPE_NETWORK_PLAYER)
+				if (PED::GET_PED_TYPE(driver_ped) == PED_TYPE_NETWORK_PLAYER)
 				{
 					TASK::CLEAR_PED_TASKS_IMMEDIATELY(driver_ped);
 				}
@@ -152,27 +152,20 @@ namespace big::vehicle
 
 	Vehicle spawn(Hash hash, Vector3 location, float heading, bool is_networked, bool script_veh)
 	{
-		for (int i = 0; !STREAMING::HAS_MODEL_LOADED(hash) && i < 100; i++)
+		if (entity::request_model(hash))
 		{
-			STREAMING::REQUEST_MODEL(hash);
-			script::get_current()->yield();
+			auto veh = VEHICLE::CREATE_VEHICLE(hash, location.x, location.y, location.z, heading, is_networked, script_veh, false);
+
+			STREAMING::SET_MODEL_AS_NO_LONGER_NEEDED(hash);
+
+			if (*g_pointers->m_gta.m_is_session_started)
+			{
+				set_mp_bitset(veh);
+			}
+
+			return veh;
 		}
-
-		if (!STREAMING::HAS_MODEL_LOADED(hash))
-		{
-			return 0;
-		}
-
-		auto veh = VEHICLE::CREATE_VEHICLE(hash, location.x, location.y, location.z, heading, is_networked, script_veh, false);
-
-		STREAMING::SET_MODEL_AS_NO_LONGER_NEEDED(hash);
-
-		if (*g_pointers->m_gta.m_is_session_started)
-		{
-			set_mp_bitset(veh);
-		}
-
-		return veh;
+		return 0;
 	}
 
 	Vehicle clone_from_vehicle_data(std::map<int, int32_t>& data, Vector3 location, float heading)
@@ -226,7 +219,7 @@ namespace big::vehicle
 			return 0;
 		}
 
-		auto veh = vehicle::get_closest_to_location(tmpLocation, 200);
+		auto veh = get_closest_to_location(tmpLocation, 200);
 		if (veh == 0)
 		{
 			return 0;
@@ -551,8 +544,6 @@ namespace big::vehicle
 
 	void max_vehicle(Vehicle veh)
 	{
-		Hash model = ENTITY::GET_ENTITY_MODEL(veh);
-
 		VEHICLE::SET_VEHICLE_MOD_KIT(veh, 0);
 
 		VEHICLE::TOGGLE_VEHICLE_MOD(veh, MOD_TURBO, TRUE);
@@ -688,13 +679,10 @@ namespace big::vehicle
 					VEHICLE::SET_VEHICLE_INDIVIDUAL_DOORS_LOCKED(veh, i, (int)state);
 				return VEHICLE::GET_VEHICLE_DOOR_LOCK_STATUS(veh) == (int)state;
 			}
-			else
-			{
-				if (VEHICLE::GET_IS_DOOR_VALID(veh, (int)doorId))
-					VEHICLE::SET_VEHICLE_INDIVIDUAL_DOORS_LOCKED(veh, (int)doorId, (int)state);
+			if (VEHICLE::GET_IS_DOOR_VALID(veh, (int)doorId))
+				VEHICLE::SET_VEHICLE_INDIVIDUAL_DOORS_LOCKED(veh, (int)doorId, (int)state);
 
-				return VEHICLE::GET_VEHICLE_INDIVIDUAL_DOOR_LOCK_STATUS(veh, (int)doorId) == (int)state;
-			}
+			return VEHICLE::GET_VEHICLE_INDIVIDUAL_DOOR_LOCK_STATUS(veh, (int)doorId) == (int)state;
 		}
 
 		return false;

commit 0b53660312d1b752910aa9cf0e4a5e5e29f406c2

Many different fixes (#2749)
* fix(BytePatch): regression in world spawn bypass introduced in #2669
* fix(View/Network): session join options not updating after language change
* fix(RapidFire): prevent game from crashing by filling up the bullet pool
* refactor(Settings): don't save certain settings that users usually don't want to have enabled
* fix(CustomWeapons): render weapon names properly
* fix(reaction): names don't appear in chat

src/backend/looped/weapons/rapid_fire.cpp
@@ -10,11 +10,27 @@ namespace big
 	{
 		using looped_command::looped_command;
 
+		const std::size_t m_tick_rate = 5u;
+		std::size_t m_current_tick    = 0;
+
+		/**
+		 * @brief We have to limit the fire rate for some shotguns as they can fill the bullet pool and crash the game.
+		 * 
+		 * @return true 
+		 * @return false 
+		 */
+		inline bool can_shoot()
+		{
+			bool result    = (m_current_tick == 0);
+			m_current_tick = (m_current_tick + 1) % m_tick_rate;
+			return result;
+		}
+
 		virtual void on_tick() override
 		{
 			if (!HUD::IS_PAUSE_MENU_ACTIVE() && !g_gui->is_open() && !PED::IS_PED_DEAD_OR_DYING(self::ped, true))
 			{
-				if (PAD::IS_DISABLED_CONTROL_PRESSED(0, (int)ControllerInputs::INPUT_ATTACK))
+				if (PAD::IS_DISABLED_CONTROL_PRESSED(0, (int)ControllerInputs::INPUT_ATTACK) && can_shoot())
 				{
 					const auto weapon_entity = WEAPON::GET_CURRENT_PED_WEAPON_ENTITY_INDEX(self::ped, 0);
 					if (!weapon_entity)
@@ -62,5 +78,6 @@ namespace big
 		}
 	};
 
-	rapid_fire g_rapid_fire("rapidfire", "BACKEND_LOOPED_WEAPONS_RAPID_FIRE", "BACKEND_LOOPED_WEAPONS_RAPID_FIRE_DESC", g.weapons.rapid_fire);
+	rapid_fire g_rapid_fire("rapidfire", "BACKEND_LOOPED_WEAPONS_RAPID_FIRE", "BACKEND_LOOPED_WEAPONS_RAPID_FIRE_DESC",
+	    g.weapons.rapid_fire);
 }

src/backend/reactions/reaction.cpp
@@ -63,7 +63,7 @@ namespace big
 		if (announce_in_chat)
 		{
 			g_fiber_pool->queue_job([player, this] {
-				auto chat = std::format("{} {}", g.session.chat_output_prefix, g_translation_service.get_translation(m_announce_message));
+				auto chat = std::format("{} {}", g.session.chat_output_prefix, std::vformat(g_translation_service.get_translation(m_announce_message), std::make_format_args(player->get_name())));
 
 				if (g_hooking->get_original<hooks::send_chat_message>()(*g_pointers->m_gta.m_send_chat_ptr,
 				        g_player_service->get_self()->get_net_data(),

src/byte_patch_manager.cpp
@@ -23,8 +23,10 @@ namespace big
 		    memory::byte_patch::make(g_pointers->m_gta.m_max_wanted_level.add(14).rip().as<uint32_t*>(), 0).get();
 
 		// Patch World Model Spawn Bypass
+		std::array<uint8_t, 24> world_spawn_patch;
+		std::fill(world_spawn_patch.begin(), world_spawn_patch.end(), 0x90);
 		world_model_bypass::m_world_model_spawn_bypass =
-		    memory::byte_patch::make(g_pointers->m_gta.m_world_model_spawn_bypass.add(3).rip().as<PVOID*>(), 0).get();
+		    memory::byte_patch::make(g_pointers->m_gta.m_world_model_spawn_bypass, world_spawn_patch).get();
 
 		// Patch blocked explosions
 		explosion_anti_cheat_bypass::m_can_blame_others =

src/core/settings.hpp
@@ -625,7 +625,7 @@ namespace big
 					NLOHMANN_DEFINE_TYPE_INTRUSIVE(auto_disarm, enable, neutralize)
 				} auto_disarm{};
 
-				NLOHMANN_DEFINE_TYPE_INTRUSIVE(nearby, ignore, ped_rain, veh_rain, high_alert, ped_rush, combative, auto_disarm)
+				NLOHMANN_DEFINE_TYPE_INTRUSIVE(nearby, ignore, high_alert, ped_rush, auto_disarm)
 			} nearby{};
 
 			struct model_swapper

src/gta_pointers.hpp
@@ -43,7 +43,7 @@ namespace big
 	{
 		memory::handle m_max_wanted_level;
 
-		memory::handle m_world_model_spawn_bypass;
+		PVOID m_world_model_spawn_bypass;
 
 		memory::handle m_blame_explode;
 

src/pointers.cpp
@@ -1510,10 +1510,10 @@ namespace big
         // World Model Spawn Bypass
         {
             "WMSB",
-            "48 85 C0 0F 84 ? ? ? ? 8B 48 ? C1 E9 ? F6 C1 ? 0F 84 ? ? ? ? 45 84 E4",
+            "48 85 C0 0F 84 ? ? ? ? 8B 48 50",
             [](memory::handle ptr)
             {
-                g_pointers->m_gta.m_world_model_spawn_bypass = ptr;
+                g_pointers->m_gta.m_world_model_spawn_bypass = ptr.as<PVOID>();
             }
         },
         // Blame Explode

src/views/network/view_network.cpp
@@ -19,25 +19,11 @@
 
 namespace big
 {
-	/*struct SessionType
+	struct SessionType
 	{
 		eSessionType id;
-		const std::string_view name;
+		const char* name;
 	};
-
-	const SessionType sessions[] = {
-	    {eSessionType::JOIN_PUBLIC, "BACKEND_SESSION_TYPE_JOIN_PUBLIC"_T},
-	    {eSessionType::NEW_PUBLIC, "BACKEND_SESSION_TYPE_NEW_PUBLIC"_T},
-	    {eSessionType::CLOSED_CREW, "BACKEND_SESSION_TYPE_CLOSED_CREW"_T},
-	    {eSessionType::CREW, "BACKEND_SESSION_TYPE_CREW"_T},
-	    {eSessionType::CLOSED_FRIENDS, "BACKEND_SESSION_TYPE_CLOSED_FRIENDS"_T},
-	    {eSessionType::FIND_FRIEND, "BACKEND_SESSION_TYPE_FIND_FRIEND"_T},
-	    {eSessionType::SOLO, "BACKEND_SESSION_TYPE_SOLO"_T},
-	    {eSessionType::INVITE_ONLY, "BACKEND_SESSION_TYPE_INVITE_ONLY"_T},
-	    {eSessionType::JOIN_CREW, "BACKEND_SESSION_TYPE_JOIN_CREW"_T},
-	    {eSessionType::SC_TV, "BACKEND_SESSION_TYPE_SC_TV"_T},
-	    {eSessionType::LEAVE_ONLINE, "BACKEND_SESSION_TYPE_LEAVE_ONLINE"_T},
-	};*/
 
 	void render_rid_joiner()
 	{
@@ -105,23 +91,23 @@ namespace big
 
 			ImGui::Spacing();
 
-			static const std::vector<std::tuple<eSessionType, std::string_view>> sessions = { //This has to be here because if it's generated at compile time, the translations break for some reason.
-				make_tuple(eSessionType::JOIN_PUBLIC, "BACKEND_SESSION_TYPE_JOIN_PUBLIC"_T),
-				make_tuple(eSessionType::NEW_PUBLIC, "BACKEND_SESSION_TYPE_NEW_PUBLIC"_T),
-				make_tuple(eSessionType::CLOSED_CREW, "BACKEND_SESSION_TYPE_CLOSED_CREW"_T),
-				make_tuple(eSessionType::CREW, "BACKEND_SESSION_TYPE_CREW"_T),
-				make_tuple(eSessionType::CLOSED_FRIENDS, "BACKEND_SESSION_TYPE_CLOSED_FRIENDS"_T),
-				make_tuple(eSessionType::FIND_FRIEND, "BACKEND_SESSION_TYPE_FIND_FRIEND"_T),
-				make_tuple(eSessionType::SOLO, "BACKEND_SESSION_TYPE_SOLO"_T),
-				make_tuple(eSessionType::INVITE_ONLY, "BACKEND_SESSION_TYPE_INVITE_ONLY"_T),
-				make_tuple(eSessionType::JOIN_CREW, "BACKEND_SESSION_TYPE_JOIN_CREW"_T),
-				make_tuple(eSessionType::SC_TV, "BACKEND_SESSION_TYPE_SC_TV"_T),
-				make_tuple(eSessionType::LEAVE_ONLINE, "BACKEND_SESSION_TYPE_LEAVE_ONLINE"_T)
-			};
+			static const auto sessions = std::to_array<SessionType>({ //This has to be here because if it's generated at compile time, the translations break for some reason.
+				{eSessionType::JOIN_PUBLIC, "BACKEND_SESSION_TYPE_JOIN_PUBLIC"},
+				{eSessionType::NEW_PUBLIC, "BACKEND_SESSION_TYPE_NEW_PUBLIC"},
+				{eSessionType::CLOSED_CREW, "BACKEND_SESSION_TYPE_CLOSED_CREW"},
+				{eSessionType::CREW, "BACKEND_SESSION_TYPE_CREW"},
+				{eSessionType::CLOSED_FRIENDS, "BACKEND_SESSION_TYPE_CLOSED_FRIENDS"},
+				{eSessionType::FIND_FRIEND, "BACKEND_SESSION_TYPE_FIND_FRIEND"},
+				{eSessionType::SOLO, "BACKEND_SESSION_TYPE_SOLO"},
+				{eSessionType::INVITE_ONLY, "BACKEND_SESSION_TYPE_INVITE_ONLY"},
+				{eSessionType::JOIN_CREW, "BACKEND_SESSION_TYPE_JOIN_CREW"},
+				{eSessionType::SC_TV, "BACKEND_SESSION_TYPE_SC_TV"},
+				{eSessionType::LEAVE_ONLINE, "BACKEND_SESSION_TYPE_LEAVE_ONLINE"}
+			});
 
 			for (const auto& [id, name] : sessions)
 			{
-				components::selectable(name, false, [&id] {
+				components::selectable(g_translation_service.get_translation(name), false, [&id] {
 					session::join_type(id);
 				});
 			}

src/views/self/view_weapons.cpp
@@ -14,20 +14,20 @@ namespace big
 	struct custom_weapon
 	{
 		big::CustomWeapon id;
-		const std::string_view name;
+		const char* name;
 	};
 
-	const custom_weapon custom_weapons[] = {
-	    {big::CustomWeapon::NONE, "VIEW_SELF_WEAPONS_NONE"_T},
-	    {big::CustomWeapon::CAGE_GUN, "VIEW_SELF_WEAPONS_CAGE_GUN"_T},
-	    {big::CustomWeapon::DELETE_GUN, "VIEW_SELF_WEAPONS_DELETE_GUN"_T},
-	    {big::CustomWeapon::GRAVITY_GUN, "VIEW_SELF_WEAPONS_GRAVITY_GUN"_T},
-	    {big::CustomWeapon::STEAL_VEHICLE_GUN, "BACKEND_LOOPED_WEAPONS_STEAL_VEHICLE_GUN"_T},
-	    {big::CustomWeapon::REPAIR_GUN, "BACKEND_LOOPED_WEAPONS_REPAIR_GUN"_T},
-	    {big::CustomWeapon::VEHICLE_GUN, "BACKEND_LOOPED_WEAPONS_VEHICLE_GUN"_T},
-	    {big::CustomWeapon::TP_GUN, "VIEW_SELF_WEAPONS_TP_GUN"_T},
-	    {big::CustomWeapon::PAINT_GUN, "VIEW_SELF_WEAPONS_PAINT_GUN"_T},
-	};
+	constexpr auto custom_weapons = std::to_array<custom_weapon>({
+	    {big::CustomWeapon::NONE, "VIEW_SELF_WEAPONS_NONE"},
+	    {big::CustomWeapon::CAGE_GUN, "VIEW_SELF_WEAPONS_CAGE_GUN"},
+	    {big::CustomWeapon::DELETE_GUN, "VIEW_SELF_WEAPONS_DELETE_GUN"},
+	    {big::CustomWeapon::GRAVITY_GUN, "VIEW_SELF_WEAPONS_GRAVITY_GUN"},
+	    {big::CustomWeapon::STEAL_VEHICLE_GUN, "BACKEND_LOOPED_WEAPONS_STEAL_VEHICLE_GUN"},
+	    {big::CustomWeapon::REPAIR_GUN, "BACKEND_LOOPED_WEAPONS_REPAIR_GUN"},
+	    {big::CustomWeapon::VEHICLE_GUN, "BACKEND_LOOPED_WEAPONS_VEHICLE_GUN"},
+	    {big::CustomWeapon::TP_GUN, "VIEW_SELF_WEAPONS_TP_GUN"},
+	    {big::CustomWeapon::PAINT_GUN, "VIEW_SELF_WEAPONS_PAINT_GUN"},
+	});
 
 	void view::weapons()
 	{
@@ -133,11 +133,11 @@ namespace big
 		ImGui::Checkbox("VIEW_WEAPON_CUSTOM_GUN_ONLY_FIRES_WHEN_THE_WEAPON_IS_OUT"_T.data(), &g.self.custom_weapon_stop);
 		CustomWeapon selected = g.weapons.custom_weapon;
 
-		if (ImGui::BeginCombo("WEAPON"_T.data(), custom_weapons[(int)selected].name.data()))
+		if (ImGui::BeginCombo("WEAPON"_T.data(), g_translation_service.get_translation(custom_weapons[(int)selected].name).data()))
 		{
 			for (const custom_weapon& weapon : custom_weapons)
 			{
-				if (ImGui::Selectable(weapon.name.data(), weapon.id == selected))
+				if (ImGui::Selectable(g_translation_service.get_translation(weapon.name).data(), weapon.id == selected))
 				{
 					g.weapons.custom_weapon = weapon.id;
 				}