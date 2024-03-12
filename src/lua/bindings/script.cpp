f55f18d0f44ada72be993041656b316dd10f357e
Alice <105157805+Alice2333g@users.noreply.github.com>
2024-03-13 05:42:22 +0800

    update

--- src/lua/bindings/script.cpp
+++ src/lua/bindings/script.cpp
@@ -157,9 +157,9 @@ namespace lua::script
 	// Name: execute_as_script
 	// Param: script_name: string: target script thread.
 	// Param: func: function: function that will be executed once in the script thread.
-	static void execute_as_script(const std::string& script_name, sol::protected_function func_, sol::this_state state)
+	static void execute_as_script(const std::string& script_name, sol::protected_function func, sol::this_state state)
 	{
-		big::gta_util::execute_as_script(rage::joaat(script_name), func_);
+		big::gta_util::execute_as_script(rage::joaat(script_name), func);
 	}
 
 	void bind(sol::state& state)
