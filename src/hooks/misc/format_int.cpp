afa09cbde744e93ae4ed8d5e3c1afc12ba2c0c6d
gir489returns <gir489returns@outlook.com>
2024-04-22 10:18:54 -0400

    Replace C-runtime strcpy_s with C++ std::strcpy.

--- src/hooks/misc/format_int.cpp
+++ src/hooks/misc/format_int.cpp
@@ -13,12 +13,12 @@ namespace big
 
 	void hooks::format_int(int64_t integer_to_format, char* format_string, size_t size_always_64, bool use_commas)
 	{
-		void* return_address        = _ReturnAddress();
-		unsigned char* return_bytes = static_cast<unsigned char*>(return_address);
+		auto return_address         = _ReturnAddress();
+		auto return_bytes           = static_cast<unsigned char*>(return_address);
 		if (return_bytes[0] == 0x48 && return_bytes[1] == 0x8D && return_bytes[2] == 0x15) //lea     rdx, aHcGreenlightFo ; "~HC_GREENLIGHT~ <font size='20'>"
 		{
 			auto money_format = format_money(integer_to_format);
-			strcpy_s(format_string, size_always_64, money_format.c_str());
+			std::strcpy(format_string, money_format.c_str());
 			return;
 		}
 		g_hooking->get_original<format_int>()(integer_to_format, format_string, size_always_64, use_commas);
