51ee53ee516aa4fbd916e416b309f3b00ea979e0
gir489 <100792176+gir489returns@users.noreply.github.com>
2024-04-24 15:14:41 -0400

    Fixed format_money not working. (#2969)

--- src/hooks/misc/format_int.cpp
+++ src/hooks/misc/format_int.cpp
@@ -5,7 +5,7 @@ namespace big
 	static inline std::string format_money(int64_t amount)
 	{
 		std::stringstream ss;
-		ss.imbue(std::locale(""));
+		ss.imbue(std::locale("en_US.UTF-8"));
 		ss << "$" << std::put_money(static_cast<long double>(amount) * 100, false);
 		std::string money = ss.str();
 		return money.substr(0, money.size() - 3);
@@ -15,12 +15,14 @@ namespace big
 	{
 		auto return_address         = _ReturnAddress();
 		auto return_bytes           = static_cast<unsigned char*>(return_address);
-		if (return_bytes[0] == 0x48 && return_bytes[1] == 0x8D && return_bytes[2] == 0x15) //lea     rdx, aHcGreenlightFo ; "~HC_GREENLIGHT~ <font size='20'>"
+		if (g.window.gui.format_money && return_bytes[0] == 0x48 && return_bytes[1] == 0x8D && return_bytes[2] == 0x15) //lea     rdx, aHcGreenlightFo ; "~HC_GREENLIGHT~ <font size='20'>"
 		{
 			auto money_format = format_money(integer_to_format);
-			std::strcpy(format_string, money_format.c_str());
-			return;
+			std::strncpy(format_string, money_format.c_str(), size_always_64);
 		}
-		g_hooking->get_original<format_int>()(integer_to_format, format_string, size_always_64, use_commas);
+		else
+		{
+			g_hooking->get_original<format_int>()(integer_to_format, format_string, size_always_64, use_commas);
+		}
 	}
 }
