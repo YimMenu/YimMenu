a3a2500702c145a42eb21fb8b90f7dfb29b99acd
gir489returns <gir489returns@outlook.com>
2024-05-02 20:05:55 -0400

    Closes #3030 Closes #3032

--- src/hooks/protections/received_clone_remove.cpp
+++ src/hooks/protections/received_clone_remove.cpp
@@ -5,7 +5,8 @@ namespace big
 {
 	void hooks::received_clone_remove(CNetworkObjectMgr* mgr, CNetGamePlayer* src, CNetGamePlayer* dst, int16_t object_id, uint32_t ownership_token)
 	{
-		if (dst->m_player_info->m_ped->m_net_object->m_object_id == object_id) [[unlikely]]
+		if (!dst || !dst->m_player_info || !dst->m_player_info->m_ped || !dst->m_player_info->m_ped->m_net_object
+		    || dst->m_player_info->m_ped->m_net_object->m_object_id == object_id) [[unlikely]]
 		{
 			notify::crash_blocked(src, "player ped removal");
 			return;
