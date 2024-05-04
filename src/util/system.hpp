d4d2193359a2182fc36a736b81a6ffe2b3b5b5dd
timdev <168923949+timdev-ger@users.noreply.github.com>
2024-05-04 18:51:15 +0200

    Update system.hpp
    TODO finished.

--- src/util/system.hpp
+++ src/util/system.hpp
@@ -5,30 +5,23 @@
 
 namespace big::system
 {
-	inline void dump_entry_points()
-	{
-		// TODO!
-#if 0
-		DWORD64 base_address = memory::module("GTA5.exe").begin().as<DWORD64>();
+    inline void dump_entry_points()
+    {
+        const auto file_path = g_file_manager.get_project_file("./entrypoints.txt");
+        std::ofstream file(file_path.get_path(), std::ios::out | std::ios::trunc);
 
-		const auto file_path = g_file_manager.get_project_file("./entrypoints.txt");
-		auto file            = std::ofstream(file_path.get_path(), std::ios::out | std::ios::trunc);
+        const uintptr_t base_address = memory::module("GTA5.exe").begin().as<uintptr_t>();
 
-		for (auto& map : g_crossmap)
-		{
-			auto address = g_pointers->m_gta.m_get_native_handler(g_pointers->m_gta.m_native_registration_table, map.second);
+        for (const auto& [key, value] : g_crossmap)
+        {
+            const auto address = g_pointers->m_gta.m_get_native_handler(g_pointers->m_gta.m_native_registration_table, value);
+            file << std::hex << std::uppercase << "0x" << key << " : GTA5.exe + 0x" << get_relative_address(address) << std::endl;
+        }
+    }
 
-			file << std::hex << std::uppercase << "0x" << map.first << " : GTA5.exe + 0x" << (DWORD64)address - base_address << std::endl;
-		}
-
-		file.close();
-#endif
-	}
-
-	inline uintptr_t get_relative_address(void* ptr)
-	{
-		uintptr_t base_address = memory::module("GTA5.exe").begin().as<uintptr_t>();
-
-		return (uintptr_t)ptr - base_address;
-	}
+    inline uintptr_t get_relative_address(void* ptr)
+    {
+        const uintptr_t base_address = memory::module("GTA5.exe").begin().as<uintptr_t>();
+        return (uintptr_t)ptr - base_address;
+    }
 }
