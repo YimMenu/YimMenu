9fa232dbe7e3b208d007750a23e5dbbb495e76b7
Muhammad007228 <evmeschenco@gmail.com>
2024-05-06 13:59:44 +0300

    replaced with multi-kick

--- src/hooks/protections/receive_net_message.cpp
+++ src/hooks/protections/receive_net_message.cpp
@@ -130,12 +130,7 @@ namespace big
 					    && !(player->is_trusted || (player->is_friend() && g.session.trust_friends) || g.session.trust_session))
 					{
 						session::add_infraction(player, Infraction::CHAT_SPAM);
-						if (g_player_service->get_self()->is_host())
-							dynamic_cast<player_command*>(command::get("breakup"_J))->call(player, {}),
-							    dynamic_cast<player_command*>(command::get("hostkick"_J))->call(player, {});
-
-						dynamic_cast<player_command*>(command::get("endkick"_J))->call(player, {});
-						dynamic_cast<player_command*>(command::get("nfkick"_J))->call(player, {});
+						dynamic_cast<player_command*>(command::get("multikick"_J))->call(player, {});
 					}
 					return true;
 				}
