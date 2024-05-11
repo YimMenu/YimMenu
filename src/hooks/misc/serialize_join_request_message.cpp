eaf517e7c5bb372d0736fe0244c5254703eb27a4
maybegreat48 <email@hostname>
2024-05-11 16:54:27 -0400

    fix: revert changes to hooks

--- src/hooks/misc/serialize_join_request_message.cpp
+++ src/hooks/misc/serialize_join_request_message.cpp
@@ -13,7 +13,7 @@ namespace big
 		if (info->unk_0xC0 == 0)
 			info->unk_0xC0 = 1; // TODO: do we need this anymore?
 
-		if (g.protections.desync_kick && (!*g_pointers->m_gta.m_is_session_started || !g_player_service->get_self()->is_host()))
+		if (g.protections.desync_kick)
 			info->m_gamer_info.m_nat_type = 0;
 
 		info->m_num_handles = 0;
@@ -27,7 +27,7 @@ namespace big
 		if (g.session.join_in_sctv_slots)
 			data.m_matchmaking_group = 4;
 
-		if (g.protections.desync_kick && (!*g_pointers->m_gta.m_is_session_started || !g_player_service->get_self()->is_host()))
+		if (g.protections.desync_kick)
 			data.m_nat_type = 0;
 
 		return g_hooking->get_original<hooks::serialize_join_request_message_2>()(msg, buf, size, bits_serialized);
