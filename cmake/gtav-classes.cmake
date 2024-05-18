36da09e57e67e655687d395b3551d9b97480d202
gir489returns <gir489returns@outlook.com>
2024-05-18 18:27:35 -0400

    Fast forwarded gtav-classes.cmake.

--- cmake/gtav-classes.cmake
+++ cmake/gtav-classes.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
     gtav_classes
     GIT_REPOSITORY https://github.com/Yimura/GTAV-Classes.git
-    GIT_TAG        df0def702dac8f7ba9bf7107734ac30b4db54e96
+    GIT_TAG        46c1c63396cf40b206b9f5dd612f293809eefcd1
     GIT_PROGRESS TRUE
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
