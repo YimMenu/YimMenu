206a7ed446916b11d78f28b21e77f6869dd414d1
maybegreat48 <email@hostname>
2024-06-21 08:33:59 -0400

    fix: use upstream classes

--- cmake/gtav-classes.cmake
+++ cmake/gtav-classes.cmake
@@ -1,11 +1,9 @@
 include(FetchContent)
 
-add_compile_definitions(CXX_FORMAT_SUPPORT)
-
 FetchContent_Declare(
     gtav_classes
-    GIT_REPOSITORY https://github.com/maybegreat48/GTAV-Classes.git
-    GIT_TAG        aa973216cc5215ff9ff70f467b96637f417c9bdd
+    GIT_REPOSITORY https://github.com/Yimura/GTAV-Classes.git
+    GIT_TAG        03b811b16a7b1697c4f156e426ca40aa31c1a5d5
     GIT_PROGRESS TRUE
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
