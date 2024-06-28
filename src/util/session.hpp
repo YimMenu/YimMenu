579aa5182aa1c1a144aa67c86ece411c6433afb3
maybegreat48 <email@hostname>
2024-06-28 10:25:51 -0400

    fix: use original chat sender

--- src/util/session.hpp
+++ src/util/session.hpp
@@ -72,8 +72,8 @@ namespace big::session
 	{
 		int idx = index / 32;
 		int bit = index % 32;
-		misc::set_bit(scr_globals::gsbd_fm_events.at(11).at(361).at(idx, 1).as<int*>(), bit);
-		misc::set_bit(scr_globals::gsbd_fm_events.at(11).at(353).at(idx, 1).as<int*>(), bit);
+		misc::set_bit(scr_globals::gsbd_fm_events.at(11).at(379).at(idx, 1).as<int*>(), bit);
+		misc::set_bit(scr_globals::gsbd_fm_events.at(11).at(370).at(idx, 1).as<int*>(), bit);
 		misc::set_bit((int*)&scr_globals::gpbd_fm_3.as<GPBD_FM_3*>()->Entries[self::id].BossGoon.ActiveFreemodeEvents[idx], bit);
 	}
 
@@ -81,8 +81,8 @@ namespace big::session
 	{
 		int idx = index / 32;
 		int bit = index % 32;
-		misc::clear_bit(scr_globals::gsbd_fm_events.at(11).at(361).at(idx, 1).as<int*>(), bit);
-		misc::clear_bit(scr_globals::gsbd_fm_events.at(11).at(353).at(idx, 1).as<int*>(), bit);
+		misc::clear_bit(scr_globals::gsbd_fm_events.at(11).at(379).at(idx, 1).as<int*>(), bit);
+		misc::clear_bit(scr_globals::gsbd_fm_events.at(11).at(370).at(idx, 1).as<int*>(), bit);
 		misc::clear_bit((int*)&scr_globals::gpbd_fm_3.as<GPBD_FM_3*>()->Entries[self::id].BossGoon.ActiveFreemodeEvents[idx], bit);
 	}
 
--- src/views/players/player/player_misc.cpp
+++ src/views/players/player/player_misc.cpp
@@ -1,5 +1,6 @@
 #include "views/view.hpp"
 #include "services/players/player_service.hpp"
+#include "util/scripts.hpp"
 
 namespace big
 {
@@ -31,6 +32,16 @@ namespace big
 		ImGui::Checkbox("NEVER_WANTED"_T.data(), &g_player_service->get_selected()->never_wanted);
 		ImGui::Checkbox("SEMI_GODMODE"_T.data(), &g_player_service->get_selected()->semi_godmode);
 		ImGui::Checkbox("VIEW_NET_SESSION_FIX_VEHICLE"_T.data(), &g_player_service->get_selected()->fix_vehicle);
+
+		components::button("Trigger UFO Abduction", [] {
+			scripts::force_host("freemode"_J);
+			*scr_globals::gsbd_fm_events.at(11).at(145).as<int*>() = g_player_service->get_selected()->id();
+			*scr_globals::gsbd_fm_events.at(11).at(144).as<int*>() = scripts::launcher_index_from_hash("fm_content_ufo_abduction"_J);
+			*scr_globals::gsbd_fm_events.at(11).at(146).as<bool*>() = false;
+			script::get_current()->yield(2s);
+			scripts::start_launcher_script("fm_content_ufo_abduction"_J);
+		});
+
 		ImGui::EndGroup();
 	}
 }
