cf8d9cf936a7d263129828f3569a02e6aeb23aba
Andreas Maerten <24669514+Yimura@users.noreply.github.com>
2024-07-03 22:46:07 +0200

    chore: correct dependency

--- cmake/gtav-classes.cmake
+++ cmake/gtav-classes.cmake
@@ -2,8 +2,8 @@ include(FetchContent)
 
 FetchContent_Declare(
     gtav_classes
-    GIT_REPOSITORY https://github.com/maybegreat48/GTAV-Classes.git
-    GIT_TAG        81a6214086ff3495d002bdec76fb73baf552cf76
+    GIT_REPOSITORY https://github.com/Yimura/GTAV-Classes.git
+    GIT_TAG        3d41688e9b74a1adb33a90bf529a647177fa486c
     GIT_PROGRESS TRUE
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
