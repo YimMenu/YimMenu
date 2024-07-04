cc3bcea9f13e752f0358ea3d8199b3e7d5c9f7ef
gir489 <100792176+gir489returns@users.noreply.github.com>
2024-07-04 14:34:49 -0400

    Pull down changes from gtav-classes project. (#3300)

--- cmake/gtav-classes.cmake
+++ cmake/gtav-classes.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
     gtav_classes
     GIT_REPOSITORY https://github.com/Yimura/GTAV-Classes.git
-    GIT_TAG        3d41688e9b74a1adb33a90bf529a647177fa486c
+    GIT_TAG        f37ff433e2910d5db3af7ddaee2abc82c508b0c0
     GIT_PROGRESS TRUE
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
