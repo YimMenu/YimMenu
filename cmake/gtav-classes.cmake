c5b8fbd0684e4caf09d54f48d5896dd7853efe30
tyackman <tyackman@gmail.com>
2024-07-08 13:53:50 -0500

    bump gtav-classes

--- cmake/gtav-classes.cmake
+++ cmake/gtav-classes.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
     gtav_classes
     GIT_REPOSITORY https://github.com/Yimura/GTAV-Classes.git
-    GIT_TAG        f37ff433e2910d5db3af7ddaee2abc82c508b0c0
+    GIT_TAG        06c58d9c11a9a22336947fbe430d5f4951ff34d7
     GIT_PROGRESS TRUE
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
