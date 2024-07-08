37cc3f3b7f5fa2500745fb18a25f3ff716af6adc
gir489 <100792176+gir489returns@users.noreply.github.com>
2024-07-08 13:34:57 -0400

    Closes #3302 (#3305)

--- cmake/asmjit.cmake
+++ cmake/asmjit.cmake
@@ -7,5 +7,8 @@ FetchContent_Declare(
     GIT_TAG        ffac9f36fb045dd2c6a81e1b5b9ccc115e5ef924
     GIT_PROGRESS TRUE
 )
+set(ASMJIT_STATIC ON) 
+set(ASMJIT_NO_AARCH64 ON) 
+set(ASMJIT_NO_FOREIGN ON) 
 FetchContent_MakeAvailable(asmjit)
 set_property(TARGET asmjit PROPERTY CXX_STANDARD 23)
