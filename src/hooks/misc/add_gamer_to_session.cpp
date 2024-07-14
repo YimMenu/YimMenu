49a34f5789238c451e2cfe1dbd3c9b88f121666c
gir489 <100792176+gir489returns@users.noreply.github.com>
2024-07-14 11:10:40 -0400

    Closes #3345 (#3346)

--- src/hooks/misc/add_gamer_to_session.cpp
+++ src/hooks/misc/add_gamer_to_session.cpp
@@ -14,7 +14,7 @@ namespace big
 		    && info->m_gamer_info.m_gamer_handle.m_rockstar_id == g_player_service->get_self()->get_rockstar_id())
 		{
 			info->m_gamer_info.m_host_token = g.session.original_host_token;
-			info->m_gamer_info.m_peer_id = (info->m_gamer_info.m_peer_id & 0xFFFFFFFF) | (g.session.original_host_token << 32); // TODO: P2pSecurity info message
+			info->m_gamer_info.m_peer_id = (info->m_gamer_info.m_peer_id & 0xFFFFFFFF) | (g.session.original_host_token & 0xFFFFFFFF00000000); // TODO: P2pSecurity info message
 		}
 
 		return g_hooking->get_original<hooks::add_gamer_to_session>()(mgr, msg_id, req_id, info, flags, a6);
