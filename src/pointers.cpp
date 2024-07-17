d0c60357c809474635cbcd718725ac75d259870b
maybegreat48 <email@hostname>
2024-07-17 10:40:28 -0400

    fix(pointers): fix nullsub pattern

--- src/pointers.cpp
+++ src/pointers.cpp
@@ -1644,7 +1644,7 @@ namespace big
         // Nullsub
         {
             "NS",
-            "90 C3",
+            "C3",
             [](memory::handle ptr)
             {
                 g_pointers->m_gta.m_nullsub = ptr.as<void(*)()>();
